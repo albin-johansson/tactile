485adbff444495c6af6a2916544bdd34655dd115
Albin Johansson <albinoscar99@hotmail.com>
2020-03-03 08:17:40 +0100

    Update flood_fill.cpp

--- src/core/flood_fill.cpp
+++ src/core/flood_fill.cpp
@@ -5,33 +5,6 @@
 #include "map_position.h"
 
 namespace tactile {
-namespace {
-
-/**
- * Updates the tile layer and queue if the supplied position should be
- * affected by the flood fill.
- *
- * @param layer the tile layer in which the flood fill is being run.
- * @param position the position in the tile layer that will be checked.
- * @param target the target tile type.
- * @param replacement the replacement tile type.
- * @param queue the queue that will be updated.
- * @since 0.1.0
- */
-void update(TileLayer& layer,
-            const MapPosition& position,
-            TileID target,
-            TileID replacement,
-            std::queue<MapPosition>& queue) noexcept
-{
-  const auto tile = layer.tile_at(position);
-  if (tile && tile == target) {
-    layer.set_tile(position, replacement);
-    queue.push(position);
-  }
-}
-
-}  // namespace
 
 void flood_fill(TileLayer& layer,
                 const MapPosition& origin,
@@ -50,14 +23,23 @@ void flood_fill(TileLayer& layer,
   std::queue<MapPosition> queue;
   queue.push(origin);
 
+  const auto update = [&layer, &queue, target, replacement](
+                          const MapPosition& position) noexcept {
+    const auto tile = layer.tile_at(position);
+    if (tile && tile == target) {
+      layer.set_tile(position, replacement);
+      queue.push(position);
+    }
+  };
+
   while (!queue.empty()) {
     const auto pos = queue.front();
     queue.pop();
 
-    update(layer, pos.west(), target, replacement, queue);
-    update(layer, pos.east(), target, replacement, queue);
-    update(layer, pos.south(), target, replacement, queue);
-    update(layer, pos.north(), target, replacement, queue);
+    update(pos.west());
+    update(pos.east());
+    update(pos.south());
+    update(pos.north());
   }
 }
 
