962bb8490c777300e0020c73aa101b6fdb9a92b0
Albin Johansson <albinoscar99@hotmail.com>
2020-03-20 22:07:55 +0100

    Add test for set_size_policy

--- src/ui/widget_size_policy.h
+++ src/ui/widget_size_policy.h
@@ -6,8 +6,28 @@ class QWidget;
 
 namespace tactile {
 
+/**
+ * Sets the size policy of a widget. This method has no effect if the
+ * supplied widget is null.
+ *
+ * @param widget a pointer to the widget that will be affected.
+ * @param policy the size policy that will be used as the vertical and
+ * horizontal size policies.
+ * @since 0.1.0
+ */
 void set_size_policy(QWidget* widget, QSizePolicy::Policy policy) noexcept;
 
+/**
+ * Sets the size policy of a widget. This method has no effect if the
+ * supplied widget is null.
+ *
+ * @param widget a pointer to the widget that will be affected.
+ * @param horizontalPolicy the size policy that will be used as the horizontal
+ * policy.
+ * @param verticalPolicy the size policy that will be used as the vertical
+ * policy.
+ * @since 0.1.0
+ */
 void set_size_policy(QWidget* widget,
                      QSizePolicy::Policy horizontalPolicy,
                      QSizePolicy::Policy verticalPolicy) noexcept;
---
+++ test/widget_size_policy_test.cpp
@@ -0,0 +1,30 @@
+#include "widget_size_policy.h"
+
+#include <QWidget>
+#include <catch.hpp>
+
+using namespace tactile;
+
+TEST_CASE("set_size_policy1", "[set_size_policy]")
+{
+  QWidget widget;
+
+  const auto policy = QSizePolicy::Expanding;
+  set_size_policy(&widget, policy);
+
+  CHECK(widget.sizePolicy().horizontalPolicy() == policy);
+  CHECK(widget.sizePolicy().verticalPolicy() == policy);
+}
+
+TEST_CASE("set_size_policy2", "[set_size_policy]")
+{
+  QWidget widget;
+
+  const auto verticalPolicy = QSizePolicy::Preferred;
+  const auto horizontalPolicy = QSizePolicy::Minimum;
+
+  set_size_policy(&widget, horizontalPolicy, verticalPolicy);
+
+  CHECK(widget.sizePolicy().horizontalPolicy() == horizontalPolicy);
+  CHECK(widget.sizePolicy().verticalPolicy() == verticalPolicy);
+}
