f4be92c53758f575e43fb7276ca38591ad517ea6
Albin Johansson <albinoscar99@hotmail.com>
2020-03-24 19:43:10 +0100

    Add serttings utils

---
+++ src/settings_utils.cpp
@@ -0,0 +1,56 @@
+#include "settings_utils.h"
+
+#include <QSettings>
+#include <QString>
+#include <QVariant>
+
+namespace tactile {
+namespace {
+
+[[nodiscard]] Maybe<QVariant> get_value(const char* setting) noexcept
+{
+  if (setting) {
+    QSettings settings;
+    const QString key = setting;
+    if (settings.contains(key)) {
+      return settings.value(key);
+    }
+  }
+
+  return nothing;
+}
+
+}  // namespace
+
+void set_if_absent(const char* setting, const QVariant& value) noexcept
+{
+  if (setting) {
+    QSettings settings;
+    const QString key = setting;
+    if (!settings.contains(key)) {
+      settings.setValue(key, value);
+    }
+  }
+}
+
+Maybe<QByteArray> settings_byte_array(const char* setting) noexcept
+{
+  const auto value = get_value(setting);
+  if (value && value->type() == QVariant::ByteArray) {
+    return value->toByteArray();
+  } else {
+    return nothing;
+  }
+}
+
+Maybe<bool> settings_bool(const char* setting) noexcept
+{
+  const auto value = get_value(setting);
+  if (value && value->type() == QVariant::Bool) {
+    return value->toBool();
+  } else {
+    return nothing;
+  }
+}
+
+}  // namespace tactile
