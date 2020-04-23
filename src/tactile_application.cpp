18e1a6b85b2b989c23f4bd1cd621d5f8f5f50fc0
Albin Johansson <albinoscar99@hotmail.com>
2020-04-23 23:21:39 +0200

    Add TODO about changing cursor in relation to active tool

--- src/tactile_application.cpp
+++ src/tactile_application.cpp
@@ -65,6 +65,11 @@ TactileApplication::TactileApplication(int argc, char** argv)
   init_connections();
   load_style_sheet(":/res/tactile_light.qss");
 
+  // TODO do this to indicate what tool is active
+//  QPixmap pixmap{":/res/icons/icons8/color/64/eraser.png"};
+//  QCursor c{pixmap};
+//  QApplication::setOverrideCursor(c);
+
   m_window->show();
 }
 
