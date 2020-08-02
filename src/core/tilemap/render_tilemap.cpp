c4e66ef4ef21eb01ec0c84d17d41644b427568e1
Albin Johansson <albinoscar99@hotmail.com>
2020-08-03 00:44:37 +0200

    Update render_tilemap.cpp

--- src/core/tilemap/render_tilemap.cpp
+++ src/core/tilemap/render_tilemap.cpp
@@ -36,44 +36,49 @@ struct render_bounds final {
 
 [[nodiscard]] auto create_pen() noexcept -> QPen
 {
-  QPen pen;
-  pen.setColor(Qt::black);
+  QPen pen{Qt::black};
   pen.setWidth(1);
   pen.setDashOffset(0);
   pen.setDashPattern({3, 4});
   return pen;
 }
 
+void render_layer(QPainter& painter,
+                  const tile_layer& layer,
+                  const render_bounds& bounds,
+                  const int tileSize,
+                  const bool renderGrid) noexcept
+{
+  constexpr QColor emptyGray{0x55, 0x55, 0x55};
+
+  for (auto row = bounds.minRow; row < bounds.maxRow; ++row) {
+    for (auto col = bounds.minCol; col < bounds.maxCol; ++col) {
+      const auto x = col * tileSize;
+      const auto y = row * tileSize;
+      painter.fillRect(x, y, tileSize, tileSize, emptyGray);
+      if (renderGrid) {
+        painter.drawRect(x, y, tileSize, tileSize);
+      }
+    }
+  }
+}
+
 }  // namespace
 
 void render_tilemap(QPainter& painter, const tilemap& map) noexcept
 {
-  const auto tileSize = map.get_tile_size().get();
-  const auto bounds =
-      calc_bounds(painter.viewport(), map.rows(), map.cols(), tileSize);
-
   const auto renderGrid = settings_bool("visuals-grid").value_or(true);
-
   if (renderGrid) {
     painter.setPen(create_pen());
   }
 
-  const QColor emptyGray{0x55, 0x55, 0x55};
+  const auto tileSize = map.get_tile_size().get();
+  const auto bounds =
+      calc_bounds(painter.viewport(), map.rows(), map.cols(), tileSize);
 
   for (auto& layer : map) {
-    if (!layer.visible()) {
-      continue;
-    }
-
-    for (auto row = bounds.minRow; row < bounds.maxRow; ++row) {
-      for (auto col = bounds.minCol; col < bounds.maxCol; ++col) {
-        const auto x = col * tileSize;
-        const auto y = row * tileSize;
-        painter.fillRect(x, y, tileSize, tileSize, emptyGray);
-        if (renderGrid) {
-          painter.drawRect(x, y, tileSize, tileSize);
-        }
-      }
+    if (layer.visible()) {
+      render_layer(painter, layer, bounds, tileSize, renderGrid);
     }
   }
 }
