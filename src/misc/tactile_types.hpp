94227b0288c52b97f5b856d4627973975d58fead
Albin Johansson <albinoscar99@hotmail.com>
2020-08-21 15:51:06 +0200

    Update tactile_types.hpp

--- src/misc/tactile_types.hpp
+++ src/misc/tactile_types.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <concepts>
 #include <gsl/gsl>
 #include <type_traits>
 
@@ -7,13 +8,17 @@ namespace tactile {
 
 using tile_id = int;
 
-inline constexpr tile_id empty = 0;
+inline constexpr tile_id empty {0};
 
-template <typename T>
+// clang-format off
+
+template <typename T> requires std::is_pointer_v<T>
 using owner = gsl::owner<T>;
 
-template <typename T>
+template <typename T> requires std::is_pointer_v<T>
 using not_null = gsl::not_null<T>;
+
+// clang-format on
 
 using czstring = gsl::czstring<>;
 using u16_czstring = gsl::cu16zstring<>;
