19fa9c5f66415ddcb2198906d795115dc19218c8
Albin Johansson <albinoscar99@hotmail.com>
2020-09-01 23:00:11 +0200

    Document map_item class

--- src/gui/editor/map_item.hpp
+++ src/gui/editor/map_item.hpp
@@ -1,15 +1,32 @@
 #pragma once
 
 #include <QGraphicsItem>
-#include <QObject>
 
 #include "core_model.hpp"
+#include "tactile_fwd.hpp"
 
 namespace tactile::gui {
 
+/**
+ * @class map_item
+ *
+ * @brief Responsible for the visual representation of maps.
+ *
+ * @since 0.1.0
+ *
+ * @headerfile map_item.hpp
+ */
 class map_item final : public QGraphicsItem
 {
  public:
+  /**
+   * @brief Creates a map item.
+   *
+   * @param map the associated map, cannot be null.
+   * @param parent the parent item.
+   *
+   * @since 0.1.0
+   */
   explicit map_item(not_null<model::tilemap*> map,
                     QGraphicsItem* parent = nullptr);
 
@@ -20,7 +37,7 @@ class map_item final : public QGraphicsItem
   [[nodiscard]] auto boundingRect() const -> QRectF override;
 
  private:
-  model::tilemap* m_map;
+  model::tilemap* m_map{};
 
   void draw_layer(QPainter& painter,
                   const model::tile_layer& layer,
