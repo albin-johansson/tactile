e7150fa855c984ff891a0ae37d29a56f192b3ddb
Albin Johansson <albinoscar99@hotmail.com>
2020-09-07 13:15:55 +0200

    Add small_map

---
+++ src/util/small_map.hpp
@@ -0,0 +1,249 @@
+#pragma once
+
+#include <algorithm>  // find_if
+#include <stdexcept>  // out_of_range
+#include <utility>    // forward
+#include <vector>     // vector
+
+namespace tactile {
+
+/**
+ * @class small_map
+ *
+ * @brief An associative container for small sets of key-value pairs.
+ *
+ * @details This container is useful when the number of pairs are small, often
+ * less than 10 elements. This is because the internal representation is based
+ * on a `std::vector`, which means that iteration and lookup is very fast and
+ * cache-friendly.
+ *
+ * @tparam Key the type of the keys.
+ * @tparam Value the type of the mapped values.
+ *
+ * @since 0.1.0
+ *
+ * @headerfile small_map.hpp
+ */
+template <typename Key, typename Value>
+class small_map final
+{
+  using storage_type = std::vector<std::pair<Key, Value>>;
+
+ public:
+  using key_type = Key;
+  using mapped_type = Value;
+  using value_type = typename storage_type::value_type;
+  using size_type = typename storage_type::size_type;
+  using iterator = typename storage_type::iterator;
+  using const_iterator = typename storage_type::const_iterator;
+
+  /**
+   * @brief Ensures that the map has capacity for the specified amount of
+   * elements.
+   *
+   * @param capacity the amount of elements to reserve memory for.
+   *
+   * @since 0.1.0
+   */
+  void reserve(size_type capacity)
+  {
+    m_data.reserve(capacity);
+  }
+
+  /**
+   * @brief Inserts a key/value-pair in-place.
+   *
+   * @details This function will replace any previous existing key/value-pair
+   * that uses the specified key.
+   *
+   * @tparam Args the types of the arguments that will be forwarded.
+   *
+   * @param key the key that will be associated with the value.
+   * @param args the arguments that will be forwarded to the appropriate value
+   * constructor.
+   *
+   * @return a reference to the inserted key/value-pair.
+   *
+   * @since 0.1.0
+   */
+  template <typename... Args>
+  decltype(auto) emplace(const key_type& key, Args&&... args)
+  {
+    if (const auto it = find(key); it != m_data.end()) {
+      m_data.erase(it);
+    }
+    return m_data.emplace_back(key, std::forward<Args>(args)...);
+  }
+
+  /**
+   * @brief Removes the key/value-pair associated with the specified key.
+   *
+   * @param key the key of the pair that will be removed.
+   *
+   * @since 0.1.0
+   */
+  void erase(const key_type& key)
+  {
+    if (const auto it = find(key); it != m_data.end()) {
+      m_data.erase(it);
+    }
+  }
+
+  /**
+   * @brief Returns the value associated with the specified key.
+   *
+   * @param key the key associated with the desired value.
+   *
+   * @return the value associated with the specified key.
+   *
+   * @throws out_of_range if the key is not associated with a value.
+   *
+   * @since 0.1.0
+   */
+  [[nodiscard]] auto at(const key_type& key) -> mapped_type&
+  {
+    if (auto it = find(key); it != m_data.end()) {
+      return it->second;
+    } else {
+      throw std::out_of_range{"Couldn't find key in small_map!"};
+    }
+  }
+
+  /**
+   * @copydoc at(const key_type&)
+   */
+  [[nodiscard]] auto at(const key_type& key) const -> const mapped_type&
+  {
+    if (const auto it = find(key); it != m_data.end()) {
+      return it->second;
+    } else {
+      throw std::out_of_range{"Couldn't find key in small_map!"};
+    }
+  }
+
+  /**
+   * @copydoc at(const key_type&)
+   */
+  [[nodiscard]] auto operator[](const key_type& key) -> mapped_type&
+  {
+    return at(key);
+  }
+
+  /**
+   * @copydoc at(const key_type&)
+   */
+  [[nodiscard]] auto operator[](const key_type& key) const -> const mapped_type&
+  {
+    return at(key);
+  }
+
+  /**
+   * @brief Indicates whether or not the map contains the specified key.
+   *
+   * @param key the key to look for.
+   *
+   * @return `true` if the map contains the supplied key; `false` otherwise.
+   *
+   * @since 0.1.0
+   */
+  [[nodiscard]] auto contains(const key_type& key) const -> bool
+  {
+    return find(key) != m_data.end();
+  }
+
+  /**
+   * @brief Indicates whether or not the map is empty.
+   *
+   * @return `true` if the map is empty; `false` otherwise.
+   *
+   * @since 0.1.0
+   */
+  [[nodiscard]] auto empty() const noexcept -> bool
+  {
+    return m_data.empty();
+  }
+
+  /**
+   * @brief Returns the number of elements stored in the map.
+   *
+   * @return the number of elements stored in the map.
+   *
+   * @since 0.1.0
+   */
+  [[nodiscard]] auto size() const noexcept -> size_type
+  {
+    return m_data.size();
+  }
+
+  /**
+   * @brief Returns the capacity of the map.
+   *
+   * @details The capacity is the number of key/value-pairs the map can store
+   * before having to re-allocate memory.
+   *
+   * @return the current capacity of the map.
+   *
+   * @since 0.1.0
+   */
+  [[nodiscard]] auto capacity() const noexcept -> size_type
+  {
+    return m_data.capacity();
+  }
+
+  /**
+   * @brief Returns an iterator to the first element.
+   *
+   * @return an iterator to the first element.
+   *
+   * @since 0.1.0
+   */
+  [[nodiscard]] auto begin() noexcept -> iterator
+  {
+    return m_data.begin();
+  }
+
+  /**
+   * @copydoc begin()
+   */
+  [[nodiscard]] auto begin() const noexcept -> const_iterator
+  {
+    return m_data.begin();
+  }
+
+  /**
+   * @brief Returns an iterator one-past the last element.
+   *
+   * @return an iterator one-past the last element.
+   */
+  [[nodiscard]] auto end() noexcept -> iterator
+  {
+    return m_data.end();
+  }
+
+  /**
+   * @copydoc end()
+   */
+  [[nodiscard]] auto end() const noexcept -> const_iterator
+  {
+    return m_data.end();
+  }
+
+ private:
+  storage_type m_data;
+
+  [[nodiscard]] auto find(const key_type& key) -> iterator
+  {
+    return std::find_if(m_data.begin(), m_data.end(), [key](const auto& pair) {
+      return pair.first == key;
+    });
+  }
+
+  [[nodiscard]] auto find(const key_type& key) const -> const_iterator
+  {
+    return std::find_if(m_data.begin(), m_data.end(), [key](const auto& pair) {
+      return pair.first == key;
+    });
+  }
+};
+
+}  // namespace tactile
---
+++ test/unit/small_map_test.cpp
@@ -0,0 +1,102 @@
+#include "small_map.hpp"
+
+#include <catch.hpp>
+#include <string>
+#include <string_view>
+
+using namespace tactile;
+
+TEST_CASE("small_map defaults", "[small_map]")
+{
+  const small_map<int, int> map;
+  CHECK(map.capacity() == 0);
+  CHECK(map.size() == 0);  // NOLINT
+  CHECK(map.empty());
+}
+
+TEST_CASE("small_map::emplace", "[small_map]")
+{
+  using namespace std::string_view_literals;
+
+  small_map<int, std::string> map;
+  const auto i = 7;
+
+  SECTION("Emplacing one string")
+  {
+    auto& [key, value] = map.emplace(i, "hello");
+    CHECK(key == i);
+    CHECK(value == "hello"sv);
+  }
+
+  SECTION("Overwriting the string with the same key")
+  {
+    auto& [key, value] = map.emplace(i, "world");
+    CHECK(map.size() == 1);
+    CHECK(key == i);
+    CHECK(value == "world"sv);
+  }
+}
+
+TEST_CASE("small_map::erase", "[small_map]")
+{
+  small_map<int, std::string> map;
+  CHECK_NOTHROW(map.erase(0));
+
+  map.emplace(0, "foo");
+  map.emplace(1, "bar");
+  CHECK(map.contains(0));
+  CHECK(map.contains(1));
+  CHECK(!map.empty());
+
+  map.erase(0);
+  CHECK(!map.contains(0));
+  CHECK(map.contains(1));
+  CHECK(!map.empty());
+
+  map.erase(1);
+  CHECK(!map.contains(1));
+  CHECK(map.empty());
+}
+
+TEST_CASE("small_map::reserve", "[small_map]")
+{
+  small_map<int, int> map;
+  const auto n = 10;
+
+  map.reserve(n);
+  CHECK(map.capacity() == n);
+}
+
+TEST_CASE("small_map::at", "[small_map]")
+{
+  using namespace std::string_view_literals;
+
+  small_map<int, std::string> map;
+  CHECK_THROWS_AS(map.at(0), std::out_of_range);
+
+  map.emplace(0, "foo");
+  const auto& str = map.at(0);
+  CHECK(str == "foo"sv);
+}
+
+TEST_CASE("small_map::operator[]", "[small_map]")
+{
+  using namespace std::string_view_literals;
+
+  small_map<int, std::string> map;
+  CHECK_THROWS_AS(map[0], std::out_of_range);
+
+  map.emplace(0, "foo");
+  const auto& str = map[0];
+  CHECK(str == "foo"sv);
+}
+
+TEST_CASE("small_map begin/end", "[small_map]")
+{
+  small_map<int, std::string> map;
+  map.emplace(1, "abc");
+  map.emplace(2, "def");
+  map.emplace(3, "ghi");
+
+  CHECK(std::distance(map.begin(), map.end()) == 3);
+}
