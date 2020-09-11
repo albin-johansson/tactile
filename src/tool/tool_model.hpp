c3e7d380dd708d7cf0f8df1d0075699a09e6286b
Albin Johansson <albinoscar99@hotmail.com>
2020-09-11 17:09:48 +0200

    Update tool_model.hpp

--- src/tool/tool_model.hpp
+++ src/tool/tool_model.hpp
@@ -10,18 +10,74 @@ namespace tactile {
 class tool_model final
 {
  public:
+  /**
+   * @brief Creates a `tool_model` instance.
+   *
+   * @param model a pointer to the associated model, cannot be null.
+   *
+   * @throws tactile_error if the supplied pointer is null.
+   *
+   * @since 0.1.0
+   */
   explicit tool_model(core::model* model);
 
+  /**
+   * @brief Selects the specified tool.
+   *
+   * @param tool the ID associated with the tool that will be enabled.
+   *
+   * @since 0.1.0
+   */
   void select(tool_id tool);
 
+  /**
+   * @brief Handles a mouse pressed event.
+   *
+   * @param event the associated event.
+   * @param mapPosition the current position of the map.
+   *
+   * @since 0.1.0
+   */
   void pressed(QMouseEvent* event, const QPointF& mapPosition);
 
+  /**
+   * @brief Handles a mouse moved event.
+   *
+   * @param event the associated event.
+   * @param mapPosition the current position of the map.
+   *
+   * @since 0.1.0
+   */
   void moved(QMouseEvent* event, const QPointF& mapPosition);
 
+  /**
+   * @brief Handles a mouse released event.
+   *
+   * @param event the associated event.
+   * @param mapPosition the current position of the map.
+   *
+   * @since 0.1.0
+   */
   void released(QMouseEvent* event, const QPointF& mapPosition);
 
+  /**
+   * @brief Handles an "entered" event, which is when the mouse enters the map
+   * widget.
+   *
+   * @param event the associated event.
+   *
+   * @since 0.1.0
+   */
   void entered(QEvent* event);
 
+  /**
+   * @brief Handles an "exited" event, which is when the mouse exits the map
+   * widget.
+   *
+   * @param event the associated event.
+   *
+   * @since 0.1.0
+   */
   void exited(QEvent* event);
 
  private:
@@ -29,6 +85,17 @@ class tool_model final
   std::unique_ptr<abstract_tool> m_stamp;
   std::unique_ptr<abstract_tool> m_eraser;
 
+  /**
+   * @brief Switches to the specified tool as the active tool.
+   *
+   * @note This function has no effect if the supplied tool is already the
+   * active tool.
+   *
+   * @param tool a pointer to the tool that will be enabled; `nullptr` if no
+   * tool should be enabled.
+   *
+   * @since 0.1.0
+   */
   void switch_to(abstract_tool* tool);
 };
 
