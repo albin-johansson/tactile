4d9ab7c49671bfe905759b9ac645510ef1a061e7
Albin Johansson <albinoscar99@hotmail.com>
2020-10-06 16:37:36 +0200

    Update save_json.hpp

--- src/service/saves/save_json.hpp
+++ src/service/saves/save_json.hpp
@@ -7,8 +7,17 @@
 
 namespace tactile::service {
 
+/**
+ * @brief Saves a map using the Tiled JSON format.
+ *
+ * @param path the destination path of of the map file.
+ * @param map the map that will be saved.
+ * @param tilesets the tilesets that are being used.
+ *
+ * @since 0.1.0
+ */
 void save_json(const QString& path,
                const core::map& map,
                const core::tileset_manager& tilesets);
 
-}
+}  // namespace tactile::service
