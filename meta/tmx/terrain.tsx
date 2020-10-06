90aaf539e5a6e2cacef3c4df2b9ed489a12ac699
Albin Johansson <albinoscar99@hotmail.com>
2020-10-06 18:09:18 +0200

    Add TMX meta files

---
+++ meta/tmx/terrain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0"?>
+<tileset version="1.4" tiledversion="1.4.2" name="terrain" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+	<image source="../terrain.png" width="1024" height="1024" />
+</tileset>
