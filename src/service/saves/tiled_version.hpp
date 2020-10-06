8b0a0787766f65401ab6b7b254bd88fc0158211e
Albin Johansson <albinoscar99@hotmail.com>
2020-10-06 17:20:37 +0200

    Update tiled_version.hpp

--- src/service/saves/tiled_version.hpp
+++ src/service/saves/tiled_version.hpp
@@ -1,10 +1,10 @@
 #pragma once
 
-#define TACTILE_TILED_VERSION "1.4.2"
 #define TACTILE_TILED_VERSION_LITERAL QStringLiteral(u"1.4.2")
 
 namespace tactile {
 
+inline constexpr auto g_tiledVersion = "1.4.2";
 inline constexpr double g_tiledJsonVersion = 1.4;
 
 }
