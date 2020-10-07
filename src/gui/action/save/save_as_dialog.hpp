32ca87a7c2eddf7f0427381ca5a0a06476434f21
Albin Johansson <albinoscar99@hotmail.com>
2020-10-07 18:18:17 +0200

    Update save_as_dialog.hpp

--- src/gui/action/save/save_as_dialog.hpp
+++ src/gui/action/save/save_as_dialog.hpp
@@ -9,19 +9,19 @@
 namespace tactile::gui {
 
 template <typename T>
-concept save_as_dialog_callback = std::invocable<T, const QString&>;
+concept SaveAsDialogCallback = std::invocable<T, const QString&>;
 
 class save_as_dialog final : public QFileDialog
 {
  public:
   explicit save_as_dialog(const QString& fileName, QWidget* parent = nullptr);
 
-  template <save_as_dialog_callback T>
+  template <SaveAsDialogCallback T>
   static void spawn(T&& callback, const QString& fileName)
   {
     save_as_dialog dialog{fileName};
     if (dialog.exec()) {
-      callback(dialog.selectedFiles().at(0));
+      callback(dialog.selectedFiles().first());
     }
   }
 };
