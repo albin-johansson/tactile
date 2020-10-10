fd9c465a6c1227ac53c9f7e4292f1824e19d1c20
Albin Johansson <albinoscar99@hotmail.com>
2020-10-11 01:24:21 +0200

    Update tileset_manager_test.cpp

--- test/unit/tileset_manager_test.cpp
+++ test/unit/tileset_manager_test.cpp
@@ -6,10 +6,18 @@
 #include "tileset.hpp"
 #include "types.hpp"
 
-using tactile::tileset_id;
-using tactile::operator""_tw;
-using tactile::operator""_th;
+using namespace tactile;
 using namespace tactile::core;
+
+namespace {
+
+[[nodiscard]] auto make_tileset(tileset_manager& manager)
+{
+  return std::make_shared<tileset>(
+      manager.next_global_tile_id(), "terrain.png", 32_tw, 32_th);
+}
+
+}  // namespace
 
 TEST_CASE("tileset_manager::emplace", "[tileset_manager]")
 {
@@ -18,11 +26,13 @@ TEST_CASE("tileset_manager::emplace", "[tileset_manager]")
   CHECK(manager.sheets() == 0);
   CHECK(!manager.has_active_tileset());
 
-  const auto first = manager.emplace("terrain.png", 32_tw, 32_th);
-  const auto second = manager.emplace("terrain.png", 32_tw, 32_th);
+  const auto fst = manager.next_tileset_id();
+  manager.add(fst, make_tileset(manager));
 
-  CHECK(second > first);
+  const auto snd = manager.next_tileset_id();
+  manager.add(snd, make_tileset(manager));
 
+  CHECK(snd > fst);
   CHECK(manager.sheets() == 2);
 }
 
@@ -31,7 +41,9 @@ TEST_CASE("tileset_manager::remove", "[tileset_manager]")
   tileset_manager manager;
   CHECK_NOTHROW(manager.remove(tileset_id{0}));
 
-  const auto id = manager.emplace("terrain.png", 32_tw, 32_th);
+  const auto id = 0_ts;
+  manager.add(id, std::make_shared<tileset>(0_t, "terrain.png", 32_tw, 32_th));
+
   CHECK(manager.sheets() == 1);
 
   manager.remove(id + tileset_id{5});
@@ -46,8 +58,8 @@ TEST_CASE("tileset_manager::remove_all", "[tileset_manager]")
   tileset_manager manager;
   CHECK_NOTHROW(manager.remove_all());
 
-  const auto a = manager.emplace("terrain.png", 32_tw, 32_th);
-  const auto b = manager.emplace("terrain.png", 32_tw, 32_th);
+  manager.add(manager.next_tileset_id(), make_tileset(manager));
+  manager.add(manager.next_tileset_id(), make_tileset(manager));
 
   CHECK(manager.sheets() == 2);
 
@@ -61,8 +73,8 @@ TEST_CASE("tileset_manager::select", "[tileset_manager]")
 
   CHECK(!manager.has_active_tileset());
 
-  const auto id = manager.emplace("terrain.png", 32_tw, 32_th);
-  manager.emplace("terrain.png", 32_tw, 32_th);
+  const auto id = manager.next_tileset_id();
+  manager.add(id, make_tileset(manager));
 
   manager.select(id);
   CHECK(manager.has_active_tileset());
