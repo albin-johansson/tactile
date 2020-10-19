08f1a930076494df5008aa6c74c0b8e2677d61a0
Albin Johansson <albinoscar99@hotmail.com>
2020-10-19 13:07:09 +0200

    Update add_row.cpp

--- src/command/add_row.cpp
+++ src/command/add_row.cpp
@@ -17,14 +17,12 @@ add_row::add_row(core::map* map)
 void add_row::undo()
 {
   QUndoCommand::undo();
-
   invoke_n(amount(), [this] { m_map->remove_row(); });
 }
 
 void add_row::redo()
 {
   QUndoCommand::redo();
-
   invoke_n(amount(), [this] { m_map->add_row(empty); });
 }
 
