1e47382f04c0cc1d5366d179818095014ef4a67f
Albin Johansson <albinoscar99@hotmail.com>
2020-10-23 22:48:22 +0200

    Update signal_blocker.hpp

--- src/util/signal_blocker.hpp
+++ src/util/signal_blocker.hpp
@@ -4,15 +4,42 @@
 
 namespace tactile {
 
+/**
+ * @class signal_blocker
+ *
+ * @brief Used to disable all signals from a `QObject` during the enclosing
+ * scope.
+ *
+ * @since 0.1.0
+ *
+ * @headerfile signal_blocker.hpp
+ */
 class signal_blocker final
 {
  public:
+  // clang-format off
+  /**
+   * @brief Creates a signal blocker instance.
+   *
+   * @pre 'object' cannot be null.
+   *
+   * @param object the object to disable the signals for.
+   *
+   * @since 0.1.0
+   */
+  [[nodiscard("Must stay alive during the entire scope!")]]
   explicit signal_blocker(QObject* object) noexcept : m_object{object}
   {
     Q_ASSERT(m_object);
     m_object->blockSignals(true);
   }
+  // clang-format on
 
+  /**
+   * @brief Restores the signals from the associated object.
+   *
+   * @since 0.1.0
+   */
   ~signal_blocker() noexcept
   {
     m_object->blockSignals(false);
