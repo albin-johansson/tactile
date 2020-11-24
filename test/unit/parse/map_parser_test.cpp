1f8d2eb32e468a2e2c9f15620bea6874c121b476
Albin Johansson <albinoscar99@hotmail.com>
2020-11-24 20:32:12 +0100

    Update map_parser_test.cpp

--- test/unit/parse/map_parser_test.cpp
+++ test/unit/parse/map_parser_test.cpp
@@ -4,26 +4,25 @@
 
 using namespace tactile;
 
-using xtmx_parser = tmx::map_parser<tmx::xml_engine>;
+using xml_parser = tmx::map_parser<tmx::xml_engine>;
 
 static_assert(tmx::is_object<tmx::tmx_element>);
 static_assert(tmx::is_object<tmx::json_element>);
 
-static_assert(
-    tmx::is_parser<tmx::xml_engine, QDomDocument, tmx::tmx_element>);
+static_assert(tmx::is_parser<tmx::xml_engine, QDomDocument, tmx::tmx_element>);
 
 TEST_CASE("foo", "[map_parser]")
 {
   SECTION("Valid map")
   {
-    xtmx_parser parser{QFileInfo{"tmx/embedded.tmx"}};
+    xml_parser parser{QFileInfo{"tmx/embedded.tmx"}};
     CHECK(parser);
     CHECK(parser.error_code() == parse_error::none);
   }
 
   SECTION("Invalid map")
   {
-    xtmx_parser parser{QFileInfo{"tmx/doesnotexist.tmx"}};
+    xml_parser parser{QFileInfo{"tmx/does_not_exist.tmx"}};
     CHECK(!parser);
     CHECK(parser.error_code() == parse_error::map_file_not_found);
   }
