516bf4f5c732e5bce95a0e5c0adef6c225e83ad4
Albin Johansson <albinoscar99@hotmail.com>
2020-12-01 09:15:25 +0100

    Update properties_widget.cpp

--- src/gui/properties/properties_widget.cpp
+++ src/gui/properties/properties_widget.cpp
@@ -35,34 +35,34 @@ void properties_widget::selected_map(const core::map_document& document)
 
   auto* documentItem = new QTreeWidgetItem{m_ui->treeWidget};
   documentItem->setIcon(nameColumn, icons::collapsed());
-  documentItem->setText(nameColumn, QStringLiteral(u"Document"));
+  documentItem->setText(nameColumn, TACTILE_QSTRING(u"Document"));
   documentItem->setFirstColumnSpanned(true);
 
   auto* rows = new QTreeWidgetItem{documentItem};
-  rows->setText(nameColumn, QStringLiteral(u"Rows"));
+  rows->setText(nameColumn, TACTILE_QSTRING(u"Rows"));
   rows->setText(valueColumn, QString::number(document.row_count().get()));
   rows->setDisabled(true);
 
   auto* cols = new QTreeWidgetItem{documentItem};
-  cols->setText(nameColumn, QStringLiteral(u"Columns"));
+  cols->setText(nameColumn, TACTILE_QSTRING(u"Columns"));
   cols->setText(valueColumn, QString::number(document.col_count().get()));
   cols->setDisabled(true);
 
   auto* tileWidth = new QTreeWidgetItem{documentItem};
-  tileWidth->setText(nameColumn, QStringLiteral(u"Tile width"));
+  tileWidth->setText(nameColumn, TACTILE_QSTRING(u"Tile width"));
   tileWidth->setText(valueColumn,
                      QString::number(prefs::saves::tile_width().value()));
   tileWidth->setDisabled(true);
 
   auto* tileHeight = new QTreeWidgetItem{documentItem};
-  tileHeight->setText(nameColumn, QStringLiteral(u"Tile height"));
+  tileHeight->setText(nameColumn, TACTILE_QSTRING(u"Tile height"));
   tileHeight->setText(valueColumn,
                       QString::number(prefs::saves::tile_height().value()));
   tileHeight->setDisabled(true);
 
   auto* custom = new QTreeWidgetItem{m_ui->treeWidget};
   custom->setIcon(nameColumn, icons::collapsed());
-  custom->setText(nameColumn, QStringLiteral(u"Custom"));
+  custom->setText(nameColumn, TACTILE_QSTRING(u"Custom"));
   custom->setFirstColumnSpanned(true);
 }
 
