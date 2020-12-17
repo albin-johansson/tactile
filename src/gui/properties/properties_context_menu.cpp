dba34eb341e0d2c2efca023cc1ce7fb485c79afa
Albin Johansson <albinoscar99@hotmail.com>
2020-12-17 22:15:19 +0100

    Add properties_context_menu

---
+++ src/gui/properties/properties_context_menu.cpp
@@ -0,0 +1,136 @@
+#include "properties_context_menu.hpp"
+
+#include "icons.hpp"
+#include "property_tree_item.hpp"
+
+namespace tactile::gui {
+
+properties_context_menu::properties_context_menu(QWidget* parent)
+    : QMenu{parent}
+    , m_add{addAction(icons::add(), tr("Add property..."))}
+    , m_sep0{addSeparator()}
+    , m_rename{addAction(icons::rename(), tr("Rename"))}
+    , m_sep1{addSeparator()}
+    , m_copy{addAction(icons::copy(), tr("Copy"))}
+    , m_paste{addAction(icons::paste(), tr("Paste"))}
+    , m_duplicate{addAction(icons::duplicate(), tr("Duplicate"))}
+    , m_sep2{addSeparator()}
+    , m_moveUp{addAction(icons::move_up(), tr("Move up"))}
+    , m_moveDown{addAction(icons::move_down(), tr("Move down"))}
+    , m_sep3{addSeparator()}
+    , m_changeType{addMenu(icons::object(), tr("Change type"))}
+    , m_sep4{addSeparator()}
+    , m_remove{addAction(icons::remove(), tr("Remove"))}
+{
+  m_typeString = m_changeType->addAction(tr("string"));
+  m_typeInt = m_changeType->addAction(tr("int"));
+  m_typeFloat = m_changeType->addAction(tr("float"));
+  m_typeBool = m_changeType->addAction(tr("bool"));
+  m_typeObject = m_changeType->addAction(tr("object"));
+  m_typeColor = m_changeType->addAction(tr("color"));
+  m_typeFile = m_changeType->addAction(tr("file"));
+
+  connect(m_add, &QAction::triggered, this, &properties_context_menu::add);
+  connect(m_remove,
+          &QAction::triggered,
+          this,
+          &properties_context_menu::remove);
+  connect(m_rename,
+          &QAction::triggered,
+          this,
+          &properties_context_menu::rename);
+  connect(m_moveUp,
+          &QAction::triggered,
+          this,
+          &properties_context_menu::move_up);
+  connect(m_moveDown,
+          &QAction::triggered,
+          this,
+          &properties_context_menu::move_down);
+  connect(m_duplicate,
+          &QAction::triggered,
+          this,
+          &properties_context_menu::duplicate);
+  connect(m_copy, &QAction::triggered, this, &properties_context_menu::copy);
+  connect(m_paste, &QAction::triggered, this, &properties_context_menu::paste);
+  // TODO change_type
+}
+
+void properties_context_menu::set_selected_item(const property_tree_item* item)
+{
+  m_copy->setVisible(item);
+  m_copy->setEnabled(item);
+  m_paste->setEnabled(!item);
+  m_changeType->setEnabled(item);
+
+  if (item) {
+    for (auto* elem : m_changeType->actions()) {
+      if (!elem->isSeparator()) {
+        elem->setEnabled(true);
+      }
+    }
+
+    switch (item->property_type()) {
+      case core::property::string: {
+        m_typeString->setEnabled(false);
+        break;
+      }
+      case core::property::integer: {
+        m_typeInt->setEnabled(false);
+        break;
+      }
+      case core::property::floating: {
+        m_typeFloat->setEnabled(false);
+        break;
+      }
+      case core::property::boolean: {
+        m_typeBool->setEnabled(false);
+        break;
+      }
+      case core::property::file: {
+        m_typeFile->setEnabled(false);
+        break;
+      }
+      case core::property::color: {
+        m_typeColor->setEnabled(false);
+        break;
+      }
+      case core::property::object: {
+        m_typeObject->setEnabled(false);
+        break;
+      }
+    }
+  }
+}
+
+void properties_context_menu::set_up_enabled(const bool enabled)
+{
+  m_moveUp->setEnabled(enabled);
+}
+
+void properties_context_menu::set_down_enabled(const bool enabled)
+{
+  m_moveDown->setEnabled(enabled);
+}
+
+void properties_context_menu::set_remove_enabled(const bool enabled)
+{
+  m_remove->setEnabled(enabled);
+}
+
+void properties_context_menu::set_duplicate_enabled(const bool enabled)
+{
+  m_duplicate->setEnabled(enabled);
+}
+
+void properties_context_menu::set_rename_enabled(const bool enabled)
+{
+  m_rename->setEnabled(enabled);
+}
+
+void properties_context_menu::set_change_type_enabled(const bool enabled)
+{
+  m_changeType->setEnabled(enabled);
+}
+
+}  // namespace tactile::gui
---
+++ src/gui/properties/properties_context_menu.hpp
@@ -0,0 +1,68 @@
+#pragma once
+
+#include <QMenu>
+
+#include "property.hpp"
+
+namespace tactile::gui {
+
+class property_tree_item;
+
+class properties_context_menu final : public QMenu
+{
+  Q_OBJECT
+
+ public:
+  explicit properties_context_menu(QWidget* parent = nullptr);
+
+  void set_selected_item(const property_tree_item* item);
+
+  void set_up_enabled(bool enabled);
+
+  void set_down_enabled(bool enabled);
+
+  void set_remove_enabled(bool enabled);
+
+  void set_duplicate_enabled(bool enabled);
+
+  void set_rename_enabled(bool enabled);
+
+  void set_change_type_enabled(bool enabled);
+
+ signals:
+  void add();
+  void remove();
+  void rename();
+  void move_up();
+  void move_down();
+  void duplicate();
+  void change_type(core::property::type type);
+  void copy();
+  void paste();
+
+ private:
+  QAction* m_add{};
+  [[maybe_unused]] QAction* m_sep0;
+  QAction* m_rename{};
+  QAction* m_duplicate{};
+  [[maybe_unused]] QAction* m_sep1;
+  QAction* m_copy{};
+  QAction* m_paste{};
+  [[maybe_unused]] QAction* m_sep2;
+  QAction* m_moveUp{};
+  QAction* m_moveDown{};
+  [[maybe_unused]] QAction* m_sep3;
+  QMenu* m_changeType{};
+  [[maybe_unused]] QAction* m_sep4;
+  QAction* m_remove{};
+
+  QAction* m_typeString{};
+  QAction* m_typeInt{};
+  QAction* m_typeFloat{};
+  QAction* m_typeBool{};
+  QAction* m_typeObject{};
+  QAction* m_typeColor{};
+  QAction* m_typeFile{};
+};
+
+}  // namespace tactile::gui
