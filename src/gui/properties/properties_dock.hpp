42a0cd6b719b5ae6b25f890a49bc0d162f1ad6d9
Albin Johansson <albinoscar99@hotmail.com>
2020-12-19 23:20:20 +0100

    Update properties_dock

--- src/gui/properties/properties_dock.hpp
+++ src/gui/properties/properties_dock.hpp
@@ -2,6 +2,7 @@
 
 #include "dock_widget.hpp"
 #include "map_document.hpp"
+#include "property.hpp"
 
 namespace tactile::gui {
 
@@ -14,8 +15,24 @@ class properties_dock final : public dock_widget
  public:
   explicit properties_dock(QWidget* parent = nullptr);
 
+ signals:
+  void ui_add_property(const QString& name, core::property::type type);
+  void ui_remove_property(const QString& name);
+  void ui_update_property(const QString& name, const core::property& property);
+  void ui_rename_property(const QString& oldName, const QString& newName);
+
  public slots:
   void selected_map(const core::map_document& document);
+
+  void added_property(const QString& name, const core::property& property);
+
+  void removed_property(const QString& name);
+
+  void moved_property_up(const QString& name);
+
+  void moved_property_down(const QString& name);
+
+  void duplicated_property(const QString& name);
 
  private:
   properties_widget* m_widget{};
