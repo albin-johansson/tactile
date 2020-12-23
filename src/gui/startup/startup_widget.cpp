af791e9b1f76a56a38e7378629b0beff321d0c1c
Albin Johansson <albinoscar99@hotmail.com>
2020-12-23 23:48:32 +0100

    Improve startup_widget

--- src/gui/startup/startup_widget.cpp
+++ src/gui/startup/startup_widget.cpp
@@ -8,7 +8,13 @@ namespace tactile::gui {
 startup_widget::startup_widget(QWidget* parent)
     : QWidget{parent}
     , m_ui{init_ui<Ui::startup_widget>(this)}
-{}
+{
+  const QKeySequence newDocument{QKeySequence::New};
+  m_ui->newShortcutLabel->setText(newDocument.toString());
+
+  const QKeySequence openDocument{QKeySequence::Open};
+  m_ui->openShortcutLabel->setText(openDocument.toString());
+}
 
 startup_widget::~startup_widget() noexcept
 {
