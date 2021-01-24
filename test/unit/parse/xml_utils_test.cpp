099070fbea369349f4627012eec7843b3e3cdfdd
Albin Johansson <albinoscar99@hotmail.com>
2021-01-24 19:02:04 +0100

    Add xml_utils_test

--- test/unit/parse/xml_utils_test.cpp
+++ test/unit/parse/xml_utils_test.cpp
@@ -1,88 +1,72 @@
 #include "xml_utils.hpp"
 
-#include <catch.hpp>
+#include <gtest/gtest.h>
 
+#include "tactile_qstring.hpp"
 #include "tile_id.hpp"
 
 using namespace tactile;
 
-TEST_CASE("xml::int_attr", "[xml_utils]")
-{
-  SECTION("Without default value")
-  {
-    QDomDocument document;
-    auto elem = document.createElement(QStringLiteral(u"foo"));
-
-    CHECK(xml::int_attr(elem, QStringLiteral(u"random")) == std::nullopt);
-
-    const auto name = QStringLiteral(u"kenobi");
-    const auto value = 1'337;
-
-    elem.setAttribute(name, value);
-    CHECK(xml::int_attr(elem, name) == value);
-
-    SECTION("Convert to strong integer type")
-    {
-      CHECK(xml::int_attr<tile_id>(elem, name) == tile_id{value});
-    }
-  }
-
-  SECTION("With default value")
-  {
-    QDomDocument document;
-    auto elem = document.createElement(QStringLiteral(u"foo"));
-
-    CHECK_NOTHROW(xml::int_attr(elem, QStringLiteral(u"random"), 0));
-
-    const auto def = 8912;
-    CHECK(xml::int_attr(elem, QStringLiteral(u"xyz"), def) == def);
-
-    SECTION("Using strong integer type")
-    {
-      CHECK(xml::int_attr<tile_id>(elem, QStringLiteral(u"xyz"), 10_t) == 10_t);
-    }
-  }
-}
-
-TEST_CASE("xml::double_attr", "[xml_utils]")
+TEST(XmlUtils, IntAttr)
 {
   QDomDocument document;
-  auto elem = document.createElement(QStringLiteral(u"foo"));
+  auto elem = document.createElement(TACTILE_QSTRING(u"foo"));
 
-  CHECK_NOTHROW(xml::double_attr(elem, QStringLiteral(u"random"), 0.0));
+  EXPECT_FALSE(xml::int_attr(elem, TACTILE_QSTRING(u"bar")));
 
-  const auto def = 27.6;
-  CHECK(xml::double_attr(elem, QStringLiteral(u"xyz"), def) == def);
+  const auto name = TACTILE_QSTRING(u"Gandalf");
+  const auto value = 1'337;
 
-  SECTION("Using strong double type")
-  {
-    using mana = nenya::mirror_type<double, struct mana_t>;
-    const mana value{9.2};
-    CHECK(xml::double_attr<mana>(elem, QStringLiteral(u"xyz"), value) == value);
-  }
+  elem.setAttribute(name, value);
+  EXPECT_EQ(value, xml::int_attr(elem, name));
+  EXPECT_EQ(tile_id{value}, xml::int_attr<tile_id>(elem, name));
 }
 
-TEST_CASE("xml::each_elem", "[xml_utils]")
+TEST(XmlUtils, IntAttrDefaultValue)
+{
+  QDomDocument document;
+  auto elem = document.createElement(TACTILE_QSTRING(u"foo"));
+
+  EXPECT_NO_THROW(xml::int_attr(elem, TACTILE_QSTRING(u"random"), 0));
+
+  const auto value = 8'912;
+  EXPECT_EQ(value, xml::int_attr(elem, TACTILE_QSTRING(u"xyz"), value));
+  EXPECT_EQ(10_t, xml::int_attr<tile_id>(elem, TACTILE_QSTRING(u"xyz"), 10_t));
+}
+
+TEST(XmlUtils, DoubleAttr)
+{
+  QDomDocument document;
+  auto elem = document.createElement(TACTILE_QSTRING(u"foo"));
+
+  EXPECT_NO_THROW(xml::double_attr(elem, TACTILE_QSTRING(u"random"), 0.0));
+
+  const auto value = 27.6;
+  EXPECT_EQ(value, xml::double_attr(elem, TACTILE_QSTRING(u"xyz"), value));
+
+  using mana_type = nenya::mirror_type<double, struct mana_t>;
+  const mana_type mana{9.2};
+  EXPECT_EQ(mana,
+            xml::double_attr<mana_type>(elem, TACTILE_QSTRING(u"xyz"), mana));
+}
+
+TEST(XmlUtils, EachElem)
 {
   QDomDocument document{};
-  auto root = document.createElement(QStringLiteral(u"root"));
 
-  root.appendChild(document.createElement(QStringLiteral(u"foo")));
-  root.appendChild(document.createElement(QStringLiteral(u"foo")));
-  root.appendChild(document.createElement(QStringLiteral(u"foo")));
+  auto root = document.createElement(TACTILE_QSTRING(u"root"));
+
+  const auto tag = TACTILE_QSTRING(u"foo");
+  root.appendChild(document.createElement(tag));
+  root.appendChild(document.createElement(tag));
+  root.appendChild(document.createElement(tag));
 
   document.appendChild(root);
 
-  int count{0};
-  xml::each_elem(root, QStringLiteral(u"foo"), [&](const QDomNode& node) {
+  auto count = 0;
+  xml::each_elem(root, tag, [&](const QDomNode&) {
     ++count;
   });
 
-  CHECK(count == 3);
-}
-
-TEST_CASE("xml::from_file", "[xml_utils]")
-{
-  CHECK_NOTHROW(xml::from_file(QStringLiteral(u"sample.xml")));
-  CHECK(xml::from_file(QStringLiteral(u"not_a_file.xml")) == std::nullopt);
+  EXPECT_EQ(3, count);
 }
