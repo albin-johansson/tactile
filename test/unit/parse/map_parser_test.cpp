3440901bc96c933d82741249dff7ea481b6df634
Albin Johansson <albinoscar99@hotmail.com>
2021-01-24 02:08:23 +0100

    Update map_parser_test.cpp

--- test/unit/parse/map_parser_test.cpp
+++ test/unit/parse/map_parser_test.cpp
@@ -1,6 +1,6 @@
 #include "map_parser.hpp"
 
-#include <catch.hpp>
+#include <gtest/gtest.h>
 
 #include "json_element.hpp"
 #include "json_engine.hpp"
@@ -15,40 +15,36 @@ using json_parser = tmx::map_parser<tmx::json_engine>;
 static_assert(tmx::is_object<tmx::xml_element>);
 static_assert(tmx::is_object<tmx::json_element>);
 
+// clang-format off
 static_assert(tmx::is_parser<tmx::xml_engine, QDomDocument, tmx::xml_element>);
-static_assert(
-    tmx::is_parser<tmx::json_engine, QJsonDocument, tmx::json_element>);
+static_assert(tmx::is_parser<tmx::json_engine, QJsonDocument, tmx::json_element>);
+// clang-format on
 
-TEST_CASE("tmx_parser", "[map_parser]")
+TEST(TMXParser, ValidMap)
 {
-  SECTION("Valid map")
-  {
-    xml_parser parser{QFileInfo{"tmx/embedded.tmx"}};
-    CHECK(parser);
-    CHECK(parser.error_code() == tmx::parse_error::none);
-  }
-
-  SECTION("Invalid map")
-  {
-    xml_parser parser{QFileInfo{"tmx/does_not_exist.tmx"}};
-    CHECK(!parser);
-    CHECK(parser.error_code() == tmx::parse_error::map_file_not_found);
-  }
+  const xml_parser parser{TACTILE_QSTRING(u"tmx/embedded.tmx")};
+  EXPECT_TRUE(parser);
+  EXPECT_EQ(tmx::parse_error::none, parser.error_code());
 }
 
-TEST_CASE("json_parser", "[map_parser]")
+TEST(TMXParser, InvalidMap)
 {
-  SECTION("Valid map")
-  {
-    json_parser parser{QFileInfo{"json/external.json"}};
-    CHECK(parser);
-    CHECK(parser.error_code() == tmx::parse_error::none);
-  }
+  const xml_parser parser{TACTILE_QSTRING(u"tmx/does_not_exist.tmx")};
+  EXPECT_FALSE(parser);
+  EXPECT_EQ(tmx::parse_error::map_file_not_found, parser.error_code());
+}
 
-  SECTION("Invalid map")
-  {
-    json_parser parser{QFileInfo{"json/invalid/layer_missing_height.json"}};
-    CHECK(!parser);
-    CHECK(parser.error_code() == tmx::parse_error::layer_missing_height);
-  }
+TEST(JSONParser, ValidMap)
+{
+  const json_parser parser{TACTILE_QSTRING(u"json/external.json")};
+  EXPECT_TRUE(parser);
+  EXPECT_EQ(tmx::parse_error::none, parser.error_code());
+}
+
+TEST(JSONParser, InvalidMap)
+{
+  const auto path = TACTILE_QSTRING(u"json/invalid/layer_missing_height.json");
+  const json_parser parser{path};
+  EXPECT_FALSE(parser);
+  EXPECT_EQ(tmx::parse_error::layer_missing_height, parser.error_code());
 }
