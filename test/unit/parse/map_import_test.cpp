d1d32cf892f752e1ae93986b2121e15205992a87
Albin Johansson <albinoscar99@hotmail.com>
2021-01-25 00:46:46 +0100

    Update map_import_test.cpp

--- test/unit/parse/map_import_test.cpp
+++ test/unit/parse/map_import_test.cpp
@@ -8,6 +8,8 @@
  * state of the parsed document.
  */
 
+#include <QDebug>
+
 #include "gtest_macros.hpp"
 #include "open_map.hpp"
 #include "tactile_qstring.hpp"
@@ -24,20 +26,100 @@ TACTILE_DEFINE_TEST_P(MapImportEmbeddedTest, QString)
 
   document->setParent(&parent);  // Avoid memory leak
 
-  {
-    // Layers: GO: tile_layer, T0: tile_layer
-    constexpr auto rows = 10_row;
-    constexpr auto cols = 15_col;
-    constexpr auto tileWidth = 32_tw;
-    constexpr auto tileHeight = 32_th;
+  constexpr auto rows = 10_row;
+  constexpr auto cols = 15_col;
+  constexpr auto tileWidth = 32_tw;
+  constexpr auto tileHeight = 32_th;
 
+  EXPECT_EQ(rows, document->row_count());
+  EXPECT_EQ(cols, document->col_count());
+
+  {  // Layers
     ASSERT_EQ(3, document->layer_count());
+
+    ASSERT_TRUE(document->has_layer(1_layer));
+
+    {  // First layer
+      const auto* layer = document->get_layer(1_layer);
+      ASSERT_TRUE(layer);
+      ASSERT_EQ(core::layer_type::tile_layer, layer->type());
+
+      EXPECT_EQ(TACTILE_QSTRING(u"G0"), layer->name());
+      EXPECT_EQ(1.0, layer->opacity());
+      EXPECT_TRUE(layer->visible());
+
+      const auto* tileLayer = document->get_tile_layer(1_layer);
+      ASSERT_TRUE(tileLayer);
+
+      EXPECT_EQ(rows, tileLayer->row_count());
+      EXPECT_EQ(cols, tileLayer->col_count());
+    }
+
+    {  // Second layer
+      const auto* layer = document->get_layer(2_layer);
+      ASSERT_TRUE(layer);
+      ASSERT_EQ(core::layer_type::tile_layer, layer->type());
+
+      EXPECT_EQ(TACTILE_QSTRING(u"T0"), layer->name());
+      EXPECT_EQ(0.75, layer->opacity());
+      EXPECT_TRUE(layer->visible());
+
+      const auto* tileLayer = document->get_tile_layer(2_layer);
+      ASSERT_TRUE(tileLayer);
+
+      EXPECT_EQ(rows, tileLayer->row_count());
+      EXPECT_EQ(cols, tileLayer->col_count());
+    }
+
+    {  // Third layer
+      const auto* layer = document->get_layer(3_layer);
+      ASSERT_TRUE(layer);
+      ASSERT_EQ(core::layer_type::object_layer, layer->type());
+
+      EXPECT_EQ(TACTILE_QSTRING(u"O1"), layer->name());
+      EXPECT_EQ(1.0, layer->opacity());
+      EXPECT_TRUE(layer->visible());
+
+      // TODO document->get_object_layer()
+    }
   }
 
-  {  // Tilesets: terrain, outside
+  {  // Tilesets
     const auto* tilesets = document->tilesets();
     ASSERT_TRUE(tilesets);
     ASSERT_EQ(2, tilesets->count());
+
+    ASSERT_TRUE(tilesets->contains(1_ts));
+
+    const auto& terrain = tilesets->at(1_ts);
+    const QFileInfo terrainPath{TACTILE_QSTRING(u"terrain.png")};
+
+    ASSERT_EQ(TACTILE_QSTRING(u"terrain"), terrain.name());
+    EXPECT_EQ(32_col, terrain.col_count());
+    EXPECT_EQ(1_t, terrain.first_id());
+    EXPECT_EQ(terrainPath.absoluteFilePath(),
+              terrain.file().canonicalFilePath());
+    EXPECT_EQ(1'024, terrain.image().width());
+    EXPECT_EQ(1'024, terrain.image().height());
+    EXPECT_EQ(1'024, terrain.tile_count());
+    EXPECT_EQ(32_tw, terrain.get_tile_width());
+    EXPECT_EQ(32_th, terrain.get_tile_height());
+
+    ASSERT_TRUE(tilesets->contains(2_ts));
+
+    const auto& outside = tilesets->at(2_ts);
+    const QFileInfo outsidePath{TACTILE_QSTRING(u"outside.png")};
+
+    ASSERT_EQ(TACTILE_QSTRING(u"outside"), outside.name());
+    EXPECT_EQ(32_col, outside.col_count());
+    EXPECT_EQ(1'025_t, outside.first_id());
+    EXPECT_EQ(outsidePath.absoluteFilePath(),
+              outside.file().canonicalFilePath());
+    EXPECT_EQ(1'024, outside.image().width());
+    EXPECT_EQ(1'024, outside.image().height());
+    EXPECT_EQ(1'024, outside.tile_count());
+    EXPECT_EQ(32_tw, outside.get_tile_width());
+    EXPECT_EQ(32_th, outside.get_tile_height());
   }
 
   {  // Properties
@@ -50,6 +132,8 @@ TACTILE_DEFINE_TEST_P(MapImportEmbeddedTest, QString)
     const auto boolean = TACTILE_QSTRING(u"isCool");
     const auto object = TACTILE_QSTRING(u"obj");
     const auto file = TACTILE_QSTRING(u"path");
+
+    // TODO use document->has_property() when added
 
     EXPECT_NO_THROW(document->get_property(string));  // NOLINT
     EXPECT_EQ(TACTILE_QSTRING(u"foobar"),
