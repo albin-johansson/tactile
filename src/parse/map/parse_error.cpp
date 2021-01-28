1c6fa95b050a054901cf1f0f1de788a036e30774
Albin Johansson <albinoscar99@hotmail.com>
2021-01-28 12:19:33 +0100

    Update parse_error.cpp

--- src/parse/map/parse_error.cpp
+++ src/parse/map/parse_error.cpp
@@ -11,19 +11,19 @@ auto to_message(const parse_error error) -> QString
       return TACTILE_QSTRING(u"No error!");
 
     case parse_error::map_file_not_found:
-      return TACTILE_QSTRING(u"Did not find map file!");
+      return TACTILE_QSTRING(u"Didn't find map file!");
 
     case parse_error::could_not_parse_file:
-      return TACTILE_QSTRING(u"Could not parse file!");
+      return TACTILE_QSTRING(u"Couldn't parse file!");
 
     case parse_error::map_missing_next_layer_id:
-      return TACTILE_QSTRING(u"Map did not feature next layer ID property!");
+      return TACTILE_QSTRING(u"Map didn't feature next layer ID property!");
 
     case parse_error::map_missing_tilesets:
-      return TACTILE_QSTRING(u"Map did not feature tilesets property!");
+      return TACTILE_QSTRING(u"Map didn't feature tilesets property!");
 
     case parse_error::map_missing_layers:
-      return TACTILE_QSTRING(u"Map did not feature layers property!");
+      return TACTILE_QSTRING(u"Map didn't feature layers property!");
 
     case parse_error::layer_missing_width:
       return TACTILE_QSTRING(u"Layer object was missing width!");
@@ -38,37 +38,37 @@ auto to_message(const parse_error error) -> QString
       return TACTILE_QSTRING(u"Layer object was missing ID!");
 
     case parse_error::layer_could_not_parse_tile:
-      return TACTILE_QSTRING(u"Could not parse tile ID in layer!");
+      return TACTILE_QSTRING(u"Couldn't parse tile ID in layer!");
 
     case parse_error::tileset_missing_first_gid:
-      return TACTILE_QSTRING(u"Tileset did not feature first GID property!");
+      return TACTILE_QSTRING(u"Tileset didn't feature first GID property!");
 
     case parse_error::tileset_missing_tile_width:
-      return TACTILE_QSTRING(u"Tileset did not feature tile width property!");
+      return TACTILE_QSTRING(u"Tileset didn't feature tile width property!");
 
     case parse_error::tileset_missing_tile_height:
-      return TACTILE_QSTRING(u"Tileset did not feature tile height property!");
+      return TACTILE_QSTRING(u"Tileset didn't feature tile height property!");
 
     case parse_error::tileset_missing_image_path:
-      return TACTILE_QSTRING(u"Tileset did not feature image property!");
+      return TACTILE_QSTRING(u"Tileset didn't feature image property!");
 
     case parse_error::tileset_missing_name:
-      return TACTILE_QSTRING(u"Tileset did not feature name property!");
+      return TACTILE_QSTRING(u"Tileset didn't feature name property!");
 
     case parse_error::could_not_create_tileset:
-      return TACTILE_QSTRING(u"Could not create tileset object!");
+      return TACTILE_QSTRING(u"Couldn't create tileset object!");
 
     case parse_error::could_not_read_external_tileset:
-      return TACTILE_QSTRING(u"Could not read external tileset file!");
+      return TACTILE_QSTRING(u"Couldn't read external tileset file!");
 
     case parse_error::external_tileset_does_not_exist:
-      return TACTILE_QSTRING(u"External tileset file does not exist!");
+      return TACTILE_QSTRING(u"External tileset file doesn't exist!");
 
     case parse_error::unknown_property_type:
-      return TACTILE_QSTRING(u"Could not recognize property type!");
+      return TACTILE_QSTRING(u"Couldn't recognize property type!");
 
     case parse_error::unknown_layer_type:
-      return TACTILE_QSTRING(u"Could not recognize layer type!");
+      return TACTILE_QSTRING(u"Couldn't recognize layer type!");
 
     default:
       return TACTILE_QSTRING(u"Unknown!");
