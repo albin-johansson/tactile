43e544404d20e6800f0b229e7021973c77b8cf91
Albin Johansson <albinoscar99@hotmail.com>
2021-01-29 22:34:44 +0100

    Update layer_type.hpp

--- src/core/map/layer/layer_type.hpp
+++ src/core/map/layer/layer_type.hpp
@@ -2,10 +2,19 @@
 
 namespace tactile::core {
 
+/**
+ * \enum layer_type
+ *
+ * \brief Provides different values that represent the various tile types.
+ *
+ * \since 0.2.0
+ *
+ * \headerfile layer_type.hpp
+ */
 enum class layer_type
 {
-  tile_layer,
-  object_layer
+  tile_layer,   ///< A layer consisting of only tiles.
+  object_layer  ///< A layer consisting of map objects.
 };
 
-}
+}  // namespace tactile::core
