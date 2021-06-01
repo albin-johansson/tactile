865fd7508d3c3fd6acc8fba427ec226ca2d885ee
Albin Johansson <albinoscar99@hotmail.com>
2021-06-01 15:42:49 +0200

    Update color_utils.hpp

--- src/util/color_utils.hpp
+++ src/util/color_utils.hpp
@@ -4,10 +4,26 @@
 
 namespace tactile {
 
+/**
+ * \brief Returns the "intensity" of the specified color.
+ *
+ * \details This function is particularly useful to determine appropriate color
+ * of foreground text.
+ *
+ * \param color the color to check.
+ *
+ * \return the intensity of the color.
+ */
 [[nodiscard]] auto intensity_of(const QColor& color) -> double;
 
-// An intensity threshold used to determine whether or not black text is
-// required when using the color as a backdrop
+/**
+ * \brief Returns an intensity threshold used to determine whether or not black
+ * text is required when using the color as a backdrop.
+ *
+ * \return an intensity threshold.
+ *
+ * \see `intensity_of(const QColor&)`
+ */
 [[nodiscard]] constexpr auto color_intensity_threshold() noexcept -> double
 {
   return 186;
