232c7fbac5df621259fc3cb907add7bff7ac6f62
Albin Johansson <albinoscar99@hotmail.com>
2021-06-14 16:29:20 +0200

    Add core_fwd.hpp

---
+++ src/core/core_fwd.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+namespace tactile::core {
+
+class IPropertyManager;
+class Property;
+
+class ADocument;
+class MapDocument;
+
+class ILayer;
+class TileLayer;
+class ObjectLayer;
+
+class Map;
+
+class Object;
+
+class Tileset;
+class TilesetManager;
+
+}  // namespace tactile::core
