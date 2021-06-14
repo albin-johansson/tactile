1693ff95f892fd155357cbf2cd187a9eb1903abf
Albin Johansson <albinoscar99@hotmail.com>
2021-06-14 16:29:33 +0200

    Add vm_fwd.hpp

---
+++ src/core/viewmodels/vm_fwd.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+namespace tactile::vm {
+
+class PropertyModel;
+class RootItem;
+class StringItem;
+class IntItem;
+class FloatItem;
+class BoolItem;
+class ObjectItem;
+class ColorItem;
+class FileItem;
+
+class LayerModel;
+class LayerItem;
+
+}  // namespace tactile::vm
