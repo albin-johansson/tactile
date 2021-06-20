435af8ba68e99b7ccddaef434bf799d78c00c445
Albin Johansson <albinoscar99@hotmail.com>
2021-06-20 23:23:23 +0200

    Add InitOpenGLAttributes function

---
+++ src/init_open_gl_attributes.cpp
@@ -0,0 +1,30 @@
+#include "init_open_gl_attributes.hpp"
+
+#include <centurion.hpp>
+
+namespace tactile {
+namespace {
+
+inline constexpr int major_version = 3;
+inline constexpr int minor_version = 0;
+
+}  // namespace
+
+void InitOpenGLAttributes()
+{
+  cen::gl::set(cen::gl_attribute::flags, 0);
+  cen::gl::set(cen::gl_attribute::context_profile_mask,
+               SDL_GL_CONTEXT_PROFILE_CORE);
+  cen::gl::set(cen::gl_attribute::context_major_version, major_version);
+  cen::gl::set(cen::gl_attribute::context_minor_version, minor_version);
+
+  // Create window with graphics context
+  cen::gl::set(cen::gl_attribute::double_buffer, 1);
+  cen::gl::set(cen::gl_attribute::depth_size, 24);
+  cen::gl::set(cen::gl_attribute::stencil_size, 8);
+
+  // Enable VSync
+  cen::gl::set_swap_interval(cen::gl_swap_interval::synchronized);
+}
+
+}  // namespace tactile
---
+++ src/init_open_gl_attributes.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace tactile {
+
+void InitOpenGLAttributes();
+
+}  // namespace tactile
