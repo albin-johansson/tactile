9be2e6a014f8551fb43eeb19dfed9eb1682df60d
Albin Johansson <albinoscar99@hotmail.com>
2021-06-22 00:25:45 +0200

    Tweak style

--- src/gui/imgui_wrapper.cpp
+++ src/gui/imgui_wrapper.cpp
@@ -3,6 +3,7 @@
 #include "imgui.h"
 #include "imgui_impl_opengl3.h"
 #include "imgui_impl_sdl.h"
+#include "themes.hpp"
 
 namespace tactile {
 namespace {
@@ -21,11 +22,32 @@ ImGuiWrapper::ImGuiWrapper(cen::window& window, cen::gl_context& context)
   io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;      // NOLINT
   io.WantCaptureKeyboard = true;
 
+  io.Fonts->AddFontFromFileTTF(R"(resources\fonts\roboto\Roboto-Regular.ttf)",
+                               15);
+
   ImGui::StyleColorsDark();
 
   auto& style = ImGui::GetStyle();
+  style.WindowPadding = ImVec2{15, 15};
+  style.FramePadding = ImVec2{5, 5};
+
+  style.ItemSpacing = ImVec2{12, 8};
+  style.ItemInnerSpacing = ImVec2{8, 6};
+  style.IndentSpacing = 25;
+
+  style.GrabMinSize = 6;
+  style.ScrollbarSize = 10;
+
+  style.WindowRounding = 5;
   style.FrameRounding = 4;
-  style.WindowBorderSize = 0;
+  style.PopupRounding = 4;
+  style.ScrollbarRounding = 4;
+  style.GrabRounding = 4;
+  style.TabRounding = 4;
+
+  ApplyNightTheme(style);
+
+  style.ScaleAllSizes(1);
 
   ImGui_ImplSDL2_InitForOpenGL(window.get(), context.get());
   ImGui_ImplOpenGL3_Init(glsl_version);
