605486038006dd1e54a905d87ae7b46c76f00cfa
Albin Johansson <albinoscar99@hotmail.com>
2021-06-22 23:23:17 +0200

    Add InvokeN algorithm

---
+++ src/core/algorithms/invoke_n.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#include <concepts>  // invocable
+
+#include "aliases/ints.hpp"
+
+namespace tactile {
+
+/**
+ * \brief Invokes the supplied callable for the specified amount of times.
+ *
+ * \tparam T the type of the callable.
+ *
+ * \param n the amount of times the callable should be invoked.
+ * \param callable the callable that will be invoked, must provide `operator()`.
+ */
+template <std::invocable T>
+constexpr void InvokeN(const usize n,
+                       T&& callable) noexcept(noexcept(callable()))
+{
+  for (usize i = 0; i < n; ++i)
+  {
+    callable();
+  }
+}
+
+}  // namespace tactile
