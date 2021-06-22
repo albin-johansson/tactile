1ce769ca30df1689f4c447450938193eb3a38885
Albin Johansson <albinoscar99@hotmail.com>
2021-06-22 20:59:01 +0200

    Update col.hpp

--- src/aliases/col.hpp
+++ src/aliases/col.hpp
@@ -2,6 +2,8 @@
 
 #include <nenya.hpp>  // strong_type
 
+#include "ints.hpp"
+
 namespace tactile {
 
 namespace tags {
@@ -9,5 +11,11 @@ struct col_tag;
 }  // namespace tags
 
 using col_t = nenya::strong_type<int, tags::col_tag>;
+
+[[nodiscard]] constexpr auto operator""_col(const ulonglong value) noexcept
+    -> col_t
+{
+  return col_t{static_cast<col_t::value_type>(value)};
+}
 
 }  // namespace tactile
