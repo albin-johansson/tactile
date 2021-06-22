173eafc61c1b33fb9a38554f5fa04dd361d833c3
Albin Johansson <albinoscar99@hotmail.com>
2021-06-22 20:58:30 +0200

    Update row.hpp

--- src/aliases/row.hpp
+++ src/aliases/row.hpp
@@ -2,6 +2,8 @@
 
 #include <nenya.hpp>  // strong_type
 
+#include "ints.hpp"
+
 namespace tactile {
 
 namespace tags {
@@ -9,5 +11,11 @@ struct row_tag;
 }  // namespace tags
 
 using row_t = nenya::strong_type<int, tags::row_tag>;
+
+[[nodiscard]] constexpr auto operator""_row(const ulonglong value) noexcept
+    -> row_t
+{
+  return row_t{static_cast<row_t::value_type>(value)};
+}
 
 }  // namespace tactile
