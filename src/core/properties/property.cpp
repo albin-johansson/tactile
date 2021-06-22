27e943c71628e74798ffae79f6e5dd032d03db75
Albin Johansson <albinoscar99@hotmail.com>
2021-06-22 16:01:50 +0200

    Add Property

---
+++ src/core/properties/property.cpp
@@ -0,0 +1,153 @@
+#include "property.hpp"
+
+namespace tactile {
+
+void Property::Reset()
+{
+  mValue.emplace<std::monostate>();
+}
+
+void Property::ResetToDefault(const PropertyType type)
+{
+  if (type == PropertyType::Integer)
+  {
+    SetValue<integer_type>(0);
+  }
+  else if (type == PropertyType::Floating)
+  {
+    SetValue<float_type>(0);
+  }
+  else if (type == PropertyType::Boolean)
+  {
+    SetValue<bool>(false);
+  }
+  else if (type == PropertyType::String)
+  {
+    SetValue<string_type>(string_type{});
+  }
+  else if (type == PropertyType::Color)
+  {
+    SetValue<color_type>(cen::colors::black);
+  }
+  else if (type == PropertyType::Object)
+  {
+    SetValue<object_ref>(object_ref{});
+  }
+  else if (type == PropertyType::File)
+  {
+    SetValue<file_type>(file_type{});
+  }
+}
+
+auto Property::HasValue() const noexcept -> bool
+{
+  return !std::holds_alternative<std::monostate>(mValue);
+}
+
+auto Property::AsString() const -> const string_type&
+{
+  return As<string_type>();
+}
+
+auto Property::AsInteger() const -> integer_type
+{
+  return As<integer_type>();
+}
+
+auto Property::AsFloating() const -> float_type
+{
+  return As<float_type>();
+}
+
+auto Property::AsBoolean() const -> bool
+{
+  return As<bool>();
+}
+
+auto Property::AsFile() const -> const file_type&
+{
+  return As<file_type>();
+}
+
+auto Property::AsObject() const -> object_ref
+{
+  return As<object_ref>();
+}
+
+auto Property::AsColor() const -> const color_type&
+{
+  return As<color_type>();
+}
+
+auto Property::IsString() const noexcept -> bool
+{
+  return Type() == PropertyType::String;
+}
+
+auto Property::IsInteger() const noexcept -> bool
+{
+  return Type() == PropertyType::Integer;
+}
+
+auto Property::IsFloating() const noexcept -> bool
+{
+  return Type() == PropertyType::Floating;
+}
+
+auto Property::IsBoolean() const noexcept -> bool
+{
+  return Type() == PropertyType::Boolean;
+}
+
+auto Property::IsFile() const noexcept -> bool
+{
+  return Type() == PropertyType::File;
+}
+
+auto Property::IsObject() const noexcept -> bool
+{
+  return Type() == PropertyType::Object;
+}
+
+auto Property::IsColor() const noexcept -> bool
+{
+  return Type() == PropertyType::Color;
+}
+
+auto Property::Type() const -> Maybe<PropertyType>
+{
+  if (Is<integer_type>())
+  {
+    return PropertyType::Integer;
+  }
+  else if (Is<float_type>())
+  {
+    return PropertyType::Floating;
+  }
+  else if (Is<string_type>())
+  {
+    return PropertyType::String;
+  }
+  else if (Is<bool>())
+  {
+    return PropertyType::Boolean;
+  }
+  else if (Is<file_type>())
+  {
+    return PropertyType::File;
+  }
+  else if (Is<object_ref>())
+  {
+    return PropertyType::Object;
+  }
+  else if (Is<color_type>())
+  {
+    return PropertyType::Color;
+  }
+  else
+  {
+    return nothing;
+  }
+}
+
+}  // namespace tactile
---
+++ src/core/properties/property.hpp
@@ -0,0 +1,268 @@
+#pragma once
+
+#include <centurion.hpp>  // color
+#include <filesystem>     // path
+#include <string>         // string
+#include <variant>        // variant, monostate, get, get_if, holds_alternative
+
+#include "aliases/maybe.hpp"
+#include "aliases/object_ref.hpp"
+#include "property_type.hpp"
+
+namespace tactile {
+
+// clang-format off
+
+template <typename T>
+concept IsPropertyType = std::same_as<T, std::string> ||
+                         std::same_as<T, int> ||
+                         std::same_as<T, double> ||
+                         std::same_as<T, bool> ||
+                         std::same_as<T, cen::color> ||
+                         std::same_as<T, std::filesystem::path> ||
+                         std::same_as<T, object_ref>;
+
+// clang-format on
+
+class Property final
+{
+ public:
+  using string_type = std::string;
+  using integer_type = int;
+  using float_type = double;
+  using color_type = cen::color;
+  using file_type = std::filesystem::path;
+
+  using value_type = std::variant<std::monostate,
+                                  string_type,
+                                  integer_type,
+                                  float_type,
+                                  bool,
+                                  color_type,
+                                  file_type,
+                                  object_ref>;
+
+  /// Creates a property with no value.
+  Property() = default;
+
+  /**
+   * \brief Creates a property.
+   *
+   * \tparam T the type of the property value.
+   *
+   * \param value the value of the property.
+   */
+  template <IsPropertyType T>
+  /*implicit*/ Property(const T& value)
+  {
+    mValue.emplace<T>(value);
+  }
+
+  /**
+   * \brief Resets the state of the property.
+   *
+   * \post `HasValue()` will return `false` after this function is invoked.
+   */
+  void Reset();
+
+  void ResetToDefault(PropertyType type);
+
+  /**
+   * \brief Sets the value stored in the property.
+   *
+   * \tparam T the type of the value.
+   *
+   * \param value the new value of the property.
+   */
+  template <IsPropertyType T>
+  void SetValue(const T& value)
+  {
+    mValue.emplace<T>(value);
+  }
+
+  /**
+   * \brief Indicates whether or not the property holds a value.
+   *
+   * \return `true` if the property holds a value; `false` otherwise.
+   */
+  [[nodiscard]] auto HasValue() const noexcept -> bool;
+
+  /**
+   * \brief Returns the stored string value.
+   *
+   * \note This function throws if the stored value isn't a string.
+   *
+   * \return the stored color.
+   */
+  [[nodiscard]] auto AsString() const -> const string_type&;
+
+  /**
+   * \brief Returns the stored integer value.
+   *
+   * \note This function throws if the stored value isn't an integer.
+   *
+   * \return the stored integer.
+   */
+  [[nodiscard]] auto AsInteger() const -> integer_type;
+
+  /**
+   * \brief Returns the stored float value.
+   *
+   * \note This function throws if the stored value isn't a float.
+   *
+   * \return the stored floating-point value.
+   */
+  [[nodiscard]] auto AsFloating() const -> float_type;
+
+  /**
+   * \brief Returns the stored boolean value.
+   *
+   * \note This function throws if the stored value isn't a boolean.
+   *
+   * \return the stored boolean value.
+   */
+  [[nodiscard]] auto AsBoolean() const -> bool;
+
+  /**
+   * \brief Returns the stored file path value.
+   *
+   * \note This function throws if the stored value isn't a file path.
+   *
+   * \return the stored file path value.
+   */
+  [[nodiscard]] auto AsFile() const -> const file_type&;
+
+  /**
+   * \brief Returns the stored object ID value.
+   *
+   * \note This function throws if the stored value isn't an object ID.
+   *
+   * \return the stored object ID value.
+   */
+  [[nodiscard]] auto AsObject() const -> object_ref;
+
+  /**
+   * \brief Returns the stored color value.
+   *
+   * \note This function throws if the stored value isn't a color.
+   *
+   * \return the stored color value.
+   */
+  [[nodiscard]] auto AsColor() const -> const color_type&;
+
+  [[nodiscard]] auto TryAsString() const noexcept -> const string_type*
+  {
+    return TryAs<string_type>();
+  }
+
+  [[nodiscard]] auto TryAsInteger() const noexcept -> const integer_type*
+  {
+    return TryAs<integer_type>();
+  }
+
+  [[nodiscard]] auto TryAsFloating() const noexcept -> const float_type*
+  {
+    return TryAs<float_type>();
+  }
+
+  [[nodiscard]] auto TryAsBoolean() const noexcept -> const bool*
+  {
+    return TryAs<bool>();
+  }
+
+  [[nodiscard]] auto TryAsFile() const noexcept -> const file_type*
+  {
+    return TryAs<file_type>();
+  }
+
+  [[nodiscard]] auto TryAsObject() const noexcept -> const object_ref*
+  {
+    return TryAs<object_ref>();
+  }
+
+  [[nodiscard]] auto TryAsColor() const noexcept -> const color_type*
+  {
+    return TryAs<color_type>();
+  }
+
+  /**
+   * \brief Indicates whether or not the stored value is a string.
+   *
+   * \return `true` if the stored value is a string; `false` otherwise.
+   */
+  [[nodiscard]] auto IsString() const noexcept -> bool;
+
+  /**
+   * \brief Indicates whether or not the stored value is an integer.
+   *
+   * \return `true` if the stored value is an integer; `false` otherwise.
+   */
+  [[nodiscard]] auto IsInteger() const noexcept -> bool;
+
+  /**
+   * \brief Indicates whether or not the stored value is a floating-point
+   * number.
+   *
+   * \return `true` if the stored value is a floating-point number; `false`
+   * otherwise.
+   */
+  [[nodiscard]] auto IsFloating() const noexcept -> bool;
+
+  /**
+   * \brief Indicates whether or not the stored value is a boolean.
+   *
+   * \return `true` if the stored value is a boolean; `false` otherwise.
+   */
+  [[nodiscard]] auto IsBoolean() const noexcept -> bool;
+
+  /**
+   * \brief Indicates whether or not the stored value is a file path.
+   *
+   * \return `true` if the stored value is a file path; `false` otherwise.
+   */
+  [[nodiscard]] auto IsFile() const noexcept -> bool;
+
+  /**
+   * \brief Indicates whether or not the stored value is an object ID.
+   *
+   * \return `true` if the stored value is an object ID; `false` otherwise.
+   */
+  [[nodiscard]] auto IsObject() const noexcept -> bool;
+
+  /**
+   * \brief Indicates whether or not the stored value is a color.
+   *
+   * \return `true` if the stored value is a color; `false` otherwise.
+   */
+  [[nodiscard]] auto IsColor() const noexcept -> bool;
+
+  /**
+   * \brief Returns the type of the stored value.
+   *
+   * \return the type of the stored value; `nothing` if there is no value.
+   */
+  [[nodiscard]] auto Type() const -> Maybe<PropertyType>;
+
+ private:
+  value_type mValue;
+
+  template <IsPropertyType T>
+  [[nodiscard]] auto Is() const noexcept -> bool
+  {
+    return std::holds_alternative<T>(mValue);
+  }
+
+  template <IsPropertyType T>
+  [[nodiscard]] auto As() const -> const T&
+  {
+    return std::get<T>(mValue);
+  }
+
+  template <IsPropertyType T>
+  [[nodiscard]] auto TryAs() const noexcept -> const T*
+  {
+    return std::get_if<T>(&mValue);
+  }
+};
+
+}  // namespace tactile
