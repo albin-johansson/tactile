598f7d431ea71a4ffd3041a4e70a830c8c033cf2
Albin Johansson <albinoscar99@hotmail.com>
2021-06-23 00:18:05 +0200

    Update tile_id.hpp

--- src/aliases/tile_id.hpp
+++ src/aliases/tile_id.hpp
@@ -2,6 +2,8 @@
 
 #include <nenya.hpp>  // strong_type
 
+#include "ints.hpp"
+
 namespace tactile {
 
 namespace tags {
@@ -11,5 +13,11 @@ struct tile_id_tag;
 using tile_id = nenya::strong_type<int, tags::tile_id_tag>;
 
 inline constexpr tile_id empty_tile{0};
+
+[[nodiscard]] constexpr auto operator""_tile(const ulonglong value) noexcept
+    -> tile_id
+{
+  return tile_id{static_cast<tile_id::value_type>(value)};
+}
 
 }  // namespace tactile
