b1b540d9ed4406d938c4d7d1926d78c9258d52b6
Albin Johansson <albinoscar99@hotmail.com>
2021-06-23 11:16:17 +0200

    Update object_id.hpp

--- src/aliases/object_id.hpp
+++ src/aliases/object_id.hpp
@@ -2,6 +2,8 @@
 
 #include <nenya.hpp>  // strong_type
 
+#include "ints.hpp"
+
 namespace tactile {
 
 namespace tags {
@@ -9,5 +11,11 @@ struct object_id_tag;
 }  // namespace tags
 
 using object_id = nenya::strong_type<int, tags::object_id_tag>;
+
+[[nodiscard]] constexpr auto operator""_obj(const ulonglong value) noexcept
+    -> object_id
+{
+  return object_id{static_cast<object_id::value_type>(value)};
+}
 
 }  // namespace tactile
