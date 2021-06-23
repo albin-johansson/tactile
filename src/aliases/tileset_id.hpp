792657ffa655b4fe2f7b62f1feb8eed7d2d5cb85
Albin Johansson <albinoscar99@hotmail.com>
2021-06-24 01:57:09 +0200

    Update tileset_id.hpp

--- src/aliases/tileset_id.hpp
+++ src/aliases/tileset_id.hpp
@@ -2,6 +2,8 @@
 
 #include <nenya.hpp>  // strong_type
 
+#include "ints.hpp"
+
 namespace tactile {
 
 namespace tags {
@@ -9,5 +11,11 @@ struct tileset_id_tag;
 }  // namespace tags
 
 using tileset_id = nenya::strong_type<int, tags::tileset_id_tag>;
+
+[[nodiscard]] constexpr auto operator""_tset(const ulonglong value) noexcept
+    -> tileset_id
+{
+  return tileset_id{static_cast<tileset_id::value_type>(value)};
+}
 
 }  // namespace tactile
