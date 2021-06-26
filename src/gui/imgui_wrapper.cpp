fbe76fe33fb2429bdd02fa23efbb1a4b0631d5ed
Albin Johansson <albinoscar99@hotmail.com>
2021-06-26 02:11:06 +0200

    Update imgui_wrapper.cpp

--- src/gui/imgui_wrapper.cpp
+++ src/gui/imgui_wrapper.cpp
@@ -49,12 +49,13 @@ ImGuiWrapper::ImGuiWrapper(cen::window& window, cen::gl_context& context)
   auto& style = ImGui::GetStyle();
   style.WindowBorderSize = 0;
 
-  style.WindowPadding = ImVec2{15, 15};
+  style.WindowPadding = ImVec2{10, 10};
   style.FramePadding = ImVec2{5, 5};
+  style.CellPadding = ImVec2{4, 4};
 
-  style.ItemSpacing = ImVec2{12, 8};
+  style.ItemSpacing = ImVec2{8, 8};
   style.ItemInnerSpacing = ImVec2{8, 6};
-  style.IndentSpacing = 25;
+  style.IndentSpacing = 15;
 
   style.GrabMinSize = 6;
   style.ScrollbarSize = 10;
