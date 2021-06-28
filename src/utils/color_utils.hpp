bf6780dde3fd0ee66a6fb7a1e2698f9412fa8a98
Albin Johansson <albinoscar99@hotmail.com>
2021-06-28 13:09:14 +0200

    Add color utilities

---
+++ src/utils/color_utils.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <array>          // array
+#include <centurion.hpp>  // color
+
+namespace tactile {
+
+[[nodiscard]] constexpr auto ColorToArray(const cen::color& color) noexcept
+    -> std::array<float, 4>
+{
+  const auto r = static_cast<float>(color.red()) / 255.0f;
+  const auto g = static_cast<float>(color.green()) / 255.0f;
+  const auto b = static_cast<float>(color.blue()) / 255.0f;
+  const auto a = static_cast<float>(color.alpha()) / 255.0f;
+  return {r, g, b, a};
+}
+
+}  // namespace tactile
