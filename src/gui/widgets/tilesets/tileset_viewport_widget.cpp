c0637966433ed468b0056bff34cbbac1662bb44d
Albin Johansson <albinoscar99@hotmail.com>
2021-06-30 02:03:49 +0200

    Use new rubber band widget in tileset viewports

--- src/gui/widgets/tilesets/tileset_viewport_widget.cpp
+++ src/gui/widgets/tilesets/tileset_viewport_widget.cpp
@@ -1,7 +1,6 @@
 #include "tileset_viewport_widget.hpp"
 
 #include <algorithm>  // min, max
-#include <cmath>      // round, fmod
 
 #include "core/events/set_tileset_selection_event.hpp"
 #include "core/tileset/tileset.hpp"
@@ -9,6 +8,7 @@
 #include "gui/get_texture_id.hpp"
 #include "gui/show_grid.hpp"
 #include "gui/widgets/mouse_tracker.hpp"
+#include "gui/widgets/rubber_band.hpp"
 #include "imgui.h"
 #include "imgui_internal.h"
 
@@ -36,64 +36,29 @@ void ClampOffsets(const ImVec2 canvasSize,
 
 void TilesetViewportWidget(const Tileset& tileset, entt::dispatcher& dispatcher)
 {
-  const auto tileWidth = static_cast<float>(tileset.GetTileWidth());
-  const auto tileHeight = static_cast<float>(tileset.GetTileHeight());
-
-  state.grid_size = {tileWidth, tileHeight};
-
   const auto texture = GetTextureID(tileset.GetTexture());
-  const auto width = static_cast<float>(tileset.GetWidth());
-  const auto height = static_cast<float>(tileset.GetHeight());
+  const auto textureSize = ImVec2{static_cast<float>(tileset.GetWidth()),
+                                  static_cast<float>(tileset.GetHeight())};
+  const auto tileSize = ImVec2{static_cast<float>(tileset.GetTileWidth()),
+                               static_cast<float>(tileset.GetTileHeight())};
+  state.grid_size = tileSize;
 
   const auto info = GetCanvasInfo();
   FillBackground(info);
 
   MouseTracker(info, state);
+  ClampOffsets(info.canvas_size, textureSize.x, textureSize.y);
 
-  ClampOffsets(info.canvas_size, width, height);
-
-  auto* drawList = ImGui::GetWindowDrawList();
-
-  const auto toRow = [=](const float y) {
-    return row_t{static_cast<row_t::value_type>(std::round(y / tileHeight))};
-  };
-
-  const auto toColumn = [=](const float x) {
-    return col_t{static_cast<col_t::value_type>(std::round(x / tileWidth))};
-  };
-
-  if (ImGui::IsItemActive())
+  if (const auto selection = RubberBand(state.scroll_offset, tileSize))
   {
-    const auto button = ImGuiMouseButton_Left;
-    const auto clicked = ImGui::IsMouseClicked(button);
-    const auto dragging = ImGui::IsMouseDragging(button);
-    if (clicked || dragging)
-    {
-      const auto mousePos = ImGui::GetMousePos();
-      const auto itemPos = ImGui::GetItemRectMin();
-      const auto delta = dragging ? ImGui::GetMouseDragDelta(button) : ImVec2{};
-
-      const auto relativeToViewport = mousePos - itemPos - delta;
-      const auto effective = relativeToViewport - state.scroll_offset;
-
-      const auto startX = effective.x - std::fmod(effective.x, tileWidth);
-      const auto startY = effective.y - std::fmod(effective.y, tileHeight);
-
-      TilesetSelection selection;
-      selection.top_left = {toRow(startY), toColumn(startX)};
-
-      const MapPosition offset{toRow(delta.y) + 1_row,
-                               toColumn(delta.x) + 1_col};
-      selection.bottom_right = selection.top_left + offset;
-
-      dispatcher.enqueue<SetTilesetSelectionEvent>(selection);
-    }
+    dispatcher.enqueue<SetTilesetSelectionEvent>(*selection);
   }
 
+  auto* drawList = ImGui::GetWindowDrawList();
   drawList->PushClipRect(info.canvas_tl, info.canvas_br, true);
 
-  const auto position = drawList->GetClipRectMin() + state.scroll_offset;
-  drawList->AddImage(texture, position, position + ImVec2{width, height});
+  const auto min = drawList->GetClipRectMin() + state.scroll_offset;
+  drawList->AddImage(texture, min, min + textureSize);
 
   if (const auto& selection = tileset.GetSelection())
   {
@@ -101,14 +66,14 @@ void TilesetViewportWidget(const Tileset& tileset, entt::dispatcher& dispatcher)
     const auto br = selection->bottom_right;
     const auto diff = br - tl;
 
-    const ImVec2 size = {static_cast<float>(diff.GetColumn()) * tileWidth,
-                         static_cast<float>(diff.GetRow()) * tileHeight};
+    const ImVec2 origin{static_cast<float>(tl.GetColumn()) * tileSize.x,
+                        static_cast<float>(tl.GetRow()) * tileSize.y};
 
-    const ImVec2 origin = {static_cast<float>(tl.GetColumn()) * tileWidth,
-                           static_cast<float>(tl.GetRow()) * tileHeight};
+    const ImVec2 size{static_cast<float>(diff.GetColumn()) * tileSize.x,
+                      static_cast<float>(diff.GetRow()) * tileSize.y};
 
-    drawList->AddRectFilled(position + origin,
-                            position + origin + size,
+    drawList->AddRectFilled(min + origin,
+                            min + origin + size,
                             rubber_band_color);
   }
 
