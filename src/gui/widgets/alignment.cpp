573b45afb439b82d4f4a0061149ff48fd16e6084
Albin Johansson <albinoscar99@hotmail.com>
2021-07-01 17:54:07 +0200

    Add PrepareVerticalAlignmentCenter function

---
+++ src/gui/widgets/alignment.cpp
@@ -0,0 +1,24 @@
+#include "alignment.hpp"
+
+#include "imgui.h"
+#include "imgui_internal.h"
+
+namespace Tactile {
+
+void PrepareVerticalAlignmentCenter(const float count)
+{
+  const auto fontSize = ImGui::GetFontSize();
+
+  const auto region = ImGui::GetContentRegionAvail();
+  const auto halfRegion = region / ImVec2{2.0f, 2.0f};
+
+  const auto padding = ImGui::GetStyle().FramePadding;
+  const auto spacing = ImGui::GetStyle().ItemSpacing;
+
+  const auto height = (count * (fontSize + (2.0f * padding.y)));
+
+  const auto cursorPos = ImGui::GetCursorPos();
+  ImGui::SetCursorPos(cursorPos + ImVec2{0, halfRegion.y - (height / 2.0f)});
+}
+
+}  // namespace Tactile
---
+++ src/gui/widgets/alignment.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace Tactile {
+
+// Ensures that the specified amount of items are centered in the window
+void PrepareVerticalAlignmentCenter(float count = 1);
+
+}  // namespace Tactile
