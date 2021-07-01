a72be2175c8d5f711086a85467904de3051bd9f4
Albin Johansson <albinoscar99@hotmail.com>
2021-07-01 17:54:42 +0200

    Update render_map.cpp

--- src/gui/widgets/viewport/render_map.cpp
+++ src/gui/widgets/viewport/render_map.cpp
@@ -3,11 +3,13 @@
 #include "core/map_document.hpp"
 #include "imgui.h"
 #include "imgui_internal.h"
+#include "io/preferences.hpp"
 
 namespace Tactile {
 namespace {
 
-inline constexpr auto border_color = IM_COL32(0x10, 0x10, 0x10, 0xFF);
+inline constexpr auto out_border_color = IM_COL32(0xFF, 0xFF, 0xFF, 0xFF);
+inline constexpr auto in_border_color = IM_COL32(0xFF, 0xFF, 0xFF, 50);
 
 void RenderOutline(ImDrawList* drawList,
                    const int nRows,
@@ -17,16 +19,7 @@ void RenderOutline(ImDrawList* drawList,
 {
   const auto width = static_cast<float>(nCols) * tileSize.x;
   const auto height = static_cast<float>(nRows) * tileSize.y;
-
-  drawList->AddLine(offset, {offset.x + width, offset.y}, border_color);
-  drawList->AddLine(offset, {offset.x, offset.y + height}, border_color);
-
-  drawList->AddLine({offset.x, offset.y + height},
-                    {offset.x + width, offset.y + height},
-                    border_color);
-  drawList->AddLine({offset.x + width, offset.y},
-                    {offset.x + width, offset.y + height},
-                    border_color);
+  drawList->AddRect(offset, offset + ImVec2{width, height}, out_border_color);
 }
 
 }  // namespace
@@ -36,6 +29,8 @@ void RenderMap(const MapDocument& document,
                const ImVec2 mapPos,
                const ImVec2 tileSize)
 {
+  const auto showGrid = Prefs::GetShowGrid();
+
   const auto nRows = document.GetRowCount();
   const auto nCols = document.GetColumnCount();
   for (auto row = 0; row < nRows; ++row)
@@ -44,7 +39,10 @@ void RenderMap(const MapDocument& document,
     {
       const ImVec2 pos = {mapPos.x + (tileSize.x * static_cast<float>(col)),
                           mapPos.y + (tileSize.y * static_cast<float>(row))};
-      drawList->AddRect(pos, pos + tileSize, border_color);
+      if (showGrid)
+      {
+        drawList->AddRect(pos, pos + tileSize, in_border_color);
+      }
     }
   }
 
