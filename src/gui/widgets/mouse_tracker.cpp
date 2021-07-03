b4a1bfd56a63dc73137d72d62e6dbfc2600fd99d
Albin Johansson <albinoscar99@hotmail.com>
2021-07-03 21:16:05 +0200

    Update mouse_tracker.cpp

--- src/gui/widgets/mouse_tracker.cpp
+++ src/gui/widgets/mouse_tracker.cpp
@@ -3,15 +3,16 @@
 namespace Tactile {
 namespace {
 
-inline constexpr auto flags =
-    ImGuiButtonFlags_MouseButtonLeft | ImGuiButtonFlags_MouseButtonRight;
+inline constexpr auto flags = ImGuiButtonFlags_MouseButtonLeft |
+                              ImGuiButtonFlags_MouseButtonMiddle |
+                              ImGuiButtonFlags_MouseButtonRight;
 
 }  // namespace
 
 void MouseTracker(const CanvasInfo& canvas, GridState& state)
 {
   ImGui::InvisibleButton("MouseTracker", canvas.size, flags);
-  if (ImGui::IsItemActive() && ImGui::IsMouseDragging(ImGuiMouseButton_Right))
+  if (ImGui::IsItemActive() && ImGui::IsMouseDragging(ImGuiMouseButton_Middle))
   {
     const auto& io = ImGui::GetIO();
     state.scroll_offset.x += io.MouseDelta.x;
