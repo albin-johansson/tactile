b68f7352a104189da6a2a187592c8051b8ab8793
Albin Johansson <albinoscar99@hotmail.com>
2021-07-05 12:50:45 +0200

    Add clickable URL to GitHub repository

--- src/gui/widgets/dialogs/about_dialog.cpp
+++ src/gui/widgets/dialogs/about_dialog.cpp
@@ -2,7 +2,10 @@
 
 #include <imgui.h>
 
+#include <centurion.hpp>  // open_url
+
 #include "gui/icons.hpp"
+#include "gui/widgets/common/button_ex.hpp"
 
 namespace Tactile {
 
@@ -17,9 +20,15 @@ void UpdateAboutDialog(bool* open)
     ImGui::Text("Tactile 0.2.0 (C) Albin Johansson 2020-2021");
     ImGui::Separator();
 
-    ImGui::Text(
-        "This tool is open-source software, using the GPL v3.0 license.");
+    ImGui::Text("This tool is open-source software, using the GPL v3.0 license.");
+
+    ImGui::AlignTextToFramePadding();
     ImGui::Text("Source code: https://www.github.com/albin-johansson/tactile");
+    ImGui::SameLine();
+    if (ButtonEx(TAC_ICON_LINK, "Open the GitHub repository in your browser."))
+    {
+      cen::open_url("https://www.github.com/albin-johansson/tactile");
+    }
 
     ImGui::Spacing();
     ImGui::Text("Icons by Font Awesome.");
