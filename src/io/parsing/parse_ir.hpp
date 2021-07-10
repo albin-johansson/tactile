21368d5665af2ea57f15b076245a4ac27f0c77b0
Albin Johansson <albinoscar99@hotmail.com>
2021-07-10 13:09:57 +0200

    Add intermediate parsing representation types

---
+++ src/io/parsing/parse_ir.hpp
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <filesystem>  // path
+#include <string>      // string
+#include <variant>     // variant
+#include <vector>      // vector
+
+#include "aliases/col.hpp"
+#include "aliases/layer_id.hpp"
+#include "aliases/object_id.hpp"
+#include "aliases/row.hpp"
+#include "aliases/tile_id.hpp"
+#include "aliases/tile_matrix.hpp"
+#include "core/map/layers/layer_type.hpp"
+#include "core/properties/property.hpp"
+
+namespace Tactile::IO {
+
+struct PropertyData final
+{
+  std::string name;
+  Property property;
+};
+
+struct TilesetData final
+{
+  tile_id first_id;
+  int tile_width{};
+  int tile_height{};
+  std::filesystem::path absolute_image_path;
+  std::string name;
+  std::vector<PropertyData> properties;
+};
+
+struct TileLayerData final
+{
+  row_t row_count;
+  col_t col_count;
+  TileMatrix tiles;
+};
+
+struct ObjectData final
+{
+  object_id id;
+  float x{};
+  float y{};
+  float width{};
+  float height{};
+  std::string custom_type;
+  std::string name;
+  std::vector<PropertyData> properties;
+  bool visible{};
+  bool is_point{};
+  bool is_rectangle{};
+};
+
+struct ObjectLayerData final
+{
+  std::vector<ObjectData> objects;
+};
+
+struct LayerData final
+{
+  using LayerContent = std::variant<TileLayerData, ObjectLayerData>;
+
+  layer_id id;
+  LayerType type;
+  LayerContent data;
+  std::string name;
+  std::vector<PropertyData> properties;
+  float opacity{};
+  bool is_visible{};
+};
+
+struct MapData final
+{
+  std::filesystem::path absolute_path;
+  layer_id next_layer_id;
+  object_id next_object_id;
+  std::vector<TilesetData> tilesets;
+  std::vector<LayerData> layers;
+  std::vector<PropertyData> properties;
+};
+
+}  // namespace Tactile::IO
