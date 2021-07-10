6892c5bc9330b918463f96dbdcaae0ddf15aa7f4
Albin Johansson <albinoscar99@hotmail.com>
2021-07-10 08:57:30 +0200

    Add MapAttribute enum

---
+++ src/io/parsing/map_attribute.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+namespace Tactile::IO {
+
+enum class MapAttribute
+{
+  Name,
+  Id,
+  Type,
+  X,
+  Y,
+  Width,
+  Height,
+  Visible,
+  Opacity,
+  Tileset,
+  NextLayerId,
+  NextObjectId,
+  Source,
+  FirstGid,
+  TileWidth,
+  TileHeight,
+  Image,
+  Value,
+  Point
+};
+
+}  // namespace Tactile::IO
