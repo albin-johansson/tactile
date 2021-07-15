f9c092853f82c2d4f972de2f1467b0fd7651d1f2
Albin Johansson <albinoscar99@hotmail.com>
2021-07-15 17:40:00 +0200

    Update save_xml.cpp

--- src/io/saving/xml/save_xml.cpp
+++ src/io/saving/xml/save_xml.cpp
@@ -2,22 +2,12 @@
 
 #include <fstream>  // ofstream
 
-#include "io/preferences.hpp"
-
 namespace Tactile::IO {
 
 void SaveXml(const pugi::xml_document& xml, const std::filesystem::path& path)
 {
   std::ofstream stream{path};
-
-  if (Prefs::GetHumanReadableOutput())
-  {
-    xml.save(stream, " ");
-  }
-  else
-  {
-    xml.save(stream, "");
-  }
+  xml.save(stream, " ");
 }
 
 }  // namespace Tactile::IO
