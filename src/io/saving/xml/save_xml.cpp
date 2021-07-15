99cdedc3b7c52a4a4b355d31b3650aa67ac54663
Albin Johansson <albinoscar99@hotmail.com>
2021-07-15 15:09:28 +0200

    Add SaveXml function

---
+++ src/io/saving/xml/save_xml.cpp
@@ -0,0 +1,23 @@
+#include "save_xml.hpp"
+
+#include <fstream>  // ofstream
+
+#include "io/preferences.hpp"
+
+namespace Tactile::IO {
+
+void SaveXml(const pugi::xml_document& xml, const std::filesystem::path& path)
+{
+  std::ofstream stream{path};
+
+  if (Prefs::GetHumanReadableOutput())
+  {
+    xml.save(stream);
+  }
+  else
+  {
+    xml.save(stream, "");
+  }
+}
+
+}  // namespace Tactile::IO
