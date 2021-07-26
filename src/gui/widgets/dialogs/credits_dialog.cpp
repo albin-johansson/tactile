4e368f6286f93e29d94374fda44c38cc5621ba3c
Albin Johansson <albinoscar99@hotmail.com>
2021-07-26 12:04:45 +0200

    Update EnTT to version 3.8.0

--- src/gui/widgets/dialogs/credits_dialog.cpp
+++ src/gui/widgets/dialogs/credits_dialog.cpp
@@ -45,7 +45,7 @@ void UpdateCreditsDialog(bool* open)
 
       Row("Centurion", "6.2.0", "MIT");
       Row("Dear ImGui", "1.84", "MIT");
-      Row("EnTT", "3.7.1", "MIT");
+      Row("EnTT", "3.8.0", "MIT");
       Row("GLEW", "2.2.0", "BSD/MIT");
       Row("IconFontCppHeaders", "N/A", "Zlib");
       Row("ImGuiFileDialog", "0.5.5", "MIT");
