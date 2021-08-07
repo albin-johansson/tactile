5bcd5ff7da87256a554dc72da10adf20ad15d7eb
Albin Johansson <albinoscar99@hotmail.com>
2021-08-07 17:18:55 +0200

    Update settings_dialog.hpp

--- src/gui/widgets/dialogs/settings_dialog.hpp
+++ src/gui/widgets/dialogs/settings_dialog.hpp
@@ -4,8 +4,25 @@
 
 namespace Tactile {
 
+/// \addtogroup gui
+/// \{
+
+/**
+ * \brief Updates the settings dialog.
+ *
+ * \param dispatcher the event dispatcher that will be used.
+ *
+ * \see `OpenSettingsDialog()`
+ */
 void UpdateSettingsDialog(entt::dispatcher& dispatcher);
 
+/**
+ * \brief Opens the settings dialog.
+ *
+ * \see `UpdateSettingsDialog()`
+ */
 void OpenSettingsDialog();
+
+/// \} End of group gui
 
 }  // namespace Tactile
