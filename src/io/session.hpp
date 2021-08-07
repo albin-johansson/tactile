e8968acee385cf045251487807e7639ea68ecd53
Albin Johansson <albinoscar99@hotmail.com>
2021-08-07 18:09:06 +0200

    Update session.hpp

--- src/io/session.hpp
+++ src/io/session.hpp
@@ -4,8 +4,27 @@ namespace Tactile {
 
 class Model;
 
+/// \addtogroup io
+/// \{
+
+/**
+ * \brief Restores the maps from the previous session.
+ *
+ * \param model the model that the previous session will be restored to.
+ *
+ * \see `SaveSession()`
+ */
 void RestoreLastSession(Model& model);
 
+/**
+ * \brief Saves the current session.
+ *
+ * \param model the model that represent the current session that will be saved.
+ *
+ * \see `RestoreLastSession()`
+ */
 void SaveSession(const Model& model);
+
+/// \} End of group io
 
 }  // namespace Tactile
