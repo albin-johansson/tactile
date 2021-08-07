3e2ad2e2ccdbdb2de308f73d04ae29e7c89e14ad
Albin Johansson <albinoscar99@hotmail.com>
2021-08-07 18:15:02 +0200

    Update parse_error.hpp

--- src/io/parsing/parse_error.hpp
+++ src/io/parsing/parse_error.hpp
@@ -6,6 +6,15 @@
 
 namespace Tactile::IO {
 
+/// \addtogroup io
+/// \{
+
+/**
+ * \brief Provides various map parsing error codes.
+ *
+ * \see `MapParser`
+ * \see `GetCause(ParseError)`
+ */
 enum class ParseError
 {
   None,
@@ -46,8 +55,28 @@ enum class ParseError
   ObjectMissingId
 };
 
+/**
+ * \brief Returns a potential cause of the specified parse error.
+ *
+ * \details The returned string is meant to help the user to identify the potential
+ * cause of a parse error.
+ *
+ * \param error the error code to obtain a potential cause for.
+ *
+ * \return a short explanation of the potential cause of the error.
+ */
 [[nodiscard]] auto GetCause(ParseError error) -> czstring;
 
+/**
+ * \brief Prints a textual representation of a parse error enumerator.
+ *
+ * \param stream the output stream that will be used.
+ * \param error the error code that will be printed.
+ *
+ * \return the used stream.
+ */
 auto operator<<(std::ostream& stream, ParseError error) -> std::ostream&;
+
+/// \} End of group io
 
 }  // namespace Tactile::IO
