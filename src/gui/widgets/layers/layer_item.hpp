3d545713028bd9060d62fb8e85b1ae201bb04308
Albin Johansson <albinoscar99@hotmail.com>
2021-08-07 17:31:36 +0200

    Update layer_item.hpp

--- src/gui/widgets/layers/layer_item.hpp
+++ src/gui/widgets/layers/layer_item.hpp
@@ -9,6 +9,16 @@ namespace Tactile {
 
 class MapDocument;
 
+/**
+ * \brief Shows a layer item that represents a layer in a map.
+ *
+ * \ingroup gui
+ *
+ * \param document the currently active map document.
+ * \param dispatcher the event dispatcher that will be used.
+ * \param id the ID of the layer associated with the item.
+ * \param layer the associated layer.
+ */
 void LayerItem(const MapDocument& document,
                entt::dispatcher& dispatcher,
                layer_id id,
