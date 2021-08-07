5b56f82fff41d55fe7b96b91640cb92826ead6cc
Albin Johansson <albinoscar99@hotmail.com>
2021-08-07 15:26:35 +0200

    Update about_dialog.cpp

--- src/gui/widgets/dialogs/about_dialog.cpp
+++ src/gui/widgets/dialogs/about_dialog.cpp
@@ -7,37 +7,41 @@
 #include "gui/icons.hpp"
 #include "gui/widgets/common/button.hpp"
 
+#define TACTILE_VER "0.2.0"
+#define TACTILE_URL "https://www.github.com/albin-johansson/tactile"
+
 namespace Tactile {
+namespace {
+
+constexpr auto source_code = "Source code: " TACTILE_URL;
+
+}  // namespace
 
 void UpdateAboutDialog(bool* open)
 {
-  ImGui::PushStyleVar(ImGuiStyleVar_WindowMinSize, ImVec2{200, 150});
-
   if (ImGui::Begin(TAC_ICON_ABOUT " About Tactile",
                    open,
                    ImGuiWindowFlags_AlwaysAutoResize))
   {
-    ImGui::TextUnformatted("Tactile 0.2.0 (C) Albin Johansson 2020-2021");
+    ImGui::TextUnformatted("Tactile " TACTILE_VER " (C) Albin Johansson 2020-2021");
     ImGui::Separator();
 
     ImGui::TextUnformatted(
         "This tool is open-source software, using the GPL v3.0 license.");
 
     ImGui::AlignTextToFramePadding();
-    ImGui::TextUnformatted(
-        "Source code: https://www.github.com/albin-johansson/tactile");
+    ImGui::TextUnformatted(source_code);
     ImGui::SameLine();
     if (Button(TAC_ICON_LINK, "Open the GitHub repository in your browser."))
     {
-      cen::open_url("https://www.github.com/albin-johansson/tactile");
+      cen::open_url(TACTILE_URL);
     }
 
     ImGui::Spacing();
     ImGui::TextUnformatted("Icons by Font Awesome.");
   }
-  ImGui::End();
 
-  ImGui::PopStyleVar();
+  ImGui::End();
 }
 
 }  // namespace Tactile
