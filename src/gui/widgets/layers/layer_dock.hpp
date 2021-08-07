bcfbcbd9dbd889b62915dc69771176e83ce03219
Albin Johansson <albinoscar99@hotmail.com>
2021-08-07 17:29:29 +0200

    Update layer_dock.hpp

--- src/gui/widgets/layers/layer_dock.hpp
+++ src/gui/widgets/layers/layer_dock.hpp
@@ -6,6 +6,14 @@ namespace Tactile {
 
 class Model;
 
+/**
+ * \brief Updates the state of the layer dock widget.
+ *
+ * \ingroup gui
+ *
+ * \param model the associated model.
+ * \param dispatcher the event dispatcher that will be used.
+ */
 void UpdateLayerDock(const Model& model, entt::dispatcher& dispatcher);
 
 }  // namespace Tactile
