78e428d20aec2ed4eb8f5fe20540c2b771faae1a
Albin Johansson <albinoscar99@hotmail.com>
2021-08-07 15:34:29 +0200

    Update credits_dialog.hpp

--- src/gui/widgets/dialogs/credits_dialog.hpp
+++ src/gui/widgets/dialogs/credits_dialog.hpp
@@ -2,6 +2,13 @@
 
 namespace Tactile {
 
+/**
+ * \brief Updates the credits dialog.
+ *
+ * \ingroup gui
+ *
+ * \param[out] open a pointer to which the open state is written to.
+ */
 void UpdateCreditsDialog(bool* open);
 
 }  // namespace Tactile
