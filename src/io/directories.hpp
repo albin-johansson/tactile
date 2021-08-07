454b3fcf5fce299090bdbce7fd422e4efb866843
Albin Johansson <albinoscar99@hotmail.com>
2021-08-07 18:09:49 +0200

    Update directories.hpp

--- src/io/directories.hpp
+++ src/io/directories.hpp
@@ -4,6 +4,13 @@
 
 namespace Tactile {
 
+/**
+ * \brief Returns the path to the persistent file directory.
+ *
+ * \ingroup io
+ *
+ * \return the persistent file directory path.
+ */
 [[nodiscard]] auto GetPersistentFileDir() -> const std::filesystem::path&;
 
 }  // namespace Tactile
