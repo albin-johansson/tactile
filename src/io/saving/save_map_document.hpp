c26cc61e3071ddb613a599fb2743be133f81ce12
Albin Johansson <albinoscar99@hotmail.com>
2021-08-07 23:55:27 +0200

    Update save_map_document.hpp

--- src/io/saving/save_map_document.hpp
+++ src/io/saving/save_map_document.hpp
@@ -4,6 +4,13 @@
 
 namespace Tactile::IO {
 
+/**
+ * \brief Saves a map document.
+ *
+ * \ingroup io
+ *
+ * \param document the map document that will be saved.
+ */
 void SaveMapDocument(const MapDocument& document);
 
 }  // namespace Tactile::IO
