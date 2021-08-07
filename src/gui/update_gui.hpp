8723313c9727a1f09ba0d35b1687aaca5c4afb4c
Albin Johansson <albinoscar99@hotmail.com>
2021-08-07 13:25:59 +0200

    Update update_gui.hpp

--- src/gui/update_gui.hpp
+++ src/gui/update_gui.hpp
@@ -6,6 +6,14 @@ namespace Tactile {
 
 class Model;
 
+/**
+ * \brief Updates the state of all GUI components.
+ *
+ * \ingroup gui
+ *
+ * \param model the model that will be displayed.
+ * \param dispatcher the event dispatcher that will be used.
+ */
 void UpdateGui(const Model& model, entt::dispatcher& dispatcher);
 
 }  // namespace Tactile
