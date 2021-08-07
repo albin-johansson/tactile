d89e8b2645ed5da9ac50016788472a51c9bd9c29
Albin Johansson <albinoscar99@hotmail.com>
2021-08-07 14:01:45 +0200

    Update uv_tile_size.hpp

--- src/gui/rendering/uv_tile_size.hpp
+++ src/gui/rendering/uv_tile_size.hpp
@@ -2,12 +2,22 @@
 
 #include <imgui.h>
 
-#include "aliases/tile_id.hpp"
-
 namespace Tactile {
 
 class Tileset;
 
+/**
+ * \brief Returns the UV tile size for a tileset.
+ *
+ * \ingroup rendering
+ *
+ * \details Use this function to acquire a vector that can be used to render
+ * individual tiles from a tileset image.
+ *
+ * \param tileset the tileset that will be queried.
+ *
+ * \return the UV tile size for rendering individual tiles.
+ */
 [[nodiscard]] auto GetTileSizeUV(const Tileset& tileset) -> ImVec2;
 
 }  // namespace Tactile
