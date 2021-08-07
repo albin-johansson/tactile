4d8ab89244745f736974329005385bad06a4a8d4
Albin Johansson <albinoscar99@hotmail.com>
2021-08-07 15:34:25 +0200

    Update map_import_error_dialog.hpp

--- src/gui/widgets/dialogs/map_import_error_dialog.hpp
+++ src/gui/widgets/dialogs/map_import_error_dialog.hpp
@@ -4,8 +4,23 @@
 
 namespace Tactile {
 
+/// \addtogroup gui
+/// \{
+
+/**
+ * \brief Updates the map parsing error dialog.
+ *
+ * \see `OpenMapImportErrorDialog()`
+ */
 void UpdateMapImportErrorDialog();
 
+/**
+ * \brief Opens the map parsing error dialog.
+ *
+ * \param error the parse error that will be displayed.
+ */
 void OpenMapImportErrorDialog(IO::ParseError error);
+
+/// \} End of group gui
 
 }  // namespace Tactile
