232e893c60e841db435b8b3a164bbe30dcb0890f
Albin Johansson <albinoscar99@hotmail.com>
2021-08-07 17:18:58 +0200

    Update save_as_dialog.hpp

--- src/gui/widgets/dialogs/save_as_dialog.hpp
+++ src/gui/widgets/dialogs/save_as_dialog.hpp
@@ -4,8 +4,25 @@
 
 namespace Tactile {
 
+/// \addtogroup gui
+/// \{
+
+/**
+ * \brief Updates the "Save As" dialog.
+ *
+ * \param dispatcher the event dispatcher that will be used.
+ *
+ * \see `OpenSaveAsDialog()`
+ */
 void UpdateSaveAsDialog(entt::dispatcher& dispatcher);
 
+/**
+ * \brief Opens the "Save As" dialog.
+ *
+ * \see `UpdateSaveAsDialog()`
+ */
 void OpenSaveAsDialog();
+
+/// \} End of group gui
 
 }  // namespace Tactile
