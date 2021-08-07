850a13f9d6fce92b7d431f4065294494b897c419
Albin Johansson <albinoscar99@hotmail.com>
2021-08-07 14:01:59 +0200

    Update render_map.hpp

--- src/gui/rendering/render_map.hpp
+++ src/gui/rendering/render_map.hpp
@@ -5,6 +5,14 @@ namespace Tactile {
 class MapDocument;
 struct RenderInfo;
 
+/**
+ * \brief Renders a map.
+ *
+ * \ingroup rendering
+ *
+ * \param document the map document that will have its map rendered.
+ * \param info the rendering context information that will be used.
+ */
 void RenderMap(const MapDocument& document, const RenderInfo& info);
 
 }  // namespace Tactile
