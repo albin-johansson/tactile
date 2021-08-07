92af94226abfe9ac58f5b03a96166fdaeee229cb
Albin Johansson <albinoscar99@hotmail.com>
2021-08-07 15:31:19 +0200

    Update add_map_dialog.hpp

--- src/gui/widgets/dialogs/add_map_dialog.hpp
+++ src/gui/widgets/dialogs/add_map_dialog.hpp
@@ -4,8 +4,25 @@
 
 namespace Tactile {
 
+/// \addtogroup gui
+/// \{
+
+/**
+ * \brief Updates the dialog for creating tilemaps.
+ *
+ * \param dispatcher the event dispatcher that will be used.
+ *
+ * \see `OpenAddMapDialog()`
+ */
 void UpdateAddMapDialog(entt::dispatcher& dispatcher);
 
+/**
+ * \brief Opens the dialog for creating new tilemaps.
+ *
+ * \see `UpdateAddMapDialog()`
+ */
 void OpenAddMapDialog();
+
+/// \} End of group gui
 
 }  // namespace Tactile
