143a7fc7f710ed847370ae7be9c2a7c5f707e1b6
Albin Johansson <albinoscar99@hotmail.com>
2021-08-07 15:27:31 +0200

    Update about_dialog.hpp

--- src/gui/widgets/dialogs/about_dialog.hpp
+++ src/gui/widgets/dialogs/about_dialog.hpp
@@ -2,6 +2,13 @@
 
 namespace Tactile {
 
+/**
+ * \brief Updates the "About Tactile" dialog.
+ *
+ * \ingroup gui
+ *
+ * \param[out] open a pointer to which the open state is written to.
+ */
 void UpdateAboutDialog(bool* open);
 
 }  // namespace Tactile
