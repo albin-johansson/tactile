96ab0b90e977d79388ba5387bf042b4ed8edf8ee
Albin Johansson <albinoscar99@hotmail.com>
2021-08-07 14:01:52 +0200

    Update render_tile.hpp

--- src/gui/rendering/render_tile.hpp
+++ src/gui/rendering/render_tile.hpp
@@ -8,6 +8,17 @@ namespace Tactile {
 
 class TilesetManager;
 
+/**
+ * \brief Renders a single tile.
+ *
+ * \ingroup rendering
+ *
+ * \param tile the ID of the tile that will be rendered.
+ * \param tilesets the available tilesets.
+ * \param screenPos the position of the rendered tile on the screen.
+ * \param gridSize the size of tiles in the viewport.
+ * \param opacity the opacity of the tile, in the range [0, 1].
+ */
 void RenderTile(tile_id tile,
                 const TilesetManager& tilesets,
                 const ImVec2& screenPos,
