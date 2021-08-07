e461769e2b5c769d7b6b073886a933d2c29ee41a
Albin Johansson <albinoscar99@hotmail.com>
2021-08-07 17:31:40 +0200

    Update layer_item_popup.hpp

--- src/gui/widgets/layers/layer_item_popup.hpp
+++ src/gui/widgets/layers/layer_item_popup.hpp
@@ -8,6 +8,15 @@ namespace Tactile {
 
 class MapDocument;
 
+/**
+ * \brief Updates the state of the layer item popup.
+ *
+ * \ingroup gui
+ *
+ * \param document the currently active map document.
+ * \param dispatcher the event dispatcher that will be used.
+ * \param id the ID of the pressed layer.
+ */
 void UpdateLayerItemPopup(const MapDocument& document,
                           entt::dispatcher& dispatcher,
                           layer_id id);
