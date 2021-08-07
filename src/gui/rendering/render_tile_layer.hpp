9c02f47386a3d4ab1c9eddaa9fb7d678df03ba10
Albin Johansson <albinoscar99@hotmail.com>
2021-08-07 14:01:49 +0200

    Update render_tile_layer.hpp

--- src/gui/rendering/render_tile_layer.hpp
+++ src/gui/rendering/render_tile_layer.hpp
@@ -6,6 +6,16 @@ class TileLayer;
 class TilesetManager;
 struct RenderInfo;
 
+/**
+ * \brief Renders a tile layer.
+ *
+ * \ingroup rendering
+ *
+ * \param layer the tile layer that will be rendered.
+ * \param tilesets the available tilesets.
+ * \param info the rendering context information that will be used.
+ * \param parentOpacity the opacity of the parent layer.
+ */
 void RenderTileLayer(const TileLayer& layer,
                      const TilesetManager& tilesets,
                      const RenderInfo& info,
