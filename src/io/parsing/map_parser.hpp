c9ced19133651fd22a0193ea0e978fb68dad717b
Albin Johansson <albinoscar99@hotmail.com>
2021-08-07 18:36:55 +0200

    Update map_parser.hpp

--- src/io/parsing/map_parser.hpp
+++ src/io/parsing/map_parser.hpp
@@ -9,21 +9,55 @@
 
 namespace Tactile::IO {
 
+/**
+ * \brief Represents the core parser engine for map files.
+ *
+ * \details The parser supports the JSON and TMX (XML) Tiled formats.
+ *
+ * \ingroup io
+ *
+ * \see `ToMapDocument()`
+ * \see `GetCause(ParseError)`
+ */
 class MapParser final
 {
  public:
+  /**
+   * \brief Parsers the map file at the specified path.
+   *
+   * \details The map file should use either of the Tiled JSON or TMX formats.
+   *
+   * \param path the file path of the map file.
+   */
   explicit MapParser(const std::filesystem::path& path);
 
+  /**
+   * \brief Indicates whether or not the map was successfully parsed.
+   *
+   * \return `true` if the map was parsed successfully parsed; `false` otherwise.
+   */
   [[nodiscard]] explicit operator bool() const noexcept
   {
     return mError == ParseError::None;
   }
 
+  /**
+   * \brief Returns the error code associated with the parser.
+   *
+   * \return the error code.
+   */
   [[nodiscard]] auto GetError() const noexcept -> ParseError
   {
     return mError;
   }
 
+  /**
+   * \brief Returns the intermediate representation of the parsed map.
+   *
+   * \return the intermediate representation of the parsed map.
+   *
+   * \see `ToMapDocument()`
+   */
   [[nodiscard]] auto GetData() const noexcept -> const MapData&
   {
     return mData;
