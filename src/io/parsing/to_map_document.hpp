63909edc3a28adafa4c25e2edb5f9f565983954b
Albin Johansson <albinoscar99@hotmail.com>
2021-08-07 18:29:29 +0200

    Update to_map_document.hpp

--- src/io/parsing/to_map_document.hpp
+++ src/io/parsing/to_map_document.hpp
@@ -6,6 +6,15 @@
 
 namespace Tactile::IO {
 
+/**
+ * \brief Creates a map document from an intermediate map representation.
+ *
+ * \ingroup io
+ *
+ * \param data the intermediate representation of the map data.
+ *
+ * \return a map document based on the intermediate representation.
+ */
 [[nodiscard]] auto ToMapDocument(const MapData& data) -> Unique<MapDocument>;
 
 }  // namespace Tactile::IO
