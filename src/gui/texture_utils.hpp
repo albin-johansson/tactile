1259890029c3d599df255454c0644c2c9f19849d
Albin Johansson <albinoscar99@hotmail.com>
2021-08-07 14:34:47 +0200

    Update texture_utils.hpp

--- src/gui/texture_utils.hpp
+++ src/gui/texture_utils.hpp
@@ -6,6 +6,15 @@
 
 namespace Tactile {
 
+/**
+ * \brief Converts an OpenGL texture identifier to a Dear ImGui texture identifier.
+ *
+ * \ingroup gui
+ *
+ * \param texture the OpenGL texture identifier that will be converted.
+ *
+ * \return a corresponding Dear ImGui texture identifier.
+ */
 [[nodiscard]] auto ToTextureID(uint texture) -> ImTextureID;
 
 }  // namespace Tactile
