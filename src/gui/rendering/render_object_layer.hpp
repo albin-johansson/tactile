2328d6c636f6f1f05095ae588e1c12842e971309
Albin Johansson <albinoscar99@hotmail.com>
2021-08-07 14:01:55 +0200

    Update render_object_layer.hpp

--- src/gui/rendering/render_object_layer.hpp
+++ src/gui/rendering/render_object_layer.hpp
@@ -5,6 +5,15 @@ namespace Tactile {
 class ObjectLayer;
 struct RenderInfo;
 
+/**
+ * \brief Renders an object layer.
+ *
+ * \ingroup rendering
+ *
+ * \param layer the object layer that will be rendered.
+ * \param info the rendering context information that will be used.
+ * \param parentOpacity the opacity of the parent layer.
+ */
 void RenderObjectLayer(const ObjectLayer& layer,
                        const RenderInfo& info,
                        float parentOpacity);
