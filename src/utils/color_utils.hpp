0f395e68fe1d635450574920f4319704c4f64621
Albin Johansson <albinoscar99@hotmail.com>
2021-08-08 02:15:27 +0200

    Update color_utils.hpp

--- src/utils/color_utils.hpp
+++ src/utils/color_utils.hpp
@@ -5,6 +5,17 @@
 
 namespace Tactile {
 
+/**
+ * \brief Converts a color into an array of normalized color components.
+ *
+ * \details The array values are stored in the order red/green/blue/alpha.
+ *
+ * \ingroup utils
+ *
+ * \param color the color that will be converted.
+ *
+ * \return an array of normalized color components.
+ */
 [[nodiscard]] constexpr auto ColorToArray(const cen::color& color) noexcept
     -> std::array<float, 4>
 {
