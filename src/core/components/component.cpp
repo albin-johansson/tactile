d8a41a54987d0a3249d29b8e141fe709ac97b496
Albin Johansson <albinoscar99@hotmail.com>
2021-08-22 22:12:26 +0200

    Update component.cpp

--- src/core/components/component.cpp
+++ src/core/components/component.cpp
@@ -2,6 +2,47 @@
 
 #include <utility>  // move
 
+/*
+ <components>
+   <component name="Movable">
+     <properties>
+       <property name="PositionX" type="float" value="0" />
+       <property name="PositionY" type="float" value="0" />
+       <property name="VelocityX" type="float" value="0" />
+       <property name="VelocityY" type="float" value="0" />
+     </properties>
+   </component>
+ </components>
+
+ "components": [
+   {
+     "name": "Movable",
+     "properties": [
+       {
+         "name": "PositionX",
+         "type": "float",
+         "value": 0
+       },
+       {
+         "name": "PositionY",
+         "type": "float",
+         "value": 0
+       },
+       {
+         "name": "VelocityX",
+         "type": "float",
+         "value": 0
+       },
+       {
+         "name": "VelocityY",
+         "type": "float",
+         "value": 0
+       }
+     ]
+   }
+ ]
+ */
+
 namespace Tactile {
 
 Component::Component(std::string type) : mType{std::move(type)}
