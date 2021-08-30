76bc30e34e8f168affd7190a30fe38c402590511
Albin Johansson <albinoscar99@hotmail.com>
2021-08-30 23:56:11 +0200

    Reintroduce LayerType

---
+++ src/core/layer_type.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace Tactile {
+
+/**
+ * \brief Represents all different layer types.
+ *
+ * \ingroup core
+ */
+enum class LayerType
+{
+  TileLayer,
+  ObjectLayer,
+  GroupLayer
+};
+
+}  // namespace Tactile
