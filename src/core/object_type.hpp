2f937ca2e0b6682cb5a1f67b52982dff4bb51215
Albin Johansson <albinoscar99@hotmail.com>
2021-08-30 23:57:09 +0200

    Reintroduce ObjectType

---
+++ src/core/object_type.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+namespace Tactile {
+
+/**
+ * \brief Represents the different available map object types.
+ *
+ * \ingroup core
+ *
+ * \todo Polygon, Polyline, Text.
+ */
+enum class ObjectType
+{
+  Point,
+  Rectangle,
+  Ellipse
+};
+
+}  // namespace Tactile
