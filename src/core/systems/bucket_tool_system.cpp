72b493b939f58e5bafc1a24f9c666484511b0e25
Albin Johansson <albinoscar99@hotmail.com>
2021-09-05 01:03:26 +0200

    Update bucket_tool_system.cpp

--- src/core/systems/bucket_tool_system.cpp
+++ src/core/systems/bucket_tool_system.cpp
@@ -13,16 +13,15 @@ void BucketToolOnPressed(entt::registry& registry,
                          entt::dispatcher& dispatcher,
                          const MouseInfo& mouse)
 {
-  if (Sys::IsTileLayerActive(registry) &&
-      Sys::IsSingleTileSelectedInTileset(registry) &&
+  if (IsTileLayerActive(registry) && IsSingleTileSelectedInTileset(registry) &&
       mouse.button == cen::mouse_button::left)
   {
-    const auto entity = Sys::GetActiveTileset(registry);
+    const auto entity = GetActiveTileset(registry);
     assert(entity != entt::null);
 
     const auto& selection = registry.get<TilesetSelection>(entity);
     const auto position = selection.region->begin;
-    const auto replacement = Sys::GetTileFromTileset(registry, entity, position);
+    const auto replacement = GetTileFromTileset(registry, entity, position);
 
     dispatcher.enqueue<FloodEvent>(mouse.position_in_map, replacement);
   }
