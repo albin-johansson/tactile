cd777c1d4a4fdb2d5eb56d0ee005b8383ceb1e50
Albin Johansson <albinoscar99@hotmail.com>
2021-09-15 13:01:37 +0200

    Update file_property_widget.cpp

--- src/gui/widgets/properties/file_property_widget.cpp
+++ src/gui/widgets/properties/file_property_widget.cpp
@@ -1,7 +1,13 @@
 #include "file_property_widget.hpp"
 
-#include <imgui.h>
+#define NOMINMAX
 
+#include <imgui.h>
+#include <portable-file-dialogs.h>
+
+#include <limits>  // numeric_limits
+
+#include "gui/icons.hpp"
 #include "utils/scope_id.hpp"
 
 namespace Tactile {
@@ -11,9 +17,25 @@ auto FilePropertyWidget(const PropertyValue& property)
 {
   const ScopeID id{&property};
 
+  if (ImGui::Button(TAC_ICON_SELECT_FILE))
+  {
+    auto files = pfd::open_file{"Select File..."}.result();
+    if (!files.empty())
+    {
+      return files.front();
+    }
+  }
+
   const auto& path = property.AsFile();
-  const auto str = path.filename().string();
-  ImGui::TextUnformatted(str.c_str());
+  auto str = path.filename().string();
+
+  ImGui::SameLine();
+  ImGui::SetNextItemWidth(-std::numeric_limits<float>::min());
+  ImGui::InputTextWithHint("##FilePropertyInput",
+                           "N/A",
+                           str.data(),
+                           str.capacity(),
+                           ImGuiInputTextFlags_ReadOnly);
 
   if (ImGui::IsItemHovered())
   {
