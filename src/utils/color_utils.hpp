2bff31f495bf2618bbb9c5033dfb587457530eed
Albin Johansson <albinoscar99@hotmail.com>
2021-09-20 00:23:01 +0200

    Update color_utils.hpp

--- src/utils/color_utils.hpp
+++ src/utils/color_utils.hpp
@@ -19,10 +19,10 @@ namespace Tactile {
 [[nodiscard]] constexpr auto ColorToArray(const cen::color& color) noexcept
     -> std::array<float, 4>
 {
-  const auto r = static_cast<float>(color.red()) / 255.0f;
-  const auto g = static_cast<float>(color.green()) / 255.0f;
-  const auto b = static_cast<float>(color.blue()) / 255.0f;
-  const auto a = static_cast<float>(color.alpha()) / 255.0f;
+  const auto r = color.red_norm();
+  const auto g = color.green_norm();
+  const auto b = color.blue_norm();
+  const auto a = color.alpha_norm();
   return {r, g, b, a};
 }
 
