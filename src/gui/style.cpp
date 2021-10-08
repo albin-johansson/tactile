a3968bef8cf3552c8f27e7f74bd14a2500b949bf
Albin Johansson <albinoscar99@hotmail.com>
2021-10-09 00:56:57 +0200

    Update style.cpp

--- src/gui/style.cpp
+++ src/gui/style.cpp
@@ -6,25 +6,28 @@ namespace Tactile {
 
 void ApplyStyle(ImGuiStyle& style)
 {
+  style.WindowMenuButtonPosition = ImGuiDir_Right;
   style.WindowBorderSize = 0;
 
   style.WindowPadding = ImVec2{10, 10};
   style.FramePadding = ImVec2{5, 5};
   style.CellPadding = ImVec2{4, 4};
 
-  style.ItemSpacing = ImVec2{10, 8};
+  style.ItemSpacing = ImVec2{12, 8};
   style.ItemInnerSpacing = ImVec2{4, 6};
   style.IndentSpacing = 15;
 
   style.GrabMinSize = 6;
   style.ScrollbarSize = 10;
 
-  const float rounding = 2;
+  constexpr float rounding = 4;
   style.WindowRounding = rounding;
+  style.ChildRounding = rounding;
   style.FrameRounding = rounding;
   style.PopupRounding = rounding;
   style.ScrollbarRounding = rounding;
   style.GrabRounding = rounding;
+  style.LogSliderDeadzone = rounding;
   style.TabRounding = rounding;
 }
 
