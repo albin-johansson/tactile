9477221bd5ad52aea54a099c2ecab334b263ab05
Albin Johansson <albinoscar99@hotmail.com>
2021-10-11 10:55:35 +0200

    Add Menu utility class

---
+++ src/gui/widgets/common/menu.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <imgui.h>
+
+#include "common/cstr.hpp"
+
+namespace Tactile {
+
+class Menu final
+{
+ public:
+  explicit Menu(const CStr name, const bool enabled = true)
+      : mOpen{ImGui::BeginMenu(name, enabled)}
+  {}
+
+  ~Menu()
+  {
+    if (mOpen) {
+      ImGui::EndMenu();
+    }
+  }
+
+  explicit operator bool() const noexcept
+  {
+    return mOpen;
+  }
+
+ private:
+  bool mOpen{};
+};
+
+}  // namespace Tactile
