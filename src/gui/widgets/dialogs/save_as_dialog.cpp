ea845ca9622362be8285bc6bc3d022575c3846c7
Albin Johansson <albinoscar99@hotmail.com>
2021-10-16 19:02:03 +0200

    Fix issue with "Save As" dialog

--- src/gui/widgets/dialogs/save_as_dialog.cpp
+++ src/gui/widgets/dialogs/save_as_dialog.cpp
@@ -18,17 +18,17 @@ void UpdateSaveAsDialog(entt::dispatcher& dispatcher)
 {
   if (show) {
     auto path =
-        pfd::save_file{"Save as...",
-                       "",
-                       {"JSON File", "*.json", "TMX File", "*.tmx", "XML File", "*.xml"}}
+        pfd::save_file{"Save as...", "", {"Map File", "*.json *.tmx *.xml *.yml *.yaml"}}
             .result();
 
-    if (!path.ends_with(".json") && !path.ends_with(".tmx") && !path.ends_with(".xml")) {
-      CENTURION_LOG_INFO("No suffix in requested file path, using preferred format...");
-      path += (Prefs::GetPreferredFormat() == "JSON") ? ".json" : ".tmx";
-    }
-
     if (!path.empty()) {
+      if (!path.ends_with(".json") && !path.ends_with(".tmx") &&
+          !path.ends_with(".xml") && !path.ends_with(".yml") && !path.ends_with(".yaml"))
+      {
+        CENTURION_LOG_INFO("No suffix in requested file path, using preferred format...");
+        path += (Prefs::GetPreferredFormat() == "JSON") ? ".json" : ".tmx";
+      }
+
       dispatcher.enqueue<SaveAsEvent>(std::move(path));
     }
 
