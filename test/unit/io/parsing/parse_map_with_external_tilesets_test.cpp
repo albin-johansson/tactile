46dd7c9affd4e61d46fe48721d547620e5aed5b9
Albin Johansson <albinoscar99@hotmail.com>
2021-10-21 23:59:34 +0200

    Update parse_map_with_external_tilesets_test.cpp

--- test/unit/io/parsing/parse_map_with_external_tilesets_test.cpp
+++ test/unit/io/parsing/parse_map_with_external_tilesets_test.cpp
@@ -46,13 +46,13 @@ void VerifyLayers(const MapData& data)
 
     const auto& a = layer.properties.at(0);
     ASSERT_EQ("a", a.name);
-    ASSERT_EQ(PropertyType::Integer, a.property.GetType());
-    ASSERT_EQ(42, a.property.AsInt());
+    ASSERT_EQ(PropertyType::Integer, a.value.GetType());
+    ASSERT_EQ(42, a.value.AsInt());
 
     const auto& b = layer.properties.at(1);
     ASSERT_EQ("b", b.name);
-    ASSERT_EQ(PropertyType::Floating, b.property.GetType());
-    ASSERT_FLOAT_EQ(2.5f, b.property.AsFloat());
+    ASSERT_EQ(PropertyType::Floating, b.value.GetType());
+    ASSERT_FLOAT_EQ(2.5f, b.value.AsFloat());
   }
 
   {
@@ -81,8 +81,8 @@ void VerifyLayers(const MapData& data)
 
     const auto& foo = rect.properties.at(0);
     ASSERT_EQ("foo", foo.name);
-    ASSERT_EQ(PropertyType::String, foo.property.GetType());
-    ASSERT_EQ("bar", foo.property.AsString());
+    ASSERT_EQ(PropertyType::String, foo.value.GetType());
+    ASSERT_EQ("bar", foo.value.AsString());
 
     const auto& point = objectLayer.objects.at(1);
     ASSERT_EQ(3_obj, point.id);
@@ -129,14 +129,14 @@ void VerifyProperties(const MapData& data)
 
   const auto& boolean = data.properties.at(0);
   ASSERT_EQ("boolean", boolean.name);
-  ASSERT_EQ(PropertyType::Boolean, boolean.property.GetType());
-  ASSERT_TRUE(boolean.property.AsBool());
+  ASSERT_EQ(PropertyType::Boolean, boolean.value.GetType());
+  ASSERT_TRUE(boolean.value.AsBool());
 
   const auto& color = data.properties.at(1);
   ASSERT_EQ("cool color", color.name);
-  ASSERT_EQ(PropertyType::Color, color.property.GetType());
+  ASSERT_EQ(PropertyType::Color, color.value.GetType());
   {
-    const auto value = color.property.AsColor();
+    const auto value = color.value.AsColor();
     ASSERT_EQ(0x1A, value.red());
     ASSERT_EQ(0x2B, value.green());
     ASSERT_EQ(0x3C, value.blue());
@@ -145,28 +145,28 @@ void VerifyProperties(const MapData& data)
 
   const auto& floating = data.properties.at(2);
   ASSERT_EQ("floating", floating.name);
-  ASSERT_EQ(PropertyType::Floating, floating.property.GetType());
-  ASSERT_FLOAT_EQ(12.3f, floating.property.AsFloat());
+  ASSERT_EQ(PropertyType::Floating, floating.value.GetType());
+  ASSERT_FLOAT_EQ(12.3f, floating.value.AsFloat());
 
   const auto& foo = data.properties.at(3);
   ASSERT_EQ("foo", foo.name);
-  ASSERT_EQ(PropertyType::String, foo.property.GetType());
-  ASSERT_EQ("bar", foo.property.AsString());
+  ASSERT_EQ(PropertyType::String, foo.value.GetType());
+  ASSERT_EQ("bar", foo.value.AsString());
 
   const auto& integer = data.properties.at(4);
   ASSERT_EQ("integer", integer.name);
-  ASSERT_EQ(PropertyType::Integer, integer.property.GetType());
-  ASSERT_EQ(42, integer.property.AsInt());
+  ASSERT_EQ(PropertyType::Integer, integer.value.GetType());
+  ASSERT_EQ(42, integer.value.AsInt());
 
   const auto& object = data.properties.at(5);
   ASSERT_EQ("object ref", object.name);
-  ASSERT_EQ(PropertyType::Object, object.property.GetType());
-  ASSERT_EQ(1_obj, object.property.AsObject());
+  ASSERT_EQ(PropertyType::Object, object.value.GetType());
+  ASSERT_EQ(1_obj, object.value.AsObject());
 
   const auto& path = data.properties.at(6);
   ASSERT_EQ("path", path.name);
-  ASSERT_EQ(PropertyType::File, path.property.GetType());
-  ASSERT_EQ(std::filesystem::path{"foo.bar"}, path.property.AsFile());
+  ASSERT_EQ(PropertyType::File, path.value.GetType());
+  ASSERT_EQ(std::filesystem::path{"foo.bar"}, path.value.AsFile());
 }
 
 void VerifyMap(const std::filesystem::path& path)
