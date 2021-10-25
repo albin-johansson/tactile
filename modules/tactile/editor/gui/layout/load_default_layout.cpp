d15b945493a7fd2b9e289b1d36594420cf313a5d
Albin Johansson <albinoscar99@hotmail.com>
2021-10-25 21:34:22 +0200

    Update load_default_layout.cpp

--- modules/tactile/editor/gui/layout/load_default_layout.cpp
+++ modules/tactile/editor/gui/layout/load_default_layout.cpp
@@ -12,14 +12,14 @@ void LoadDefaultLayout(ImGuiID id, const bool resetVisibility)
 
   // clang-format off
   const auto root = id;
-  const auto top = ImGui::DockBuilderSplitNode(id, ImGuiDir_Up, 0.04f, nullptr, &id);
+  const auto left = ImGui::DockBuilderSplitNode(id, ImGuiDir_Left, 0.04f, nullptr, &id);
 
   auto right = ImGui::DockBuilderSplitNode(id, ImGuiDir_Right, 0.20f, nullptr, &id);
   const auto rightBottom = ImGui::DockBuilderSplitNode(right, ImGuiDir_Down, 0.25f, nullptr, &right);
   // clang-format on
 
   ImGui::DockBuilderDockWindow("Viewport", root);
-  ImGui::DockBuilderDockWindow("Toolbar", top);
+  ImGui::DockBuilderDockWindow("Toolbar", left);
   ImGui::DockBuilderDockWindow("Tilesets", right);
   ImGui::DockBuilderDockWindow("Properties", right);
   ImGui::DockBuilderDockWindow("Layers", rightBottom);
