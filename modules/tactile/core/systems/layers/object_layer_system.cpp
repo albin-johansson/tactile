b8657e8aa77b2561392fe93db51033a8e24fb4be
Albin Johansson <albinoscar99@hotmail.com>
2021-10-28 14:30:21 +0200

    Add function for looking up objects at specific viewport coordinates

--- modules/tactile/core/systems/layers/object_layer_system.cpp
+++ modules/tactile/core/systems/layers/object_layer_system.cpp
@@ -1,9 +1,42 @@
 #include "object_layer_system.hpp"
 
+#include <cassert>  // assert
+
 #include "core/components/object.hpp"
 #include "core/components/object_layer.hpp"
+#include "core/map.hpp"
+#include "core/systems/viewport_system.hpp"
+#include "core/viewport.hpp"
 
 namespace Tactile::Sys {
+namespace {
+
+[[nodiscard]] auto GetHitDetectionBounds(const Object& object,
+                                         const float mapTileWidth,
+                                         const float mapTileHeight,
+                                         const float xRatio,
+                                         const float yRatio) -> cen::frect
+{
+  /* Points have no width or height, so we have to create a hitbox large enough to be easy
+     for the user to click */
+  if (object.type == ObjectType::Point) {
+    const auto width = static_cast<float>(mapTileWidth) / 2.0f;
+    const auto height = static_cast<float>(mapTileHeight) / 2.0f;
+
+    return cen::rect((object.x - (width / 2.0f)) * xRatio,
+                     (object.y - (height / 2.0f)) * yRatio,
+                     width * xRatio,
+                     height * yRatio);
+  }
+  else {
+    return cen::rect(object.x * xRatio,
+                     object.y * yRatio,
+                     object.width * xRatio,
+                     object.height * yRatio);
+  }
+}
+
+}  // namespace
 
 auto HasObject(const entt::registry& registry,
                const entt::entity entity,
@@ -21,6 +54,33 @@ auto FindObject(const entt::registry& registry,
   for (const auto objectEntity : layer.objects) {
     const auto& object = registry.get<Object>(objectEntity);
     if (object.id == id) {
+      return objectEntity;
+    }
+  }
+
+  return entt::null;
+}
+
+auto FindObject(const entt::registry& registry,
+                const entt::entity entity,
+                const float x,
+                const float y) -> entt::entity
+{
+  assert(entity != entt::null);
+  assert(registry.all_of<ObjectLayer>(entity));
+
+  const auto& map = registry.ctx<Map>();
+  const auto [xRatio, yRatio] = GetViewportScalingRatio(registry);
+
+  const auto& layer = registry.get<ObjectLayer>(entity);
+  for (const auto objectEntity : layer.objects) {
+    const auto& object = registry.get<Object>(objectEntity);
+    const auto bounds = GetHitDetectionBounds(object,
+                                              static_cast<float>(map.tile_width),
+                                              static_cast<float>(map.tile_height),
+                                              xRatio,
+                                              yRatio);
+    if (bounds.contains({x, y})) {
       return objectEntity;
     }
   }
--- modules/tactile/core/systems/layers/object_layer_system.hpp
+++ modules/tactile/core/systems/layers/object_layer_system.hpp
@@ -17,6 +17,29 @@ namespace Tactile::Sys {
                               entt::entity entity,
                               ObjectID id) -> entt::entity;
 
+/**
+ * \brief Attempts to find an object in the specified layer that contains the supplied
+ * coordinates.
+ *
+ * \details This function is designed to be used to find objects at the location that the
+ * user has clicked in the map. As a result, this function takes care of the relationship
+ * between viewport coordinates and logical coordinates. It also handles aspects such as
+ * points having no width/height, so that points can be detected using this approach as
+ * well.
+ *
+ * \param registry the current map registry.
+ * \param entity the object layer entity.
+ * \param x the viewport relative x-coordinate.
+ * \param y the viewport relative y-coordinate.
+ *
+ * \return an object entity that contains the specified coordinates; a null entity is
+ * returned if there is none.
+ */
+[[nodiscard]] auto FindObject(const entt::registry& registry,
+                              entt::entity entity,
+                              float x,
+                              float y) -> entt::entity;
+
 /// \} End of object layer system
 
 }  // namespace Tactile::Sys
