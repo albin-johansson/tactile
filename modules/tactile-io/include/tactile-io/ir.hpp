faa830335bb4d49fdd2e2ba91247275e8fa129d1
Albin Johansson <albinoscar99@hotmail.com>
2021-11-01 18:28:39 +0100

    Add ir_tileset.hpp

--- modules/tactile-io/include/tactile-io/ir.hpp
+++ modules/tactile-io/include/tactile-io/ir.hpp
@@ -10,49 +10,12 @@
 #include "api.hpp"
 #include "ir_common.hpp"
 #include "ir_map.hpp"
+#include "ir_tileset.hpp"
 
 namespace Tactile::IO {
 
 /// \addtogroup io
 /// \{
-
-/// \name Tileset API
-/// \{
-
-TACTILE_IO_API void SetName(Tileset& tileset, CStr name);
-TACTILE_IO_API void SetFirstGlobalId(Tileset& tileset, int32 id);
-TACTILE_IO_API void SetTileWidth(Tileset& tileset, int32 width);
-TACTILE_IO_API void SetTileHeight(Tileset& tileset, int32 height);
-TACTILE_IO_API void SetTileCount(Tileset& tileset, int32 count);
-TACTILE_IO_API void SetColumnCount(Tileset& tileset, int32 columns);
-TACTILE_IO_API void SetImagePath(Tileset& tileset, CPathStr path);
-TACTILE_IO_API void SetImageWidth(Tileset& tileset, int32 width);
-TACTILE_IO_API void SetImageHeight(Tileset& tileset, int32 height);
-
-TACTILE_IO_API void ReserveTiles(Tileset& tileset, usize n);
-TACTILE_IO_API void ReserveProperties(Tileset& tileset, usize n);
-
-TACTILE_IO_API_QUERY auto AddTile(Tileset& tileset) -> Tile&;
-TACTILE_IO_API_QUERY auto AddProperty(Tileset& tileset) -> Property&;
-
-TACTILE_IO_API_QUERY auto GetName(const Tileset& tileset) -> CStr;
-TACTILE_IO_API_QUERY auto GetFirstGlobalId(const Tileset& tileset) -> int32;
-TACTILE_IO_API_QUERY auto GetTileWidth(const Tileset& tileset) -> int32;
-TACTILE_IO_API_QUERY auto GetTileHeight(const Tileset& tileset) -> int32;
-TACTILE_IO_API_QUERY auto GetTileCount(const Tileset& tileset) -> int32;
-TACTILE_IO_API_QUERY auto GetColumnCount(const Tileset& tileset) -> int32;
-TACTILE_IO_API_QUERY auto GetImagePath(const Tileset& tileset) -> CPathStr;
-TACTILE_IO_API_QUERY auto GetImageWidth(const Tileset& tileset) -> int32;
-TACTILE_IO_API_QUERY auto GetImageHeight(const Tileset& tileset) -> int32;
-
-TACTILE_IO_API_QUERY auto GetTileInfoCount(const Tileset& tileset) -> usize;
-TACTILE_IO_API_QUERY auto GetTileInfo(const Tileset& tileset, usize index) -> const Tile&;
-
-TACTILE_IO_API_QUERY auto GetPropertyCount(const Tileset& tileset) -> usize;
-TACTILE_IO_API_QUERY auto GetProperty(const Tileset& tileset, usize index)
-    -> const Property&;
-
-/// \} End of tileset API
 
 /// \name Tile API
 /// \{
