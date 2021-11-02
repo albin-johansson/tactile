4898c3c106beefd59c067a3e19981ef8163a3524
Albin Johansson <albinoscar99@hotmail.com>
2021-11-02 15:30:11 +0100

    Add tactile_io.hpp

---
+++ modules/tactile-io/include/tactile_io.hpp
@@ -0,0 +1,18 @@
+#ifndef TACTILE_IO_TACTILE_IO_HPP_
+#define TACTILE_IO_TACTILE_IO_HPP_
+
+/* This convenience header includes every public header of the TactileIO library  */
+
+#include "tactile-io/api.hpp"
+#include "tactile-io/emitter.hpp"
+#include "tactile-io/ir.hpp"
+#include "tactile-io/ir_common.hpp"
+#include "tactile-io/ir_map.hpp"
+#include "tactile-io/ir_object.hpp"
+#include "tactile-io/ir_property.hpp"
+#include "tactile-io/ir_tile.hpp"
+#include "tactile-io/ir_tileset.hpp"
+#include "tactile-io/parse_error.hpp"
+#include "tactile-io/parser.hpp"
+
+#endif  // TACTILE_IO_TACTILE_IO_HPP_
