27913dc6e1f845bb693e1f922e35ca71db6b675f
Albin Johansson <albinoscar99@hotmail.com>
2021-11-17 18:24:17 +0100

    io: remove redundant aliases in ir_common.hpp

--- modules/tactile-io/include/tactile-io/ir_common.hpp
+++ modules/tactile-io/include/tactile-io/ir_common.hpp
@@ -3,7 +3,9 @@
 
 #include <cstddef>     // size_t
 #include <cstdint>     // uint8_t, int32_t
-#include <filesystem>  // path::value_type
+#include <filesystem>  // path
+
+#include <tactile_def.hpp>
 
 #include "api.hpp"
 
@@ -20,14 +22,6 @@ struct GroupLayer;
 struct Tile;
 struct Tileset;
 struct Map;
-
-using usize = std::size_t;     ///< Large unsigned integer.
-using uint8 = std::uint8_t;    ///< 8-bit unsigned integer.
-using uint32 = std::uint32_t;  ///< 32-bit unsigned integer.
-using int32 = std::int32_t;    ///< 32-bit signed integer.
-
-/// \brief Alias for a C-style string.
-using CStr = const char*;
 
 /// \brief Alias for a C-style string using the native preferred path `char` type.
 using CPathStr = const std::filesystem::path::value_type*;
