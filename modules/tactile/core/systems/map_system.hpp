6a4ab01b69a7fc879e42f0dcdc6b19851549ca2a
Albin Johansson <albinoscar99@hotmail.com>
2021-11-21 10:40:51 +0100

    Update map_system.hpp

--- modules/tactile/core/systems/map_system.hpp
+++ modules/tactile/core/systems/map_system.hpp
@@ -11,16 +11,55 @@ namespace Tactile::Sys {
 /// \name Map system
 /// \{
 
+/**
+ * \brief Appends a row of empty tiles to a map.
+ *
+ * \param registry the map registry that will be modified.
+ */
 void AddRow(entt::registry& registry);
 
+/**
+ * \brief Appends a column of empty tiles to a map.
+ *
+ * \param registry the map registry that will be modified.
+ */
 void AddColumn(entt::registry& registry);
 
+/**
+ * \brief Removes the last row of tiles from a map.
+ *
+ * \details This function has no effect if there is only one row in the map.
+ *
+ * \param registry the map registry that will be modified.
+ */
 void RemoveRow(entt::registry& registry);
 
+/**
+ * \brief Removes the last column of tiles from a map.
+ *
+ * \details This function has no effect if there is only one column in the map.
+ *
+ * \param registry the map registry that will be modified.
+ */
 void RemoveColumn(entt::registry& registry);
 
+/**
+ * \brief Sets the size of a map.
+ *
+ * \param registry the map registry that will be modified.
+ * \param nRows the new number of rows.
+ * \param nCols the new number of columns.
+ */
 void ResizeMap(entt::registry& registry, usize nRows, usize nCols);
 
+/**
+ * \brief Indicates whether or not a position is within the bounds of a map.
+ *
+ * \param registry the map registry that will be queried.
+ * \param position the position that will be checked.
+ *
+ * \return `true` if the position is within the map; `false` otherwise.
+ */
 [[nodiscard]] auto IsPositionInMap(const entt::registry& registry,
                                    const MapPosition& position) -> bool;
 
