9f7aef7e319f5c5b8ff85fce4e7d518a74fffa81
Albin Johansson <albinoscar99@hotmail.com>
2021-11-21 10:28:43 +0100

    Update object_system.hpp

--- modules/tactile/core/systems/object_system.hpp
+++ modules/tactile/core/systems/object_system.hpp
@@ -6,7 +6,20 @@
 
 namespace Tactile::Sys {
 
+/// \name Object system
+/// \{
+
+/**
+ * \brief Returns the object entity associated with a specific ID.
+ *
+ * \param registry the registry that will be queried.
+ * \param id the ID associated with the desired object entity.
+ *
+ * \return the found entity; the null entity is returned if no match is found.
+ */
 [[nodiscard]] auto FindObject(const entt::registry& registry, ObjectID id)
     -> entt::entity;
+
+/// \} End of object system
 
 }  // namespace Tactile::Sys
