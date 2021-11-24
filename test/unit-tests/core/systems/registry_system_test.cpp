e7835799f4d16b8183cee5910ac2062d56b89962
Albin Johansson <albinoscar99@hotmail.com>
2021-11-24 10:26:34 +0100

    Rename registry system tests

--- test/unit-tests/core/systems/registry_system_test.cpp
+++ test/unit-tests/core/systems/registry_system_test.cpp
@@ -1,8 +1,10 @@
 #include <gtest/gtest.h>
 
 #include "core/components/layer.hpp"
+#include "core/components/object.hpp"
 #include "core/components/property_context.hpp"
 #include "core/components/tileset.hpp"
+#include "core/components/tool.hpp"
 #include "core/map.hpp"
 #include "core/mouse_pos.hpp"
 #include "core/systems/registry_system.hpp"
@@ -10,13 +12,15 @@
 
 using namespace Tactile;
 
-TEST(RegistryFactorySystem, MakeRegistry)
+TEST(RegistrySystem, MakeRegistry)
 {
   const auto registry = Sys::MakeRegistry();
   ASSERT_TRUE(registry.try_ctx<Map>());
   ASSERT_TRUE(registry.try_ctx<ActiveLayer>());
   ASSERT_TRUE(registry.try_ctx<ActiveTileset>());
   ASSERT_TRUE(registry.try_ctx<ActivePropertyContext>());
+  ASSERT_TRUE(registry.try_ctx<ActiveTool>());
+  ASSERT_TRUE(registry.try_ctx<ActiveObject>());
   ASSERT_TRUE(registry.try_ctx<Viewport>());
   ASSERT_TRUE(registry.try_ctx<PropertyContext>());
   ASSERT_TRUE(registry.try_ctx<TilesetContext>());
