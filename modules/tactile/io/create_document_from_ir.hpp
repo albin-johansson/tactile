2404e61c2c6c5dd36a4de4b379e17e8d09e97f29
Albin Johansson <albinoscar99@hotmail.com>
2021-12-27 08:18:26 +0100

    Update create_document_from_ir.hpp

--- modules/tactile/io/create_document_from_ir.hpp
+++ modules/tactile/io/create_document_from_ir.hpp
@@ -11,12 +11,10 @@ class TextureManager;
 /**
  * \brief Creates a map document from an intermediate map representation.
  *
- * \ingroup io
- *
  * \param data the intermediate representation of the map data.
  * \param textures the texture manager that will be used.
  *
- * \return a map document based on the intermediate representation.
+ * \return a document based on the intermediate representation.
  */
 [[nodiscard]] auto CreateDocumentFromIR(const IO::Map& data, TextureManager& textures)
     -> Document;
