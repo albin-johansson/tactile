1df1686493b079c6288f28ca10dbef8fe95a8cab
Albin Johansson <albinoscar99@hotmail.com>
2021-12-28 08:05:02 +0100

    Update imgui_context.hpp

--- modules/tactile/cfg/imgui_context.hpp
+++ modules/tactile/cfg/imgui_context.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <centurion.hpp>
+#include <tactile_def.hpp>
 
 namespace Tactile {
 
@@ -10,6 +11,9 @@ class ImGuiContext final {
   ImGuiContext(cen::Window& window, cen::GLContext& context);
 
   ~ImGuiContext();
+
+  TACTILE_DEFAULT_COPY(ImGuiContext)
+  TACTILE_DEFAULT_MOVE(ImGuiContext)
 
  private:
   /* This is used as a workaround to annoying unit test issues for Windows CI builds */
