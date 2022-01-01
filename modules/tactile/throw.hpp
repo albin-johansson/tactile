a6389e8a8dbdf2462c35993eaa06e563e6db706e
Albin Johansson <albinoscar99@hotmail.com>
2022-01-01 01:39:21 +0100

    Ignore sign conversion warnings from boost::stacktrace

--- modules/tactile/throw.hpp
+++ modules/tactile/throw.hpp
@@ -1,7 +1,18 @@
 #pragma once
 
+#include "build.hpp"
+
+#if TACTILE_COMPILER_GCC || TACTILE_COMPILER_CLANG
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wsign-conversion"
+#endif  // TACTILE_COMPILER_GCC || TACTILE_COMPILER_CLANG
+
 #include <boost/exception/all.hpp>
 #include <boost/stacktrace.hpp>
+
+#if TACTILE_COMPILER_GCC || TACTILE_COMPILER_CLANG
+#pragma GCC diagnostic pop
+#endif  // TACTILE_COMPILER_GCC || TACTILE_COMPILER_CLANG
 
 namespace Tactile {
 
