949a1eb8d0c3ff472eda71bfafb516987eb0dec6
Albin Johansson <albinoscar99@hotmail.com>
2022-01-02 14:24:43 +0100

    Update property_context.hpp

--- modules/tactile/core/components/property_context.hpp
+++ modules/tactile/core/components/property_context.hpp
@@ -13,6 +13,7 @@ struct PropertyContext final {
   ContextID id{};
   std::string name;
   std::vector<entt::entity> properties;
+  // TODO consider including component entities here? (and rename to Context?)
 };
 
 struct ActivePropertyContext final {
