29bb796a45e278e8e87db7eadaf12d8894010f29
Albin Johansson <albinoscar99@hotmail.com>
2022-01-02 14:24:39 +0100

    Update component_events.hpp

--- modules/tactile/editor/events/component_events.hpp
+++ modules/tactile/editor/events/component_events.hpp
@@ -8,31 +8,59 @@
 
 namespace Tactile {
 
-struct CreateComponentDefEvent final {
+struct CreateComponentDefEvent final
+{
   std::string name;
 };
 
-struct RenameComponentDefEvent final {
-  ComponentID id{};
-  std::string name;
-};
-
-struct RemoveComponentDefEvent final {
-  ComponentID id{};
-};
-
-struct ResetComponentEvent final {
-  ComponentID id{};
-};
-
-struct CreateComponentAttributeEvent final {
+struct RenameComponentDefEvent final
+{
   ComponentID id{};
   std::string name;
 };
 
-struct RenameComponentAttributeEvent final {
+struct RemoveComponentDefEvent final
+{
+  ComponentID id{};
+};
+
+struct CreateComponentAttributeEvent final
+{
+  ComponentID id{};
+  std::string name;
+};
+
+struct RemoveComponentAttributeEvent final
+{
+  ComponentID id{};
+  std::string name;
+};
+
+struct RenameComponentAttributeEvent final
+{
+  ComponentID id{};
   std::string previous;
   std::string updated;
+};
+
+struct SetComponentAttributeTypeEvent final
+{
+  ComponentID id{};
+  std::string attribute;
+  PropertyType type{};
+};
+
+struct UpdateComponentAttributeEvent final
+{
+  ComponentID id{};
+  std::string attribute;
+  PropertyValue value;
+};
+
+struct ResetComponentValuesEvent final
+{
+  ContextID context{};
+  ComponentID component{};
 };
 
 }  // namespace Tactile
