7445c6b6d1a3e00aea0f1f76d45485895be6fbec
Albin Johansson <albinoscar99@hotmail.com>
2022-01-02 12:53:45 +0100

    Update assert.cpp

--- modules/tactile/assert.cpp
+++ modules/tactile/assert.cpp
@@ -3,9 +3,8 @@
 #include <cstdlib>  // abort
 
 #include <boost/stacktrace.hpp>
-#include <fmt/color.h>
-#include <fmt/core.h>
-#include <fmt/ostream.h>
+
+#include "logging.hpp"
 
 namespace boost {
 
@@ -16,24 +15,13 @@ void assertion_failed_msg(const char* expr,
                           const long line)
 
 {
-  if constexpr (Tactile::IsPlatformWindows()) {
-    fmt::print("{}:{} expression '{}' evaluated to false: {}\n{}",
-               file,
-               line,
-               expr,
-               msg ? msg : "N/A",
-               stacktrace::stacktrace{});
-  }
-  else {
-    fmt::print(fmt::fg(fmt::color::orange_red),
-               "{}:{} expression '{}' evaluated to false: {}\n{}",
-               file,
-               line,
-               expr,
-               msg ? msg : "N/A",
-               stacktrace::stacktrace{});
-  }
-
+  Tactile::Print(fmt::color::orange_red,
+                 "{}:{} expression '{}' evaluated to false: {}\n{}",
+                 file,
+                 line,
+                 expr,
+                 msg ? msg : "N/A",
+                 stacktrace::stacktrace{});
   std::abort();
 }
 
