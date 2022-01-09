9f3738c2c47d13679561109ea98ddbb3bff16af1
Albin Johansson <albinoscar99@hotmail.com>
2022-01-09 23:59:27 +0100

    Add FileDialog class

---
+++ modules/tactile/io/file_dialog.cpp
@@ -0,0 +1,77 @@
+#include "file_dialog.hpp"
+
+#include <cstdlib>  // free
+
+#include "assert.hpp"
+
+namespace Tactile {
+namespace {
+
+constexpr const nfdchar_t* gMapFileFilter = "yaml,yml,json,tmx,xml";
+constexpr const nfdchar_t* gImageFileFilter = "png,jpg";
+
+}  // namespace
+
+FileDialog::FileDialog(const nfdresult_t result, nfdchar_t* path)
+    : mResult{result}
+    , mPath{path}
+{}
+
+FileDialog::~FileDialog() noexcept
+{
+  if (mResult == NFD_OKAY) {
+    free(mPath);
+  }
+}
+
+auto FileDialog::IsOkay() const noexcept -> bool
+{
+  return mResult == NFD_OKAY;
+}
+
+auto FileDialog::WasCanceled() const noexcept -> bool
+{
+  return mResult == NFD_CANCEL;
+}
+
+auto FileDialog::GetPath() const -> std::filesystem::path
+{
+  TACTILE_ASSERT(IsOkay());
+  return {mPath};
+}
+
+auto FileDialog::OpenFile(const nfdchar_t* filter, const nfdchar_t* defaultPath)
+    -> FileDialog
+{
+  nfdchar_t* path{};
+  return {NFD_OpenDialog(filter, defaultPath, &path), path};
+}
+
+auto FileDialog::OpenMap() -> FileDialog
+{
+  return OpenFile(gMapFileFilter);
+}
+
+auto FileDialog::OpenImage() -> FileDialog
+{
+  return OpenFile(gImageFileFilter);
+}
+
+auto FileDialog::SaveFile(const nfdchar_t* filter, const nfdchar_t* defaultPath)
+    -> FileDialog
+{
+  nfdchar_t* path{};
+  return {NFD_SaveDialog(filter, defaultPath, &path), path};
+}
+
+auto FileDialog::SaveMap() -> FileDialog
+{
+  return SaveFile(gMapFileFilter);
+}
+
+auto FileDialog::SaveImage() -> FileDialog
+{
+  return SaveFile(gImageFileFilter);
+}
+
+}  // namespace Tactile
---
+++ modules/tactile/io/file_dialog.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <filesystem>  // path
+
+#include <nfd.h>
+#include <tactile_def.hpp>
+
+namespace Tactile {
+
+class FileDialog final {
+ public:
+  TACTILE_DELETE_COPY(FileDialog)
+  TACTILE_DELETE_MOVE(FileDialog)
+
+  ~FileDialog() noexcept;
+
+  [[nodiscard]] static auto OpenFile(const nfdchar_t* filter = nullptr,
+                                     const nfdchar_t* defaultPath = nullptr)
+      -> FileDialog;
+
+  [[nodiscard]] static auto OpenMap() -> FileDialog;
+
+  [[nodiscard]] static auto OpenImage() -> FileDialog;
+
+  [[nodiscard]] static auto SaveFile(const nfdchar_t* filter = nullptr,
+                                     const nfdchar_t* defaultPath = nullptr)
+      -> FileDialog;
+
+  [[nodiscard]] static auto SaveMap() -> FileDialog;
+
+  [[nodiscard]] static auto SaveImage() -> FileDialog;
+
+  [[nodiscard]] auto IsOkay() const noexcept -> bool;
+
+  [[nodiscard]] auto WasCanceled() const noexcept -> bool;
+
+  [[nodiscard]] auto GetPath() const -> std::filesystem::path;
+
+ private:
+  nfdresult_t mResult{};
+  nfdchar_t* mPath{};
+
+  FileDialog(nfdresult_t result, nfdchar_t* path);
+};
+
+}  // namespace Tactile
