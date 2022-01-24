74afcc7eff9b6e16b113d0b5708771e39c23a5ff
Albin Johansson <albinoscar99@hotmail.com>
2022-01-24 19:42:27 +0100

    Update object_system.hpp

--- modules/tactile/core/systems/object_system.hpp
+++ modules/tactile/core/systems/object_system.hpp
@@ -10,13 +10,11 @@ namespace tactile::sys {
  * \defgroup object-system Object System
  */
 
-/// \name Object system
+/// \addtogroup object-system
 /// \{
 
 /**
  * \brief Returns the object entity associated with a specific ID.
- *
- * \ingroup object-system
  *
  * \param registry the document registry.
  * \param id the ID associated with the desired object.
@@ -26,6 +24,6 @@ namespace tactile::sys {
 [[nodiscard]] auto find_object(const entt::registry& registry, ObjectID id)
     -> entt::entity;
 
-/// \} End of object system
+/// \} End of group object-system
 
 }  // namespace tactile::sys
