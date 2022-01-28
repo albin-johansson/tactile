f64f6e2912ecd0c9e242f14ee87914bb332e71f0
Albin Johansson <albinoscar99@hotmail.com>
2022-01-28 23:24:07 +0100

    Remove tactile-io files

--- modules/tactile-io/json/json_common.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <nlohmann/json.hpp>  // json
-
-namespace tactile::IO {
-
-using JSON = nlohmann::json;
-
-}  // namespace tactile::IO
--- modules/tactile-io/yaml/yaml_fwd.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-namespace YAML {
-
-class Emitter;
-class Node;
-
-}  // namespace YAML
