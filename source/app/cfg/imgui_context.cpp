dc25aa1091f2dd8b11e41c6ec9660b82c0ec5163
Albin Johansson <albinoscar99@hotmail.com>
2022-02-07 15:25:00 +0100

    Attempt at improving DPI handling

--- source/app/cfg/imgui_context.cpp
+++ source/app/cfg/imgui_context.cpp
@@ -1,7 +1,5 @@
 #include "imgui_context.hpp"
 
-#include <array>  // array
-
 #include <IconsFontAwesome5.h>
 #include <imgui.h>
 
@@ -15,40 +13,33 @@
 namespace tactile {
 namespace {
 
-void load_icon_font(const float size)
-{
-  static constexpr std::array<ImWchar, 3> range = {ICON_MIN_FA, ICON_MAX_FA, 0};
-  ImFontConfig config{};
-  config.MergeMode = true;
-  config.PixelSnapH = true;
-  config.GlyphOffset = {0, 1};
-
-  auto& io = ImGui::GetIO();
-  io.Fonts->AddFontFromFileTTF("resources/fonts/fa/fa-solid-900.otf",
-                               size,
-                               &config,
-                               range.data());
-}
-
 void load_fonts()
 {
   auto& io = ImGui::GetIO();
+  io.Fonts->Clear();
 
-  if constexpr (on_osx) {
-    constexpr auto fontSize = 11.0f;
+  constexpr float font_size = 13.0f;
+  constexpr float scale = on_osx ? 2.0f : 1.0f;  // io.DisplayFramebufferScale
 
-    const auto dpi = cen::display_dpi().value();
-    const auto scaling = dpi.diagonal / 96.0f;
+  io.FontGlobalScale = 1.0f / scale;
 
-    io.FontGlobalScale = 0.5f;
-    io.Fonts->AddFontFromFileTTF("resources/fonts/roboto/Roboto-Regular.ttf",
-                                 scaling * fontSize);
-    load_icon_font(scaling * fontSize);
-  }
-  else {
-    io.Fonts->AddFontDefault();
-    load_icon_font(13.0f);
-  }
+  io.Fonts->AddFontFromFileTTF("resources/fonts/roboto/Roboto-Regular.ttf",
+                               font_size * scale);
+
+  ImFontConfig config{};
+  config.MergeMode = true;
+  config.PixelSnapH = true;
+  config.GlyphOffset = {0, 2};
+  config.GlyphMinAdvanceX = font_size * scale;
+  config.GlyphMaxAdvanceX = config.GlyphMinAdvanceX;
+
+  static constexpr ImWchar icon_range[] = {ICON_MIN_FA, ICON_MAX_FA, 0};
+  io.Fonts->AddFontFromFileTTF("resources/fonts/fa/fa-solid-900.otf",
+                               font_size * scale,
+                               &config,
+                               icon_range);
+
+  io.Fonts->Build();
 }
 
 }  // namespace
@@ -63,6 +54,14 @@ im_gui_context::im_gui_context(cen::window& window, cen::gl_context& context)
   io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // NOLINT
   io.WantCaptureKeyboard = true;
 
+  ImGui_ImplSDL2_InitForOpenGL(window.get(), context.get());
+  if constexpr (on_osx) {
+    mInitializedBackend = ImGui_ImplOpenGL3_Init("#version 150");
+  }
+  else {
+    mInitializedBackend = ImGui_ImplOpenGL3_Init("#version 130");
+  }
+
   load_fonts();
 
   ImGui::StyleColorsDark();
@@ -74,14 +73,6 @@ im_gui_context::im_gui_context(cen::window& window, cen::gl_context& context)
   apply_theme(style, prefs.get_theme());
 
   style.WindowBorderSize = prefs.has_window_border() ? 1.0f : 0.0f;
-
-  ImGui_ImplSDL2_InitForOpenGL(window.get(), context.get());
-  if constexpr (on_osx) {
-    mInitializedBackend = ImGui_ImplOpenGL3_Init("#version 150");
-  }
-  else {
-    mInitializedBackend = ImGui_ImplOpenGL3_Init("#version 130");
-  }
 
   log_debug("Initialized renderer backend... {}", mInitializedBackend ? "yes" : "no");
 }
