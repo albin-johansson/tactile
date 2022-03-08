6c4e5944ab14a32c298af3a969915592a2aba7f4
Albin Johansson <albinoscar99@hotmail.com>
2022-03-08 18:21:39 +0100

    Add copyright notices to all source files

--- source/app/editor/gui/components/dialogs/component_name_dialog.cpp
+++ source/app/editor/gui/components/dialogs/component_name_dialog.cpp
@@ -1,3 +1,22 @@
+/*
+ * This source file is a part of the Tactile map editor.
+ *
+ * Copyright (C) 2022 Albin Johansson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "component_name_dialog.hpp"
 
 #include "core/systems/component_system.hpp"
