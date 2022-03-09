6971754f31547713be992dc06dc37c21ab8b4c44
Albin Johansson <albinoscar99@hotmail.com>
2022-03-09 18:21:20 +0100

    Update invoke_n.hpp

--- source/app/core/algorithms/invoke_n.hpp
+++ source/app/core/algorithms/invoke_n.hpp
@@ -24,13 +24,13 @@ namespace tactile {
 /**
  * \brief Invokes the supplied callable for the specified amount of times.
  *
- * \ingroup core
- *
  * \tparam Integer the integral type that represents the number of invocations.
  * \tparam T the type of the callable.
  *
  * \param n the amount of times the callable should be invoked.
  * \param callable the callable that will be invoked.
+ *
+ * \ingroup core
  */
 template <typename Integer, typename T>
 constexpr void invoke_n(const Integer n, T&& callable) noexcept(noexcept(callable()))
