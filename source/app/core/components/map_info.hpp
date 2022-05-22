eea1633c0fc40e8c4b9a7e64a8827e266af57b28
Albin Johansson <albinoscar99@hotmail.com>
2022-05-22 21:05:17 +0200

    Update map_info.hpp

--- source/app/core/components/map_info.hpp
+++ source/app/core/components/map_info.hpp
@@ -32,6 +32,7 @@ struct MapInfo final  // TODO comp
   usize row_count{};     ///< Total amount of rows.
   usize column_count{};  ///< Total amount of columns.
 
+  // TODO ivec2
   int32 tile_width{};   ///< Logical width of all tiles.
   int32 tile_height{};  ///< Logical height of all tiles.
 
