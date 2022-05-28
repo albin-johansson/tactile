da6dcc529a35b317c4468d34947201c058a1c626
Albin Johansson <albinoscar99@hotmail.com>
2022-05-29 01:45:44 +0200

    Update viewport_system_test.cpp

--- test/unit-tests/app/core/systems/viewport_system_test.cpp
+++ test/unit-tests/app/core/systems/viewport_system_test.cpp
@@ -18,71 +18,71 @@ class ViewportSystemTest : public testing::Test
 TEST_F(ViewportSystemTest, OffsetViewport)
 {
   const auto& viewport = mRegistry.ctx().at<comp::Viewport>();
-  ASSERT_EQ(0, viewport.x_offset);
-  ASSERT_EQ(0, viewport.y_offset);
+  ASSERT_EQ(0, viewport.offset.x);
+  ASSERT_EQ(0, viewport.offset.y);
 
   sys::offset_viewport(mRegistry, 42.5f, 27.3f);
-  ASSERT_EQ(42.5f, viewport.x_offset);
-  ASSERT_EQ(27.3f, viewport.y_offset);
+  ASSERT_EQ(42.5f, viewport.offset.x);
+  ASSERT_EQ(27.3f, viewport.offset.y);
 
   sys::offset_viewport(mRegistry, -12.0f, -1.9f);
-  ASSERT_EQ(42.5f - 12.0f, viewport.x_offset);
-  ASSERT_EQ(27.3f - 1.9f, viewport.y_offset);
+  ASSERT_EQ(42.5f - 12.0f, viewport.offset.x);
+  ASSERT_EQ(27.3f - 1.9f, viewport.offset.y);
 }
 
 TEST_F(ViewportSystemTest, PanViewportLeft)
 {
   const auto& viewport = mRegistry.ctx().at<comp::Viewport>();
-  ASSERT_GT(viewport.tile_width, 0);
-  ASSERT_GT(viewport.tile_height, 0);
+  ASSERT_GT(viewport.tile_size.x, 0);
+  ASSERT_GT(viewport.tile_size.y, 0);
 
   sys::pan_viewport_left(mRegistry);
-  ASSERT_EQ(viewport.tile_width, viewport.x_offset);
-  ASSERT_EQ(0, viewport.y_offset);
+  ASSERT_EQ(viewport.tile_size.x, viewport.offset.x);
+  ASSERT_EQ(0, viewport.offset.y);
 }
 
 TEST_F(ViewportSystemTest, PanViewportRight)
 {
   const auto& viewport = mRegistry.ctx().at<comp::Viewport>();
-  ASSERT_GT(viewport.tile_width, 0);
-  ASSERT_GT(viewport.tile_height, 0);
+  ASSERT_GT(viewport.tile_size.x, 0);
+  ASSERT_GT(viewport.tile_size.y, 0);
 
   sys::pan_viewport_right(mRegistry);
-  ASSERT_EQ(-viewport.tile_width, viewport.x_offset);
-  ASSERT_EQ(0, viewport.y_offset);
+  ASSERT_EQ(-viewport.tile_size.x, viewport.offset.x);
+  ASSERT_EQ(0, viewport.offset.y);
 }
 
 TEST_F(ViewportSystemTest, PanViewportUp)
 {
   const auto& viewport = mRegistry.ctx().at<comp::Viewport>();
-  ASSERT_GT(viewport.tile_width, 0);
-  ASSERT_GT(viewport.tile_height, 0);
+  ASSERT_GT(viewport.tile_size.x, 0);
+  ASSERT_GT(viewport.tile_size.y, 0);
 
   sys::pan_viewport_up(mRegistry);
-  ASSERT_EQ(0, viewport.x_offset);
-  ASSERT_EQ(viewport.tile_height, viewport.y_offset);
+  ASSERT_EQ(0, viewport.offset.x);
+  ASSERT_EQ(viewport.tile_size.y, viewport.offset.y);
 }
 
 TEST_F(ViewportSystemTest, PanViewportDown)
 {
   const auto& viewport = mRegistry.ctx().at<comp::Viewport>();
-  ASSERT_GT(viewport.tile_width, 0);
-  ASSERT_GT(viewport.tile_height, 0);
+  ASSERT_GT(viewport.tile_size.x, 0);
+  ASSERT_GT(viewport.tile_size.y, 0);
 
   sys::pan_viewport_down(mRegistry);
-  ASSERT_EQ(0, viewport.x_offset);
-  ASSERT_EQ(-viewport.tile_height, viewport.y_offset);
+  ASSERT_EQ(0, viewport.offset.x);
+  ASSERT_EQ(-viewport.tile_size.y, viewport.offset.y);
 }
 
 TEST_F(ViewportSystemTest, DecreaseViewportZoom)
 {
   const auto& viewport = mRegistry.ctx().at<comp::Viewport>();
 
-  const auto width = viewport.tile_width;
-  const auto height = viewport.tile_height;
+  const auto width = viewport.tile_size.x;
+  const auto height = viewport.tile_size.y;
 
   sys::decrease_viewport_zoom(mRegistry, 0, 0);
 
-  ASSERT_LT(viewport.tile_width, width);
-  ASSERT_LT(viewport.tile_height, height);
+  ASSERT_LT(viewport.tile_size.x, width);
+  ASSERT_LT(viewport.tile_size.y, height);
 }
