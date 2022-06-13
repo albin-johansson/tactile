0b6ea4874bce437732d8b6584a2c2dc69c75b134
Albin Johansson <albinoscar99@hotmail.com>
2022-06-13 07:36:29 +0200

    Add element interface

---
+++ source/app/core/element.hpp
@@ -0,0 +1,36 @@
+/*
+ * This source file is a part of the Tactile map editor.
+ *
+ * Copyright (C) 2022 Albin Johansson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "core/common/uuid.hpp"
+
+namespace tactile::core {
+
+/// Interface for all identifiable objects in the core model.
+class IElement
+{
+ public:
+  virtual ~IElement() noexcept = default;
+
+  /// Returns the unique identifier associated with the element.
+  virtual auto get_uuid() const -> const UUID& = 0;
+};
+
+}  // namespace tactile::core
