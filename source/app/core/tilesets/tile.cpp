d3214b7453b7e6f0b7623a4f5e3a3080c1944569
Albin Johansson <albinoscar99@hotmail.com>
2022-06-19 02:53:55 +0200

    Add tile class

---
+++ source/app/core/tilesets/tile.cpp
@@ -0,0 +1,101 @@
+/*
+ * This source file is a part of the Tactile map editor.
+ *
+ * Copyright (C) 2022 Albin Johansson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "tile.hpp"
+
+#include <utility>  // move
+
+#include "misc/panic.hpp"
+
+namespace tactile::core {
+
+Tile::Tile(const TileIndex index) : mIndex{index} {}
+
+void Tile::reserve_objects(usize n)
+{
+  mObjects.reserve(n);
+}
+
+void Tile::add_object(Object object)
+{
+  const auto id = object.get_uuid();
+  mObjects[id] = std::move(object);
+}
+
+void Tile::clear_animation()
+{
+  mAnimation.reset();
+}
+
+void Tile::set_animation(TileAnimation animation)
+{
+  mAnimation = std::move(animation);
+}
+
+auto Tile::object_count() const -> usize
+{
+  return mObjects.size();
+}
+
+auto Tile::object_capacity() const -> usize
+{
+  return mObjects.bucket_count();
+}
+
+auto Tile::is_animated() const -> bool
+{
+  return mAnimation.has_value();
+}
+
+auto Tile::get_animation() const -> const TileAnimation&
+{
+  if (mAnimation) {
+    return *mAnimation;
+  }
+  else {
+    throw TactileError{"Tile is not animated!"};
+  }
+}
+
+auto Tile::get_props() -> PropertyBundle&
+{
+  return mDelegate.get_props();
+}
+
+auto Tile::get_props() const -> const PropertyBundle&
+{
+  return mDelegate.get_props();
+}
+
+auto Tile::get_comps() -> ComponentBundle&
+{
+  return mDelegate.get_comps();
+}
+
+auto Tile::get_comps() const -> const ComponentBundle&
+{
+  return mDelegate.get_comps();
+}
+
+auto Tile::get_uuid() const -> const UUID&
+{
+  return mDelegate.get_uuid();
+}
+
+}  // namespace tactile::core
---
+++ source/app/core/tilesets/tile.hpp
@@ -0,0 +1,75 @@
+/*
+ * This source file is a part of the Tactile map editor.
+ *
+ * Copyright (C) 2022 Albin Johansson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <boost/uuid/uuid_hash.hpp>
+
+#include "core/common/associative.hpp"
+#include "core/common/identifiers.hpp"
+#include "core/common/ints.hpp"
+#include "core/common/maybe.hpp"
+#include "core/common/uuid.hpp"
+#include "core/context.hpp"
+#include "core/context_delegate.hpp"
+#include "core/layers/object.hpp"
+#include "core/tilesets/tile_animation.hpp"
+
+namespace tactile::core {
+
+/// Provides metadata for tiles in a tileset.
+class Tile final : public IContext
+{
+ public:
+  explicit Tile(TileIndex index);
+
+  void reserve_objects(usize n);
+
+  void add_object(Object object);
+
+  void clear_animation();
+
+  void set_animation(TileAnimation animation);
+
+  [[nodiscard]] auto object_count() const -> usize;
+
+  [[nodiscard]] auto object_capacity() const -> usize;
+
+  [[nodiscard]] auto is_animated() const -> bool;
+
+  [[nodiscard]] auto get_animation() const -> const TileAnimation&;
+
+  [[nodiscard]] auto get_props() -> PropertyBundle& override;
+  [[nodiscard]] auto get_props() const -> const PropertyBundle& override;
+
+  [[nodiscard]] auto get_comps() -> ComponentBundle& override;
+  [[nodiscard]] auto get_comps() const -> const ComponentBundle& override;
+
+  [[nodiscard]] auto get_uuid() const -> const UUID& override;
+
+  [[nodiscard]] auto index() const noexcept -> TileIndex { return mIndex; }
+
+ private:
+  TileIndex mIndex;
+  ContextDelegate mDelegate;
+  Maybe<TileAnimation> mAnimation;
+  HashMap<UUID, Object> mObjects;
+};
+
+}  // namespace tactile::core
