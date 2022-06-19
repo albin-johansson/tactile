adbf3d102693db1e7a30478142ca869432b6da7a
Albin Johansson <albinoscar99@hotmail.com>
2022-06-19 02:35:40 +0200

    Create tileset_info.hpp

---
+++ source/app/core/tilesets/tileset_info.hpp
@@ -0,0 +1,37 @@
+/*
+ * This source file is a part of the Tactile map editor.
+ *
+ * Copyright (C) 2022 Albin Johansson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "core/common/ints.hpp"
+#include "core/common/math.hpp"
+
+namespace tactile::core {
+
+/// Provides information necessary to construct tilesets.
+struct TilesetInfo final
+{
+  uint texture_id{};        /// The associated OpenGL texture identifier.
+  Vector2i texture_size{};  /// The size of the texture.
+  Vector2i tile_size{};     /// Logical tile size.
+  int32 row_count{};        /// The amount of tile rows.
+  int32 column_count{};     /// The amount of tile columns.
+};
+
+}  // namespace tactile::core
