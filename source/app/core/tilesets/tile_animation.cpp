d49808b349b15033a85891804cf2b5d83177da63
Albin Johansson <albinoscar99@hotmail.com>
2022-06-19 02:35:28 +0200

    Add tile animation class

---
+++ source/app/core/tilesets/tile_animation.cpp
@@ -0,0 +1,76 @@
+/*
+ * This source file is a part of the Tactile map editor.
+ *
+ * Copyright (C) 2022 Albin Johansson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "tile_animation.hpp"
+
+#include "misc/assert.hpp"
+#include "misc/panic.hpp"
+
+namespace tactile::core {
+
+void TileAnimation::update()
+{
+  const auto now = Clock::now();
+
+  TACTILE_ASSERT(mIndex < mFrames.size());
+  const auto& currentFrame = mFrames.at(mIndex);
+
+  if (now - mLastUpdate >= currentFrame.duration) {
+    mLastUpdate = now;
+    mIndex = (mIndex + 1u) % mFrames.size();
+  }
+}
+
+void TileAnimation::reserve_frames(const usize n)
+{
+  mFrames.reserve(n);
+}
+
+void TileAnimation::add_frame(const TileIndex tile, const Millis duration)
+{
+  mFrames.push_back({tile, duration});
+}
+
+auto TileAnimation::nth(const usize index) const -> const Frame&
+{
+  if (index < mFrames.size()) {
+    return mFrames[index];
+  }
+  else {
+    throw TactileError{"Invalid frame index!"};
+  }
+}
+
+auto TileAnimation::current_tile() const -> TileIndex
+{
+  TACTILE_ASSERT(mIndex < mFrames.size());
+  return mFrames.at(mIndex).tile;
+}
+
+auto TileAnimation::capacity() const -> usize
+{
+  return mFrames.capacity();
+}
+
+auto TileAnimation::size() const -> usize
+{
+  return mFrames.size();
+}
+
+}  // namespace tactile::core
---
+++ source/app/core/tilesets/tile_animation.hpp
@@ -0,0 +1,63 @@
+/*
+ * This source file is a part of the Tactile map editor.
+ *
+ * Copyright (C) 2022 Albin Johansson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <chrono>  // system_clock, milliseconds
+#include <vector>  // vector
+
+#include "core/common/identifiers.hpp"
+#include "core/common/ints.hpp"
+
+namespace tactile::core {
+
+class TileAnimation final
+{
+ public:
+  using Millis = std::chrono::milliseconds;
+  using Clock = std::chrono::system_clock;
+  using TimePoint = Clock::time_point;
+
+  struct Frame final
+  {
+    TileIndex tile{};
+    Millis duration{};
+  };
+
+  void update();
+
+  void reserve_frames(usize n);
+
+  void add_frame(TileIndex tile, Millis duration);
+
+  [[nodiscard]] auto nth(usize index) const -> const Frame&;
+
+  [[nodiscard]] auto current_tile() const -> TileIndex;
+
+  [[nodiscard]] auto capacity() const -> usize;
+
+  [[nodiscard]] auto size() const -> usize;
+
+ private:
+  std::vector<Frame> mFrames;
+  usize mIndex{};
+  TimePoint mLastUpdate{};
+};
+
+}  // namespace tactile::core
