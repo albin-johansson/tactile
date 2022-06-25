d4394ea0f9c6fae4a9be38de1eda5eb3690cd508
Albin Johansson <albinoscar99@hotmail.com>
2022-06-26 00:56:01 +0200

    Add tileset class

---
+++ source/app/core/tilesets/tileset.cpp
@@ -0,0 +1,116 @@
+/*
+ * This source file is a part of the Tactile map editor.
+ *
+ * Copyright (C) 2022 Albin Johansson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "tileset.hpp"
+
+#include <utility>  // move
+
+#include "core/tilesets/tileset_info.hpp"
+#include "misc/panic.hpp"
+
+namespace tactile::core {
+
+Tileset::Tileset(const UUID& id, const TilesetInfo& info)
+    : mContext{id}
+    , mTextureId{info.texture_id}
+    , mTextureSize{info.texture_size}
+    , mTileSize{info.tile_size}
+    , mRowCount{mTextureSize.y / mTileSize.y}
+    , mColumnCount{mTextureSize.x / mTileSize.x}
+    , mUvSize{Vector2f{mTileSize} / Vector2f{mTileSize}}
+    , mTexturePath{std::move(info.texture_path)}
+{}
+
+Tileset::Tileset(const TilesetInfo& info) : Tileset{make_uuid(), info} {}
+
+void Tileset::set_name(std::string name)
+{
+  mContext.set_name(std::move(name));
+}
+
+auto Tileset::operator[](const TileIndex index) -> Tile&
+{
+  if (!is_valid(index)) {
+    throw TactileError{"Invalid tile index!"};
+  }
+
+  if (const auto iter = mIdentifiers.find(index); iter != mIdentifiers.end()) {
+    const auto& id = iter->second;
+    return lookup_in(mMetaTiles, id);
+  }
+  else {
+    Tile       tile{index};
+    const auto id = tile.get_uuid();
+
+    mMetaTiles.try_emplace(id, std::move(tile));
+    mIdentifiers[index] = id;
+
+    return lookup_in(mMetaTiles, id);
+  }
+}
+
+auto Tileset::index_of(const TilePos& pos) const -> TileIndex
+{
+  const auto row = pos.row();
+  const auto col = pos.col();
+
+  if (row >= 0 && col >= 0 && row < mRowCount && col < mColumnCount) {
+    return row * mColumnCount + col;
+  }
+  else {
+    throw TactileError{"Invalid tile position!"};
+  }
+}
+
+auto Tileset::get_props() -> PropertyBundle&
+{
+  return mContext.get_props();
+}
+
+auto Tileset::get_props() const -> const PropertyBundle&
+{
+  return mContext.get_props();
+}
+
+auto Tileset::get_comps() -> ComponentBundle&
+{
+  return mContext.get_comps();
+}
+
+auto Tileset::get_comps() const -> const ComponentBundle&
+{
+  return mContext.get_comps();
+}
+
+auto Tileset::get_uuid() const -> const UUID&
+{
+  return mContext.get_uuid();
+}
+
+auto Tileset::get_name() const -> const std::string&
+{
+  return mContext.get_name();
+}
+
+auto Tileset::is_valid(const TileIndex index) const -> bool
+{
+  return index >= 0 && index < tile_count();
+}
+
+}  // namespace tactile::core
---
+++ source/app/core/tilesets/tileset.hpp
@@ -0,0 +1,98 @@
+/*
+ * This source file is a part of the Tactile map editor.
+ *
+ * Copyright (C) 2022 Albin Johansson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <filesystem>  // path
+#include <vector>      // vector
+
+#include <boost/uuid/uuid_hash.hpp>
+
+#include "core/common/associative.hpp"
+#include "core/common/identifiers.hpp"
+#include "core/common/ints.hpp"
+#include "core/common/math.hpp"
+#include "core/common/uuid.hpp"
+#include "core/context.hpp"
+#include "core/context_delegate.hpp"
+#include "core/fwd.hpp"
+#include "core/tile_pos.hpp"
+#include "core/tilesets/tile.hpp"
+
+namespace tactile::core {
+
+class Tileset final : public IContext
+{
+ public:
+  Tileset(const UUID& id, const TilesetInfo& info);
+
+  explicit Tileset(const TilesetInfo& info);
+
+  void set_name(std::string name) override;
+
+  [[nodiscard]] auto operator[](TileIndex index) -> Tile&;
+
+  [[nodiscard]] auto index_of(const TilePos& pos) const -> TileIndex;
+
+  [[nodiscard]] auto get_props() -> PropertyBundle& override;
+  [[nodiscard]] auto get_props() const -> const PropertyBundle& override;
+
+  [[nodiscard]] auto get_comps() -> ComponentBundle& override;
+  [[nodiscard]] auto get_comps() const -> const ComponentBundle& override;
+
+  [[nodiscard]] auto get_uuid() const -> const UUID& override;
+
+  [[nodiscard]] auto get_name() const -> const std::string& override;
+
+  [[nodiscard]] auto texture_id() const -> uint { return mTextureId; }
+
+  [[nodiscard]] auto texture_path() const -> const std::filesystem::path&
+  {
+    return mTexturePath;
+  }
+
+  [[nodiscard]] auto texture_size() const -> const Vector2i& { return mTextureSize; }
+
+  [[nodiscard]] auto tile_size() const -> const Vector2i& { return mTileSize; }
+
+  [[nodiscard]] auto uv_size() const -> const Vector2f& { return mUvSize; }
+
+  [[nodiscard]] auto row_count() const -> int32 { return mRowCount; }
+  [[nodiscard]] auto column_count() const -> int32 { return mColumnCount; }
+  [[nodiscard]] auto tile_count() const -> int32 { return row_count() * column_count(); }
+
+  [[nodiscard]] auto begin() const noexcept { return mMetaTiles.begin(); }
+  [[nodiscard]] auto end() const noexcept { return mMetaTiles.end(); }
+
+ private:
+  ContextDelegate          mContext;
+  uint                     mTextureId{};
+  Vector2i                 mTextureSize{};
+  Vector2i                 mTileSize{};
+  int32                    mRowCount{};
+  int32                    mColumnCount{};
+  Vector2f                 mUvSize{};
+  HashMap<TileIndex, UUID> mIdentifiers;
+  HashMap<UUID, Tile>      mMetaTiles;
+  std::filesystem::path    mTexturePath;
+
+  [[nodiscard]] auto is_valid(TileIndex index) const -> bool;
+};
+
+}  // namespace tactile::core
