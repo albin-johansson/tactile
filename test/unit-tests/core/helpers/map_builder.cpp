c7b26d85c794c5c414db31bda07128a37a0e4b94
Albin Johansson <albinoscar99@hotmail.com>
2022-07-10 11:38:14 +0200

    Add tests for add/remove layer commands

--- test/unit-tests/core/helpers/map_builder.cpp
+++ test/unit-tests/core/helpers/map_builder.cpp
@@ -30,12 +30,12 @@
 namespace tactile::test {
 
 MapBuilder::MapBuilder()
-    : mDocument{std::make_unique<MapDocument>(Vector2f{32, 32}, 5, 5)}
+    : mDocument {std::make_unique<MapDocument>(Vector2f {32, 32}, 5, 5)}
 {}
 
 auto MapBuilder::build() -> MapBuilder
 {
-  return MapBuilder{};
+  return MapBuilder {};
 }
 
 auto MapBuilder::with_tile_size(const Vector2i& size) -> MapBuilder&
@@ -52,8 +52,10 @@ auto MapBuilder::with_size(const usize rows, const usize columns) -> MapBuilder&
 
 auto MapBuilder::with_tile_layer(UUID* id, Maybe<TileID> initialValue) -> MapBuilder&
 {
-  auto&      map = mDocument->get_map();
+  auto& map = mDocument->get_map();
+
   const auto layerId = map.add_tile_layer();
+  mDocument->register_context(map.get_layer(layerId));
 
   if (id) {
     *id = layerId;
@@ -71,7 +73,10 @@ auto MapBuilder::with_tile_layer(UUID* id, Maybe<TileID> initialValue) -> MapBui
 
 auto MapBuilder::with_object_layer(UUID* id) -> MapBuilder&
 {
-  const auto layerId = mDocument->get_map().add_object_layer();
+  auto& map = mDocument->get_map();
+
+  const auto layerId = map.add_object_layer();
+  mDocument->register_context(map.get_layer(layerId));
 
   if (id) {
     *id = layerId;
@@ -87,6 +92,7 @@ auto MapBuilder::with_object(const ObjectType type, Shared<Object>* outObject)
 
   if (!mDedicatedObjectLayer) {
     mDedicatedObjectLayer = map.add_object_layer();
+    mDocument->register_context(map.get_layer(*mDedicatedObjectLayer));
   }
 
   auto object = std::make_shared<Object>();
@@ -96,6 +102,8 @@ auto MapBuilder::with_object(const ObjectType type, Shared<Object>* outObject)
     *outObject = object;
   }
 
+  mDocument->register_context(object);
+
   auto& layer = map.view_object_layer(*mDedicatedObjectLayer);
   layer.add_object(std::move(object));
 
@@ -104,7 +112,7 @@ auto MapBuilder::with_object(const ObjectType type, Shared<Object>* outObject)
 
 auto MapBuilder::with_tileset(UUID* id) -> MapBuilder&
 {
-  auto tileset = std::make_shared<Tileset>(TilesetInfo{
+  auto tileset = std::make_shared<Tileset>(TilesetInfo {
       .texture_path = "foo.png",
       .texture_id = 9,
       .texture_size = {1024, 1024},
@@ -115,7 +123,10 @@ auto MapBuilder::with_tileset(UUID* id) -> MapBuilder&
     *id = tileset->get_uuid();
   }
 
-  mDocument->get_map().attach_tileset(std::move(tileset), false);
+  mDocument->register_context(tileset);
+
+  auto& map = mDocument->get_map();
+  map.attach_tileset(std::move(tileset), false);
 
   return *this;
 }
