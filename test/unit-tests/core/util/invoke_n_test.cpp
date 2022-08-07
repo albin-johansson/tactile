02f1aba46667c8f0af59da3458e75b7227942f0a
Albin Johansson <albinoscar99@hotmail.com>
2022-08-07 20:34:25 +0200

    Update invoke_n_test.cpp

--- test/unit-tests/core/util/invoke_n_test.cpp
+++ test/unit-tests/core/util/invoke_n_test.cpp
@@ -17,9 +17,9 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-#include "core/util/functional.hpp"
-
 #include <gtest/gtest.h>
+
+#include "core/util/functional.hpp"
 
 namespace tactile::test {
 
