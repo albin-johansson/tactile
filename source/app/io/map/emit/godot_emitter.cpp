75a938a7a43dc579f0b09a339c7ee9db72dbb8c4
Albin Johansson <albinoscar99@hotmail.com>
2022-08-10 18:36:14 +0200

    Update godot_emitter.cpp

--- source/app/io/map/emit/godot_emitter.cpp
+++ source/app/io/map/emit/godot_emitter.cpp
@@ -17,6 +17,7 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include <algorithm>    // sort
 #include <cmath>        // sin, cos
 #include <fstream>      // ofstream
 #include <ios>          // ios
