4d4c607c18311dffffdc35dc5e52a4a13eb98432
Albin Johansson <albinoscar99@hotmail.com>
2022-08-16 23:20:44 +0200

    Update compression_test.cpp

--- test/unit-tests/io/maps/compression_test.cpp
+++ test/unit-tests/io/maps/compression_test.cpp
@@ -17,28 +17,106 @@
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include <utility>  // pair, move
+
 #include <gtest/gtest.h>
 
+#include "core/common/fs.hpp"
 #include "core/common/tiles.hpp"
+#include "core/util/tiles.hpp"
+#include "io/map/emit/emit_info.hpp"
+#include "io/map/emit/emitter.hpp"
+#include "io/map/ir/ir.hpp"
 #include "io/map/parse/parse_map.hpp"
 
 namespace tactile::test {
+namespace {
 
-TEST(CompressedTileData, TactileYAMLWithZlib)
+inline const TileMatrix test_tiles {
+    {123, 8324, 4123, 68, 0},
+    {7483, 32, 12944, 123, 8},
+    {8, 284, 221, 435, 2123},
+    {939, 842, 993, 759, 435},
+    {57, 348, 118, 12, 136},
+};
+
+[[nodiscard]] auto create_test_map() -> ir::MapData
 {
-  const auto result = io::parse_map("test-resources/yaml/compressed_zlib.yaml");
+  ir::MapData map;
+  map.row_count = 5;
+  map.col_count = 5;
+  map.tile_size = {32, 32};
+
+  map.next_layer_id = 1;
+  map.next_object_id = 1;
+
+  auto& layer = map.layers.emplace_back();
+  layer.name = "Foo";
+  layer.id = map.next_layer_id++;
+  layer.type = LayerType::TileLayer;
+  layer.opacity = 1.0f;
+  layer.visible = true;
+
+  auto& tile_layer = layer.data.emplace<ir::TileLayerData>();
+  tile_layer.row_count = map.row_count;
+  tile_layer.col_count = map.col_count;
+  tile_layer.tiles = test_tiles;
+
+  return map;
+}
+
+using TileFormatFactory = ir::TileFormatData (*)();
+using TestData = std::pair<std::string_view, TileFormatFactory>;
+
+[[nodiscard]] auto create_zlib_tile_format() -> ir::TileFormatData
+{
+  return {
+      .encoding = TileEncoding::Base64,
+      .compression = TileCompression::Zlib,
+      .zlib_compression_level = 6,
+  };
+}
+
+[[nodiscard]] auto create_zstd_tile_format() -> ir::TileFormatData
+{
+  return {
+      .encoding = TileEncoding::Base64,
+      .compression = TileCompression::Zstd,
+      .zstd_compression_level = 22,
+  };
+}
+
+const auto cases =
+    testing::Values(TestData {"zlib_compression_test.yaml", create_zlib_tile_format},
+                    TestData {"zstd_compression_test.yaml", create_zstd_tile_format});
+
+}  // namespace
+
+struct CompressedMapTest : testing::TestWithParam<TestData>
+{};
+
+TEST_P(CompressedMapTest, EmitAndParseMap)
+{
+  const auto [path, format_factory] = GetParam();
+
+  {
+    auto test_map = create_test_map();
+    test_map.tile_format = format_factory();
+
+    const io::EmitInfo info {path, std::move(test_map)};
+    io::emit_yaml_map(info);
+  }
+
+  const auto result = io::parse_map(path);
   ASSERT_EQ(io::ParseError::None, result.error());
 
   const auto& map = result.data();
-  ASSERT_EQ(1, map.layers.size());
-
   const auto& layer = map.layers.front();
-  ASSERT_EQ(LayerType::TileLayer, layer.type);
-
   const auto& tile_layer = std::get<ir::TileLayerData>(layer.data);
 
-  const TileMatrix expected {{1000, 2000, 3000}, {4000, 5000, 6000}, {7000, 8000, 9000}};
-  ASSERT_EQ(expected, tile_layer.tiles);
+  ASSERT_EQ(test_tiles, tile_layer.tiles);
 }
+
+INSTANTIATE_TEST_SUITE_P(CompressedMapTests, CompressedMapTest, cases);
 
 }  // namespace tactile::test
