d090dc8eaa96b2c3002bf4516e074d07d0cf7a7f
Albin Johansson <albinoscar99@hotmail.com>
2022-08-17 17:54:23 +0200

    Simplify XML emitter

--- source/app/io/map/emit/xml_emitter.cpp
+++ source/app/io/map/emit/xml_emitter.cpp
@@ -38,17 +38,17 @@
 namespace tactile::io {
 namespace {
 
-void append_properties(XMLNode node, const ir::ContextData& context_data)
+void append_properties(XMLNode node, const ir::ContextData& context)
 {
-  if (context_data.properties.empty()) {
+  if (context.properties.empty()) {
     return;
   }
 
   auto collection = node.append_child("properties");
 
-  for (const auto& [propertyName, propertyValue] : context_data.properties) {
+  for (const auto& [property_name, property_value] : context.properties) {
     auto property_node = collection.append_child("property");
-    property_node.append_attribute("name").set_value(propertyName.c_str());
+    property_node.append_attribute("name").set_value(property_n§ame.c_str());
 
     const auto type = propertyValue.type();
 
@@ -61,128 +61,128 @@ void append_properties(XMLNode node, const ir::ContextData& context_data)
 
     switch (type) {
       case AttributeType::String:
-        value_attr.set_value(propertyValue.as_string().c_str());
+        value_attr.set_value(property_value.as_string().c_str());
         break;
 
       case AttributeType::Int:
-        value_attr.set_value(propertyValue.as_int());
+        value_attr.set_value(property_value.as_int());
         break;
 
       case AttributeType::Float:
-        value_attr.set_value(propertyValue.as_float());
+        value_attr.set_value(property_value.as_float());
         break;
 
       case AttributeType::Bool:
-        value_attr.set_value(propertyValue.as_bool());
+        value_attr.set_value(property_value.as_bool());
         break;
 
       case AttributeType::Path: {
-        const auto str = convert_to_forward_slashes(propertyValue.as_path());
+        const auto str = convert_to_forward_slashes(property_value.as_path());
         value_attr.set_value(str.c_str());
         break;
       }
       case AttributeType::Color:
-        value_attr.set_value(propertyValue.as_color().as_argb().c_str());
+        value_attr.set_value(property_value.as_color().as_argb().c_str());
         break;
 
       case AttributeType::Object:
-        value_attr.set_value(propertyValue.as_object());
+        value_attr.set_value(property_value.as_object());
         break;
     }
   }
 }
 
-void append_object(XMLNode node, const ir::ObjectData& object_data)
+void append_object(XMLNode node, const ir::ObjectData& object)
 {
   auto object_node = node.append_child("object");
-  object_node.append_attribute("id").set_value(object_data.id);
+  object_node.append_attribute("id").set_value(object.id);
 
-  if (!object_data.name.empty()) {
-    object_node.append_attribute("name").set_value(object_data.name.c_str());
+  if (!object.name.empty()) {
+    object_node.append_attribute("name").set_value(object.name.c_str());
   }
 
-  if (!object_data.tag.empty()) {
-    object_node.append_attribute("type").set_value(object_data.tag.c_str());
+  if (!object.tag.empty()) {
+    object_node.append_attribute("type").set_value(object.tag.c_str());
   }
 
-  if (object_data.pos.x != 0.0f) {
-    object_node.append_attribute("x").set_value(object_data.pos.x);
+  if (object.pos.x != 0.0f) {
+    object_node.append_attribute("x").set_value(object.pos.x);
   }
 
-  if (object_data.pos.y != 0.0f) {
-    object_node.append_attribute("y").set_value(object_data.pos.y);
+  if (object.pos.y != 0.0f) {
+    object_node.append_attribute("y").set_value(object.pos.y);
   }
 
-  if (object_data.size.x != 0.0f) {
-    object_node.append_attribute("width").set_value(object_data.size.x);
+  if (object.size.x != 0.0f) {
+    object_node.append_attribute("width").set_value(object.size.x);
   }
 
-  if (object_data.size.y != 0.0f) {
-    object_node.append_attribute("height").set_value(object_data.size.y);
+  if (object.size.y != 0.0f) {
+    object_node.append_attribute("height").set_value(object.size.y);
   }
 
-  if (!object_data.visible) {
+  if (!object.visible) {
     object_node.append_attribute("visible").set_value(0);
   }
 
   // Objects are assumed to be rectangles unless explicitly told otherwise
-  if (object_data.type == ObjectType::Point) {
+  if (object.type == ObjectType::Point) {
     object_node.append_child("point");
   }
-  else if (object_data.type == ObjectType::Ellipse) {
+  else if (object.type == ObjectType::Ellipse) {
     object_node.append_child("ellipse");
   }
 
-  append_properties(object_node, object_data.context);
+  append_properties(object_node, object.context);
 }
 
-void append_common_layer_attributes(XMLNode node, const ir::LayerData& layer_data)
+void append_common_layer_attributes(XMLNode node, const ir::LayerData& layer)
 {
-  node.append_attribute("id").set_value(layer_data.id);
-  node.append_attribute("name").set_value(layer_data.name.c_str());
+  node.append_attribute("id").set_value(layer.id);
+  node.append_attribute("name").set_value(layer.name.c_str());
 
-  if (layer_data.opacity != 1.0f) {
-    node.append_attribute("opacity").set_value(layer_data.opacity);
+  if (layer.opacity != 1.0f) {
+    node.append_attribute("opacity").set_value(layer.opacity);
   }
 
-  if (!layer_data.visible) {
+  if (!layer.visible) {
     node.append_attribute("visible").set_value(0);
   }
 }
 
-void append_tile_layer(XMLNode root, const ir::LayerData& layer_data)
+void append_tile_layer(XMLNode root, const ir::LayerData& layer)
 {
   const auto& prefs = get_preferences();
-  const auto& tile_layer_data = std::get<ir::TileLayerData>(layer_data.data);
+  const auto& tile_layer = std::get<ir::TileLayerData>(layer.data);
 
   auto node = root.append_child("layer");
-  append_common_layer_attributes(node, layer_data);
+  append_common_layer_attributes(node, layer);
 
-  node.append_attribute("width").set_value(tile_layer_data.col_count);
-  node.append_attribute("height").set_value(tile_layer_data.row_count);
+  node.append_attribute("width").set_value(tile_layer.col_count);
+  node.append_attribute("height").set_value(tile_layer.row_count);
 
-  append_properties(node, layer_data.context);
+  append_properties(node, layer.context);
 
   auto data_node = node.append_child("data");
   data_node.append_attribute("encoding").set_value("csv");
 
-  const auto count = tile_layer_data.row_count * tile_layer_data.col_count;
+  const auto count = tile_layer.row_count * tile_layer.col_count;
 
   std::stringstream stream;
 
-  invoke_mn(tile_layer_data.row_count,
-            tile_layer_data.col_count,
+  invoke_mn(tile_layer.row_count,
+            tile_layer.col_count,
             [&, index = 0ull](const usize row, const usize col) mutable {
               if (prefs.fold_tile_data && index == 0) {
                 stream << '\n';
               }
 
-              stream << tile_layer_data.tiles[row][col];
+              stream << tile_layer.tiles[row][col];
               if (index < count - 1u) {
                 stream << ',';
               }
 
-              if (prefs.fold_tile_data && (index + 1) % tile_layer_data.col_count == 0) {
+              if (prefs.fold_tile_data && (index + 1) % tile_layer.col_count == 0) {
                 stream << '\n';
               }
 
@@ -192,126 +192,124 @@ void append_tile_layer(XMLNode root, const ir::LayerData& layer_data)
   data_node.text().set(stream.str().c_str());
 }
 
-void append_object_layer(XMLNode root, const ir::LayerData& layer_data)
+void append_object_layer(XMLNode root, const ir::LayerData& layer)
 {
-  const auto& object_layer_data = std::get<ir::ObjectLayerData>(layer_data.data);
+  const auto& object_layer = std::get<ir::ObjectLayerData>(layer.data);
 
   auto node = root.append_child("objectgroup");
-  append_common_layer_attributes(node, layer_data);
-  append_properties(node, layer_data.context);
+  append_common_layer_attributes(node, layer);
+  append_properties(node, layer.context);
 
-  for (const auto& object_data : object_layer_data.objects) {
-    append_object(node, object_data);
+  for (const auto& object : object_layer.objects) {
+    append_object(node, object);
   }
 }
 
-void append_layer(XMLNode root, const ir::LayerData& layer_data)
+void append_layer(XMLNode root, const ir::LayerData& layer)
 {
-  switch (layer_data.type) {
+  switch (layer.type) {
     case LayerType::TileLayer:
-      append_tile_layer(root, layer_data);
+      append_tile_layer(root, layer);
       break;
 
     case LayerType::ObjectLayer:
-      append_object_layer(root, layer_data);
+      append_object_layer(root, layer);
       break;
 
     case LayerType::GroupLayer: {
-      const auto& group_layer_data = std::get<ir::GroupLayerData>(layer_data.data);
+      const auto& group_layer = std::get<ir::GroupLayerData>(layer.data);
 
       auto collection = root.append_child("group");
-      append_common_layer_attributes(collection, layer_data);
-      append_properties(collection, layer_data.context);
+      append_common_layer_attributes(collection, layer);
+      append_properties(collection, layer.context);
 
-      for (const auto& child_layer_data : group_layer_data.children) {
-        append_layer(collection, *child_layer_data);
+      for (const auto& child_layer : group_layer.children) {
+        append_layer(collection, *child_layer);
       }
 
       break;
     }
-
     default:
-      throw TactileError("Invalid layer type!");
+      throw TactileError {"Invalid layer type!"};
   }
 }
 
-void append_fancy_tiles(XMLNode node, const ir::TilesetData& tileset_data)
+void append_fancy_tiles(XMLNode node, const ir::TilesetData& tileset)
 {
-  for (const auto& [id, tileData] : tileset_data.fancy_tiles) {
+  for (const auto& [id, tile] : tileset.fancy_tiles) {
     auto tile_node = node.append_child("tile");
     tile_node.append_attribute("id").set_value(id);
 
     if (!tileData.frames.empty()) {
       auto animation_node = tile_node.append_child("animation");
 
-      for (const auto& frame_data : tileData.frames) {
+      for (const auto& frame : tile.frames) {
         auto frame_node = animation_node.append_child("frame");
-        frame_node.append_attribute("tileid").set_value(frame_data.local_id);
-        frame_node.append_attribute("duration").set_value(frame_data.duration_ms);
+        frame_node.append_attribute("tileid").set_value(frame.local_id);
+        frame_node.append_attribute("duration").set_value(frame.duration_ms);
       }
     }
 
-    if (!tileData.objects.empty()) {
+    if (!tile.objects.empty()) {
       auto object_layer_node = tile_node.append_child("objectgroup");
       object_layer_node.append_attribute("draworder").set_value("index");
 
-      for (const auto& object_data : tileData.objects) {
-        append_object(object_layer_node, object_data);
+      for (const auto& object : tile.objects) {
+        append_object(object_layer_node, object);
       }
     }
 
-    append_properties(tile_node, tileData.context);
+    append_properties(tile_node, tile.context);
   }
 }
 
 void append_common_tileset_attributes(XMLNode                node,
-                                      const ir::TilesetData& tileset_data,
+                                      const ir::TilesetData& tileset,
                                       const fs::path&        dir)
 {
-  node.append_attribute("name").set_value(tileset_data.name.c_str());
+  node.append_attribute("name").set_value(tileset.name.c_str());
 
-  node.append_attribute("tilewidth").set_value(tileset_data.tile_size.x);
-  node.append_attribute("tileheight").set_value(tileset_data.tile_size.y);
+  node.append_attribute("tilewidth").set_value(tileset.tile_size.x);
+  node.append_attribute("tileheight").set_value(tileset.tile_size.y);
 
-  node.append_attribute("tilecount").set_value(tileset_data.tile_count);
-  node.append_attribute("columns").set_value(tileset_data.column_count);
+  node.append_attribute("tilecount").set_value(tileset.tile_count);
+  node.append_attribute("columns").set_value(tileset.column_count);
 
   {
     auto image_node = node.append_child("image");
 
-    const auto source =
-        convert_to_forward_slashes(fs::relative(tileset_data.image_path, dir));
+    const auto source = convert_to_forward_slashes(fs::relative(tileset.image_path, dir));
     image_node.append_attribute("source").set_value(source.c_str());
 
-    image_node.append_attribute("width").set_value(tileset_data.image_size.x);
-    image_node.append_attribute("height").set_value(tileset_data.image_size.y);
+    image_node.append_attribute("width").set_value(tileset.image_size.x);
+    image_node.append_attribute("height").set_value(tileset.image_size.y);
   }
 
-  append_fancy_tiles(node, tileset_data);
-  append_properties(node, tileset_data.context);
+  append_fancy_tiles(node, tileset);
+  append_properties(node, tileset.context);
 }
 
 void append_embedded_tileset(XMLNode                root,
-                             const ir::TilesetData& tileset_data,
+                             const ir::TilesetData& tileset,
                              const fs::path&        dir)
 {
   auto node = root.append_child("tileset");
-  node.append_attribute("firstgid").set_value(tileset_data.first_tile);
+  node.append_attribute("firstgid").set_value(tileset.first_tile);
 
-  append_common_tileset_attributes(node, tileset_data, dir);
+  append_common_tileset_attributes(node, tileset, dir);
 }
 
 void append_external_tileset(XMLNode                root,
-                             const ir::TilesetData& tileset_data,
+                             const ir::TilesetData& tileset,
                              const std::string&     filename)
 {
   auto node = root.append_child("tileset");
-  node.append_attribute("firstgid").set_value(tileset_data.first_tile);
+  node.append_attribute("firstgid").set_value(tileset.first_tile);
   node.append_attribute("source").set_value(filename.c_str());
 }
 
 void emit_external_tileset_file(const fs::path&        path,
-                                const ir::TilesetData& tileset_data,
+                                const ir::TilesetData& tileset,
                                 const fs::path&        dir)
 {
   pugi::xml_document document;
@@ -320,31 +318,29 @@ void emit_external_tileset_file(const fs::path&        path,
   root.append_attribute("version").set_value(tiled_xml_format_version);
   root.append_attribute("tiledversion").set_value(tiled_version);
 
-  append_common_tileset_attributes(root, tileset_data, dir);
+  append_common_tileset_attributes(root, tileset, dir);
 
   std::ofstream stream {path, std::ios::out};
   document.save(stream);
 }
 
-void append_tileset(XMLNode                root,
-                    const ir::TilesetData& tileset_data,
-                    const fs::path&        dir)
+void append_tileset(XMLNode root, const ir::TilesetData& tileset, const fs::path& dir)
 {
   const auto& prefs = get_preferences();
   if (prefs.embed_tilesets) {
-    append_embedded_tileset(root, tileset_data, dir);
+    append_embedded_tileset(root, tileset, dir);
   }
   else {
-    const auto filename = fmt::format("{}.tsx", tileset_data.name);
+    const auto filename = fmt::format("{}.tsx", tileset.name);
     const auto source = dir / filename;
-    emit_external_tileset_file(source, tileset_data, dir);
-    append_external_tileset(root, tileset_data, filename);
+    emit_external_tileset_file(source, tileset, dir);
+    append_external_tileset(root, tileset, filename);
   }
 }
 
 void append_root(pugi::xml_document& document, const EmitInfo& info)
 {
-  const auto& map_data = info.data();
+  const auto& map = info.data();
   auto        root = document.append_child("map");
 
   root.append_attribute("version").set_value(tiled_xml_format_version);
@@ -354,23 +350,23 @@ void append_root(pugi::xml_document& document, const EmitInfo& info)
   root.append_attribute("renderorder").set_value("right-down");
   root.append_attribute("infinite").set_value(0);
 
-  root.append_attribute("tilewidth").set_value(map_data.tile_size.x);
-  root.append_attribute("tileheight").set_value(map_data.tile_size.y);
+  root.append_attribute("tilewidth").set_value(map.tile_size.x);
+  root.append_attribute("tileheight").set_value(map.tile_size.y);
 
-  root.append_attribute("width").set_value(map_data.col_count);
-  root.append_attribute("height").set_value(map_data.row_count);
+  root.append_attribute("width").set_value(map.col_count);
+  root.append_attribute("height").set_value(map.row_count);
 
-  root.append_attribute("nextlayerid").set_value(map_data.next_layer_id);
-  root.append_attribute("nextobjectid").set_value(map_data.next_object_id);
+  root.append_attribute("nextlayerid").set_value(map.next_layer_id);
+  root.append_attribute("nextobjectid").set_value(map.next_object_id);
 
-  append_properties(root, map_data.context);
+  append_properties(root, map.context);
 
-  for (const auto& tileset_data : map_data.tilesets) {
-    append_tileset(root, tileset_data, info.destination_dir());
+  for (const auto& tileset : map.tilesets) {
+    append_tileset(root, tileset, info.destination_dir());
   }
 
-  for (const auto& layer_data : map_data.layers) {
-    append_layer(root, layer_data);
+  for (const auto& layer : map.layers) {
+    append_layer(root, layer);
   }
 }
 
