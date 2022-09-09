74a4b2514a1b0927ae7a473d73486f4275c8ec99
Albin Johansson <albinoscar99@hotmail.com>
2022-09-09 15:25:03 +0200

    Update alignment.hpp

--- source/app/editor/ui/alignment.hpp
+++ source/app/editor/ui/alignment.hpp
@@ -25,63 +25,47 @@
 
 namespace tactile::ui {
 
-/**
- * Makes the next window centered when it is first shown.
- */
+/// Makes the next window centered when it is first shown.
 void center_next_window_on_appearance();
 
-/**
- * Centers the next item horizontally, given its width.
- *
- * \param width the width of the component.
- */
+/// Centers the next item horizontally, given its width.
+///
+/// \param width the width of the component.
 void center_next_item_horizontally(float width);
 
-/**
- * Ensures that a certain amount of components will be centered vertically.
- *
- * \param count the amount of items to be centered.
- */
+/// Ensures that a specific amount of components will be centered vertically.
+///
+/// \param count the amount of items to be centered.
 void prepare_vertical_alignment_center(float count = 1);
 
+/// Prepares the widget cursor for a right-aligned component.
 void right_align_next_item();
 
-/**
- * Prepares the cursor for a right-aligned component.
- *
- * \param width the width of the component.
- *
- * \return the new cursor x-coordinate.
- */
+/// Prepares the widget cursor for a right-aligned component.
+///
+/// \param width the width of the component.
+/// \return the new cursor x-coordinate.
 auto right_align_next_item(float width) -> float;
 
-/**
- * Prepares the cursor for a right-aligned standard component with a label.
- *
- * \param text the component label text.
- *
- * \return the new cursor x-coordinate.
- *
- * \see standard_component_width()
- */
+/// Prepares the cursor for a right-aligned standard component with a label.
+///
+/// Use the `standard_component_width()` to query expected component widths 
+/// based on the label of the component.
+///
+/// \param text the component label text.
+/// \return the new cursor x-coordinate.
 auto right_align_next_item(const char* text) -> float;
 
-/**
- * Returns the width of a "standard" component with a label, e.g. a button.
- *
- * \param text the component label text.
- *
- * \return the component width.
- */
+/// Returns the width of a "standard" component with a label, e.g. a button.
+///
+/// \param text the component label text.
+/// \return the component width.
 [[nodiscard]] auto standard_component_width(const char* text) -> float;
 
-/**
- * Determines the minimum offset to align a collection of strings.
- *
- * \param strings a pack of strings.
- *
- * \return the minimum offset.
- */
+/// Determines the minimum offset to align a collection of strings.
+///
+/// \param strings a pack of strings.
+/// \return the minimum offset.
 [[nodiscard]] auto minimum_offset_to_align(auto&&... strings) -> float
 {
   const auto spacing = ImGui::GetStyle().ItemSpacing.x * 2.0f;
