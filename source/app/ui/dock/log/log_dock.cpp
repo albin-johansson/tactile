bd1acce797dc0d055b2546d56a5e1d80864e3a3a
Albin Johansson <albinoscar99@hotmail.com>
2022-12-28 23:23:25 +0100

    Improve log dock appearance

--- source/app/ui/dock/log/log_dock.cpp
+++ source/app/ui/dock/log/log_dock.cpp
@@ -23,51 +23,28 @@
 
 #include "core/debug/logging.hpp"
 #include "core/debug/panic.hpp"
-#include "core/type/maybe.hpp"
+#include "core/type/hash_map.hpp"
+#include "core/util/assoc.hpp"
 #include "io/proto/preferences.hpp"
 #include "lang/language.hpp"
 #include "lang/strings.hpp"
-#include "ui/constants.hpp"
-#include "ui/style/icons.hpp"
 #include "ui/widget/scoped.hpp"
 #include "ui/widget/widgets.hpp"
 
 namespace tactile::ui {
 namespace {
 
-constexpr auto window_flags = ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoScrollbar;
-constexpr auto child_flags = ImGuiWindowFlags_AlwaysVerticalScrollbar |
-                             ImGuiWindowFlags_HorizontalScrollbar |
-                             ImGuiWindowFlags_AlwaysAutoResize;
-
 constinit LogFilter log_filter;
 constinit bool is_dock_focused = false;
 
-[[nodiscard]] constexpr auto color_for_level(const LogLevel level) -> ImVec4
-{
-  switch (level) {
-    case LogLevel::trace:
-      return {0.93f, 0.51f, 0.93f, 1.0f};
-
-    case LogLevel::debug:
-      return {0.5f, 1.0f, 0.7f, 1.0f};
-
-    case LogLevel::info:
-      return {1.0f, 1.0f, 1.0f, 1.00f};
-
-    case LogLevel::warn:
-      return {1.0f, 1.0f, 0.00f, 1.00f};
-
-    case LogLevel::err:
-      return {1.00f, 0.40f, 0.40f, 1.00f};
-
-    case LogLevel::critical:
-      return {1.00f, 0.00f, 0.00f, 1.00f};
-
-    default:
-      throw TactileError {"Did not recognize log level!"};
-  }
-}
+const HashMap<LogLevel, ImVec4> log_level_colors = {
+    {LogLevel::trace, ImVec4 {0.93f, 0.51f, 0.93f, 1.00f}},
+    {LogLevel::debug, ImVec4 {0.50f, 1.00f, 0.70f, 1.00f}},
+    {LogLevel::info, ImVec4 {1.00f, 1.00f, 1.00f, 1.00f}},
+    {LogLevel::warn, ImVec4 {1.00f, 1.00f, 0.00f, 1.00f}},
+    {LogLevel::err, ImVec4 {1.00f, 0.40f, 0.40f, 1.00f}},
+    {LogLevel::critical, ImVec4 {1.00f, 0.00f, 0.00f, 1.00f}},
+};
 
 void ui_message_filter_selector()
 {
@@ -78,27 +55,38 @@ void ui_message_filter_selector()
   }
 
   if (const Popup popup {"##FilterPopup"}; popup.is_open()) {
-    if (ImGui::MenuItem(lang.misc.log_trace_filter.c_str(), nullptr, log_filter.trace)) {
+    if (const StyleColor text {ImGuiCol_Text,
+                               lookup_in(log_level_colors, LogLevel::trace)};
+        ImGui::MenuItem(lang.misc.log_trace_filter.c_str(), nullptr, log_filter.trace)) {
       log_filter.trace = !log_filter.trace;
     }
 
-    if (ImGui::MenuItem(lang.misc.log_debug_filter.c_str(), nullptr, log_filter.debug)) {
+    if (const StyleColor text {ImGuiCol_Text,
+                               lookup_in(log_level_colors, LogLevel::debug)};
+        ImGui::MenuItem(lang.misc.log_debug_filter.c_str(), nullptr, log_filter.debug)) {
       log_filter.debug = !log_filter.debug;
     }
 
-    if (ImGui::MenuItem(lang.misc.log_info_filter.c_str(), nullptr, log_filter.info)) {
+    if (const StyleColor text {ImGuiCol_Text,
+                               lookup_in(log_level_colors, LogLevel::info)};
+        ImGui::MenuItem(lang.misc.log_info_filter.c_str(), nullptr, log_filter.info)) {
       log_filter.info = !log_filter.info;
     }
 
-    if (ImGui::MenuItem(lang.misc.log_warn_filter.c_str(), nullptr, log_filter.warn)) {
+    if (const StyleColor text {ImGuiCol_Text,
+                               lookup_in(log_level_colors, LogLevel::warn)};
+        ImGui::MenuItem(lang.misc.log_warn_filter.c_str(), nullptr, log_filter.warn)) {
       log_filter.warn = !log_filter.warn;
     }
 
-    if (ImGui::MenuItem(lang.misc.log_error_filter.c_str(), nullptr, log_filter.error)) {
+    if (const StyleColor text {ImGuiCol_Text, lookup_in(log_level_colors, LogLevel::err)};
+        ImGui::MenuItem(lang.misc.log_error_filter.c_str(), nullptr, log_filter.error)) {
       log_filter.error = !log_filter.error;
     }
 
-    if (ImGui::MenuItem(lang.misc.log_critical_filter.c_str(),
+    if (const StyleColor text {ImGuiCol_Text,
+                               lookup_in(log_level_colors, LogLevel::critical)};
+        ImGui::MenuItem(lang.misc.log_critical_filter.c_str(),
                         nullptr,
                         log_filter.critical)) {
       log_filter.critical = !log_filter.critical;
@@ -106,34 +94,13 @@ void ui_message_filter_selector()
   }
 }
 
-void ui_logged_message_color_legend()
-{
-  ImGui::TextDisabled("(?)");
-
-  if (ImGui::IsItemHovered()) {
-    const StyleColor bg {ImGuiCol_PopupBg, {0.1f, 0.1f, 0.1f, 0.75f}};
-    const Tooltip tooltip;
-
-    constexpr auto verbose_color = color_for_level(LogLevel::trace);
-    constexpr auto debug_color = color_for_level(LogLevel::debug);
-    constexpr auto info_color = color_for_level(LogLevel::info);
-    constexpr auto warning_color = color_for_level(LogLevel::warn);
-    constexpr auto error_color = color_for_level(LogLevel::err);
-    constexpr auto critical_color = color_for_level(LogLevel::critical);
-
-    const auto& lang = get_current_language();
-    ImGui::TextColored(verbose_color, "%s", lang.tooltip.log_verbose_msg.c_str());
-    ImGui::TextColored(debug_color, "%s", lang.tooltip.log_debug_msg.c_str());
-    ImGui::TextColored(info_color, "%s", lang.tooltip.log_info_msg.c_str());
-    ImGui::TextColored(warning_color, "%s", lang.tooltip.log_warn_msg.c_str());
-    ImGui::TextColored(error_color, "%s", lang.tooltip.log_error_msg.c_str());
-    ImGui::TextColored(critical_color, "%s", lang.tooltip.log_critical_msg.c_str());
-  }
-}
-
 void ui_logged_message_view(const usize message_count)
 {
   const StyleColor child_bg {ImGuiCol_ChildBg, {0.1f, 0.1f, 0.1f, 0.75f}};
+
+  constexpr auto child_flags = ImGuiWindowFlags_AlwaysVerticalScrollbar |
+                               ImGuiWindowFlags_HorizontalScrollbar |
+                               ImGuiWindowFlags_AlwaysAutoResize;
 
   if (const Child pane {"##LogPane", {}, true, child_flags}; pane.is_open()) {
     ImGuiListClipper clipper;
@@ -144,7 +111,7 @@ void ui_logged_message_view(const usize message_count)
                                  clipper.DisplayStart,
                                  clipper.DisplayEnd,
                                  [](const LogLevel level, const String& msg) {
-                                   const auto color = color_for_level(level);
+                                   const auto& color = lookup_in(log_level_colors, level);
                                    ImGui::TextColored(color, "%s", msg.c_str());
                                  });
     }
@@ -168,15 +135,13 @@ void update_log_dock()
 
   const auto& lang = get_current_language();
 
-  const Window dock {lang.window.log_dock.c_str(), window_flags, &prefs.show_log_dock};
+  const Window dock {lang.window.log_dock.c_str(),
+                     ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoScrollbar,
+                     &prefs.show_log_dock};
   is_dock_focused = dock.has_focus(ImGuiFocusedFlags_RootAndChildWindows);
 
   if (dock.is_open()) {
     ui_message_filter_selector();
-
-
-    ImGui::SameLine();
-    ui_logged_message_color_legend();
 
     const auto message_count = count_matching_log_entries(log_filter);
     if (message_count != 0u) {
