33480dd2a038b5aa4ba3adca4d1605830473f3ad
Albin Johansson <albinoscar99@hotmail.com>
2023-01-19 08:30:26 +0100

    Log property events

--- source/app/handlers/property_event_handler.cpp
+++ source/app/handlers/property_event_handler.cpp
@@ -18,7 +18,10 @@
  */
 
 #include <entt/signal/dispatcher.hpp>
+#include <magic_enum.hpp>
+#include <spdlog/spdlog.h>
 
+#include "common/util/fmt.hpp"
 #include "core/context/context_manager.hpp"
 #include "handlers/event_handlers.hpp"
 #include "model/document/document.hpp"
@@ -32,8 +35,10 @@
 namespace tactile {
 namespace {
 
-void on_show_new_property_dialog()
+void on_show_new_property_dialog(const ShowNewPropertyDialogEvent&)
 {
+  spdlog::trace("ShowNewPropertyDialogEvent");
+
   if (auto* document = get_model().active_document()) {
     const auto& context_id = document->get_contexts().active_context_id();
     ui::open_add_property_dialog(context_id);
@@ -42,6 +47,8 @@ void on_show_new_property_dialog()
 
 void on_show_rename_property_dialog(const ShowRenamePropertyDialogEvent& event)
 {
+  spdlog::trace("ShowRenamePropertyDialogEvent");
+
   if (const auto* document = get_model().active_document()) {
     const auto& context_id = document->get_contexts().active_context_id();
     ui::open_rename_property_dialog(context_id, event.current_name);
@@ -50,6 +57,10 @@ void on_show_rename_property_dialog(const ShowRenamePropertyDialogEvent& event)
 
 void on_show_change_property_type_dialog(const ShowChangePropertyTypeDialogEvent& event)
 {
+  spdlog::trace("ShowChangePropertyTypeDialogEvent(name: {}, current_type: {})",
+                event.name,
+                event.current_type);
+
   if (const auto* document = get_model().active_document()) {
     const auto& context_id = document->get_contexts().active_context_id();
     ui::open_change_property_type_dialog(context_id, event.name, event.current_type);
@@ -58,6 +69,11 @@ void on_show_change_property_type_dialog(const ShowChangePropertyTypeDialogEvent
 
 void on_add_property(const AddPropertyEvent& event)
 {
+  spdlog::trace("AddPropertyEvent(context_id: {}, name: {}, type: {})",
+                event.context_id,
+                event.name,
+                event.type);
+
   if (auto* document = get_model().active_document()) {
     document->add_property(event.context_id, event.name, event.type);
   }
@@ -65,6 +81,10 @@ void on_add_property(const AddPropertyEvent& event)
 
 void on_remove_property(const RemovePropertyEvent& event)
 {
+  spdlog::trace("RemovePropertyEvent(context_id: {}, name: {})",
+                event.context_id,
+                event.name);
+
   if (auto* document = get_model().active_document()) {
     document->remove_property(event.context_id, event.name);
   }
@@ -72,6 +92,11 @@ void on_remove_property(const RemovePropertyEvent& event)
 
 void on_rename_property(const RenamePropertyEvent& event)
 {
+  spdlog::trace("RenamePropertyEvent(context_id: {}, old_name: {}, new_name: {})",
+                event.context_id,
+                event.old_name,
+                event.new_name);
+
   if (auto* document = get_model().active_document()) {
     document->rename_property(event.context_id, event.old_name, event.new_name);
   }
@@ -79,6 +104,11 @@ void on_rename_property(const RenamePropertyEvent& event)
 
 void on_update_property(const UpdatePropertyEvent& event)
 {
+  spdlog::trace("UpdatePropertyEvent(context_id: {}, name: {}, value: {})",
+                event.context_id,
+                event.name,
+                event.value);
+
   if (auto* document = get_model().active_document()) {
     document->update_property(event.context_id, event.name, event.value);
   }
@@ -86,6 +116,11 @@ void on_update_property(const UpdatePropertyEvent& event)
 
 void on_change_property_type(const ChangePropertyTypeEvent& event)
 {
+  spdlog::trace("ChangePropertyTypeEvent(context_id: {}, name: {}, type: {})",
+                event.context_id,
+                event.name,
+                event.type);
+
   if (auto* document = get_model().active_document()) {
     document->change_property_type(event.context_id, event.name, event.type);
   }
@@ -93,6 +128,8 @@ void on_change_property_type(const ChangePropertyTypeEvent& event)
 
 void on_inspect_context(const InspectContextEvent& event)
 {
+  spdlog::trace("InspectContextEvent(context_id: {})", event.context_id);
+
   if (auto* document = get_model().active_document()) {
     document->get_contexts().select(event.context_id);
   }
