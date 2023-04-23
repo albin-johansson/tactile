2bcc0807e702a20ec70981ca980238fb3e45f8f1
Albin Johansson <albinoscar99@hotmail.com>
2023-04-23 14:20:45 +0200

    Remove some old OOP stuff

--- source/lib/core/texture.hpp
+++ source/lib/core/texture.hpp
@@ -29,31 +29,7 @@ namespace tactile {
 
 TACTILE_FWD_DECLARE_STRUCT(TextureHandle)
 
-/// Represents an OpenGL texture.
-class Texture final {
- public:
-  TACTILE_DELETE_COPY(Texture);
 
-  Texture(uint id, Int2 size, Path path);
-
-  ~Texture() noexcept;
-
-  Texture(Texture&& other) noexcept;
-
-  auto operator=(Texture&& other) noexcept -> Texture&;
-
-  [[nodiscard]] auto get_id() const -> uint { return mId; }
-
-  [[nodiscard]] auto get_size() const -> Int2 { return mSize; }
-
-  [[nodiscard]] auto get_path() const -> const Path& { return mPath; }
-
- private:
-  uint mId {};
-  Int2 mSize {};
-  Path mPath;
-
-  void destroy() noexcept;
 };
 
 }  // namespace tactile
