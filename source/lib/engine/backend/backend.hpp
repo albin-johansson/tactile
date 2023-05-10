462cb1b44e5af030efb86a17265872a91bef2e08
Albin Johansson <albinoscar99@hotmail.com>
2023-05-10 21:04:15 +0200

    Add ImGui backend interface

---
+++ source/lib/engine/backend/backend.hpp
@@ -0,0 +1,48 @@
+/*
+ * This source file is a part of the Tactile map editor.
+ *
+ * Copyright (C) 2023 Albin Johansson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <SDL2/SDL.h>
+
+#include "common/type/result.hpp"
+
+namespace tactile {
+
+/// Interface for ImGui rendering backends.
+class Backend {
+ public:
+  virtual ~Backend() noexcept = default;
+
+  virtual void process_event(const SDL_Event* event) = 0;
+
+  /// Attempts to begin a new frame.
+  /// \return `success` if rendering can proceed; `failure` otherwise.
+  virtual auto new_frame() -> Result = 0;
+
+  /// Ends the current frame.
+  /// \note This function can only be called after a successful call to `new_frame`.
+  virtual void end_frame() = 0;
+
+  /// Attempts to reload font resources.
+  /// \return `success` if the font resources were reloaded; `failure` otherwise.
+  virtual auto reload_font_resources() -> Result { return failure; }
+};
+
+}  // namespace tactile
