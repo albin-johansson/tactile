c30013fa0b3361b8dc40cbcac8a8bdb1f2e7c921
Albin Johansson <albinoscar99@hotmail.com>
2023-05-19 11:45:29 +0200

    Add font system

---
+++ source/lib/model/systems/font_system.hpp
@@ -0,0 +1,32 @@
+/*
+ * This source file is a part of the Tactile map editor.
+ *
+ * Copyright (C) 2023 Albin Johansson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "model/model.hpp"
+
+namespace tactile::sys {
+
+[[nodiscard]] auto can_increase_font_size(const Model& model) -> bool;
+
+[[nodiscard]] auto can_decrease_font_size(const Model& model) -> bool;
+
+[[nodiscard]] auto can_reset_font_size(const Model& model) -> bool;
+
+}  // namespace tactile::sys
