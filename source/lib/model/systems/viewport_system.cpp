2e6a83b3ba53ba7e41135a6095f5f6467bd1562d
Albin Johansson <albinoscar99@hotmail.com>
2023-05-19 11:46:22 +0200

    Add viewport system

---
+++ source/lib/model/systems/viewport_system.cpp
@@ -0,0 +1,39 @@
+/*
+ * This source file is a part of the Tactile map editor.
+ *
+ * Copyright (C) 2023 Albin Johansson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "viewport_system.hpp"
+
+#include "core/viewport.hpp"
+#include "model/systems/document_system.hpp"
+
+namespace tactile::sys {
+
+auto is_viewport_zoom_out_possible(const Model& model) -> bool
+{
+  const auto document_entity = sys::get_active_document(model);
+
+  if (document_entity != kNullEntity) {
+    const auto& viewport = model.get<Viewport>(document_entity);
+    return viewport.can_zoom_out();
+  }
+
+  return false;
+}
+
+}  // namespace tactile::sys
