8cccba6a9fff9b813bf9385fb66d6ae37afe2a44
Albin Johansson <albinoscar99@hotmail.com>
2023-05-23 16:38:04 +0200

    Update compression_test.cpp

--- test/src/io/compression_test.cpp
+++ test/src/io/compression_test.cpp
@@ -1,6 +1,8 @@
 #include "io/compression.hpp"
 
 #include <doctest/doctest.h>
+#include <zlib.h>
+#include <zstd.h>
 
 #include "common/util/random.hpp"
 
@@ -30,7 +32,7 @@ TEST_SUITE("Compression")
     const auto compressed_bytes = zlib_compress(ByteSpan {original_bytes}).value();
     const auto decompressed_bytes = zlib_decompress(ByteSpan {compressed_bytes}).value();
 
-    REQUIRE(original_bytes == decompressed_bytes);
+    CHECK(original_bytes == decompressed_bytes);
   }
 
   TEST_CASE("zstd compress and decompress")
@@ -40,7 +42,46 @@ TEST_SUITE("Compression")
     const auto compressed_bytes = zstd_compress(ByteSpan {original_bytes}).value();
     const auto decompressed_bytes = zstd_decompress(ByteSpan {compressed_bytes}).value();
 
-    REQUIRE(original_bytes == decompressed_bytes);
+    CHECK(original_bytes == decompressed_bytes);
+  }
+
+  TEST_CASE("Zlib compression level bounds")
+  {
+    CHECK(min_zlib_compression_level() == Z_BEST_SPEED);
+    CHECK(max_zlib_compression_level() == Z_BEST_COMPRESSION);
+  }
+
+  TEST_CASE("Zstd compression level bounds")
+  {
+    // Zstd technically accepts other levels, but we use this range
+    CHECK(min_zstd_compression_level() == 1);
+    CHECK(max_zstd_compression_level() == 19);
+  }
+
+  TEST_CASE("Zlib compression level checking")
+  {
+    CHECK(is_valid_zlib_compression_level(Z_DEFAULT_COMPRESSION));
+    CHECK(is_valid_zlib_compression_level(Z_BEST_SPEED));
+    CHECK(is_valid_zlib_compression_level(Z_BEST_COMPRESSION));
+
+    CHECK(!is_valid_zlib_compression_level(Z_BEST_SPEED - 1));
+    CHECK(!is_valid_zlib_compression_level(Z_BEST_COMPRESSION + 1));
+
+    CHECK(is_valid_zlib_compression_level(Z_BEST_SPEED + 1));
+    CHECK(is_valid_zlib_compression_level(Z_BEST_COMPRESSION - 1));
+  }
+
+  TEST_CASE("Zstd compression level checking")
+  {
+    CHECK(is_valid_zstd_compression_level(ZSTD_defaultCLevel()));
+    CHECK(is_valid_zstd_compression_level(1));
+    CHECK(is_valid_zstd_compression_level(19));
+
+    CHECK(!is_valid_zstd_compression_level(0));
+    CHECK(!is_valid_zstd_compression_level(20));
+
+    CHECK(is_valid_zstd_compression_level(2));
+    CHECK(is_valid_zstd_compression_level(10));
   }
 }
 
