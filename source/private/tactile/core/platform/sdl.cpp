057dda081321b9de6cf24da48d3044723eec5c88
Albin Johansson <albinoscar99@hotmail.com>
2023-08-15 17:45:02 +0200

    Add new SDL initializer class

---
+++ source/private/tactile/core/platform/sdl.cpp
@@ -0,0 +1,44 @@
+/*
+ * This source file is a part of the Tactile map editor.
+ *
+ * Copyright (C) 2023 Albin Johansson
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include "tactile/core/platform/sdl.hpp"
+
+#include <SDL2/SDL.h>
+
+#include "tactile/core/common/error.hpp"
+
+namespace tactile {
+
+SDL::SDL()
+{
+  SDL_SetHint(SDL_HINT_APP_NAME, "Tactile");
+  SDL_SetHint(SDL_HINT_MOUSE_TOUCH_EVENTS, "1");  // Enable touchpad multi-gesture events
+  SDL_SetHint(SDL_HINT_RENDER_SCALE_QUALITY, "nearest");  // Ensure nearest pixel sampling
+
+  if (SDL_Init(SDL_INIT_TIMER | SDL_INIT_VIDEO | SDL_INIT_EVENTS) < 0) {
+    throw Error {"Could not initialize SDL"};
+  }
+}
+
+SDL::~SDL() noexcept
+{
+  SDL_Quit();
+}
+
+}  // namespace tactile
