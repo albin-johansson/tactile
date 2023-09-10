eed4b4a3650ce029a6da904a7124e353c0733590
Albin Johansson <albinoscar99@hotmail.com>
2023-09-10 17:48:18 +0200

    Add TMJ format plugin skeleton

---
+++ modules/main/src/main.cpp
@@ -0,0 +1,8 @@
+// Copyright (C) 2023 Albin Johansson (GNU General Public License v3.0)
+
+#include <cstdlib>    // EXIT_SUCCESS
+
+auto main(const int argc, char* argv[]) -> int
+{
+    return EXIT_SUCCESS;
+}
---
+++ modules/tmj-format/inc/tactile/tmj-format/api.hpp
@@ -0,0 +1,11 @@
+// Copyright (C) 2023 Albin Johansson (GNU General Public License v3.0)
+
+#pragma once
+
+#include "tactile/core/prelude.hpp"
+
+#ifdef TACTILE_BUILDING_TMJ_PLUGIN
+  #define TACTILE_TMJ_API TACTILE_DLL_EXPORT
+#else
+  #define TACTILE_TMJ_API TACTILE_DLL_IMPORT
+#endif
