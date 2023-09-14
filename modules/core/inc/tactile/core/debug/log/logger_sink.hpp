1d4c2ee3fc4f40bab464e377ea74e0d5b185b185
Albin Johansson <albinoscar99@hotmail.com>
2023-09-14 23:00:24 +0200

    Create logger_sink.hpp

---
+++ modules/core/inc/tactile/core/debug/log/logger_sink.hpp
@@ -0,0 +1,45 @@
+// Copyright (C) 2023 Albin Johansson (GNU General Public License v3.0)
+
+#pragma once
+
+#include "tactile/core/api.hpp"
+#include "tactile/core/debug/log/log_level.hpp"
+#include "tactile/core/prelude.hpp"
+#include "tactile/core/type/chrono.hpp"
+#include "tactile/core/type/string.hpp"
+
+namespace tactile {
+
+/**
+ * Represents a single logged message.
+ *
+ * \property   level the severity of the message.
+ * \property    text the formatted log message.
+ * \property instant the instant that the message was logged.
+ */
+struct TACTILE_CORE_API LogMessage final {
+  LogLevel level;
+  StringView text;
+  SteadyClockInstant instant;
+};
+
+/**
+ * Interface for logger "sink" implementations.
+ *
+ * \details A logger may feature several associated sinks which are called
+ *          whenever a message is logged. This allows for the application to
+ *          control exactly what happens with logged messages, e.g., whether to
+ *          print them to a console or store them in a file.
+ */
+class TACTILE_CORE_API ILoggerSink {
+ public:
+  TACTILE_INTERFACE_CLASS(ILoggerSink);
+
+  /** Logs an incoming message. */
+  virtual void log(const LogMessage& msg) = 0;
+
+  /** Flushes any pending state, most likely an associated I/O stream. */
+  virtual void flush() = 0;
+};
+
+}  // namespace tactile
