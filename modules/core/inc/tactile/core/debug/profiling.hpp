9f9478545f08ec09069a307708538c95a3277677
Albin Johansson <albinoscar99@hotmail.com>
2023-09-15 18:52:15 +0200

    Update profiling.hpp

--- modules/core/inc/tactile/core/debug/profiling.hpp
+++ modules/core/inc/tactile/core/debug/profiling.hpp
@@ -3,20 +3,20 @@
 #pragma once
 
 #include <fmt/chrono.h>
-#include <spdlog/spdlog.h>
 
+#include "tactile/core/debug/log/logger.hpp"
 #include "tactile/core/prelude.hpp"
 #include "tactile/core/type/chrono.hpp"
 
 #define TACTILE_PROFILE_START \
-  const auto tactile_profile_start = tactile::SystemClock::now();
+  const auto tactile_profile_start = tactile::SteadyClock::now();
 
 #define TACTILE_PROFILE_END(Msg)                                                 \
-  const auto tactile_profile_end = tactile::SystemClock::now();                  \
+  const auto tactile_profile_end = tactile::SteadyClock::now();                  \
   const auto tactile_profile_diff = tactile_profile_end - tactile_profile_start; \
   const auto tactile_profile_ms =                                                \
       chrono::duration_cast<tactile::ms_t>(tactile_profile_diff);                \
-  spdlog::debug(Msg " in {}", tactile_profile_ms);
+  TACTILE_LOG_DEBUG(Msg " in {}", tactile_profile_ms);
 
 #if TACTILE_DEBUG
   #define TACTILE_DEBUG_PROFILE_START TACTILE_PROFILE_START
