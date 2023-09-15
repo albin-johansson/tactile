42e6366ec549526e66a2736cc2171603fe444ec5
Albin Johansson <albinoscar99@hotmail.com>
2023-09-15 18:32:12 +0200

    Add logger class

---
+++ modules/core/inc/tactile/core/debug/log/logger.hpp
@@ -0,0 +1,88 @@
+// Copyright (C) 2023 Albin Johansson (GNU General Public License v3.0)
+
+#pragma once
+
+#include <fmt/core.h>
+
+#include "tactile/core/api.hpp"
+#include "tactile/core/debug/log/log_level.hpp"
+#include "tactile/core/prelude.hpp"
+#include "tactile/core/type/chrono.hpp"
+#include "tactile/core/type/maybe.hpp"
+#include "tactile/core/type/string.hpp"
+#include "tactile/core/type/vector.hpp"
+
+#define TACTILE_LOG(Level, FmtString, ...)                                           \
+  do {                                                                               \
+    auto* tactile_logger = tactile::get_logger();                                    \
+    if (tactile_logger && tactile_logger->would_log((Level))) {                      \
+      tactile_logger->log((Level), (FmtString), fmt::make_format_args(__VA_ARGS__)); \
+    }                                                                                \
+  } while (false)
+
+#define TACTILE_LOG_TRACE(FmtString, ...) \
+  TACTILE_LOG(tactile::LogLevel::kTrace, FmtString __VA_OPT__(, ) __VA_ARGS__)
+
+#define TACTILE_LOG_DEBUG(FmtString, ...) \
+  TACTILE_LOG(tactile::LogLevel::kDebug, FmtString __VA_OPT__(, ) __VA_ARGS__)
+
+#define TACTILE_LOG_INFO(FmtString, ...) \
+  TACTILE_LOG(tactile::LogLevel::kInfo, FmtString __VA_OPT__(, ) __VA_ARGS__)
+
+#define TACTILE_LOG_WARN(FmtString, ...) \
+  TACTILE_LOG(tactile::LogLevel::kWarn, FmtString __VA_OPT__(, ) __VA_ARGS__)
+
+#define TACTILE_LOG_ERROR(FmtString, ...) \
+  TACTILE_LOG(tactile::LogLevel::kError, FmtString __VA_OPT__(, ) __VA_ARGS__)
+
+#define TACTILE_LOG_FATAL(FmtString, ...) \
+  TACTILE_LOG(tactile::LogLevel::kFatal, FmtString __VA_OPT__(, ) __VA_ARGS__)
+
+namespace tactile {
+
+TACTILE_FWD(class ILoggerSink)
+
+/** A simple logger implementation. */
+class TACTILE_CORE_API Logger final {
+ public:
+  /** Logs an arbitrary message. */
+  void log(LogLevel level, StringView fmt_string, fmt::format_args args) noexcept;
+
+  /** Sets the minimum severity of messages that get logged. */
+  void set_min_level(LogLevel level) noexcept;
+
+  /** Sets the minimum severity of messages that will initiate flushes. */
+  void flush_on(LogLevel level) noexcept;
+
+  /** Associates a sink implementation with the logger. */
+  void add_sink(ILoggerSink* sink);
+
+  /** Sets a reference time point to use as a relative baseline for timestamps. */
+  void set_reference_instant(Maybe<SteadyClockInstant> instant);
+
+  /** Indicates whether a message with the specified severity would be logged. */
+  [[nodiscard]] auto would_log(LogLevel level) const noexcept -> bool;
+
+  /** Indicates whether a message with the specified severity would trigger a flush. */
+  [[nodiscard]] auto would_flush(LogLevel level) const noexcept -> bool;
+
+  /** Returns a three-letter acronym for the specified log level. */
+  [[nodiscard]] static auto get_acronym(LogLevel level) noexcept -> StringView;
+
+ private:
+  LogLevel mMinLevel {LogLevel::kInfo};
+  LogLevel mFlushLevel {LogLevel::kError};
+  Vector<ILoggerSink*> mSinks;
+  Maybe<SteadyClockInstant> mReferenceInstant;
+
+  [[nodiscard]] auto _to_elapsed_time(SteadyClockInstant instant) const
+      -> chrono::microseconds;
+};
+
+/** Sets the logger instance that is used by the logging macros. */
+TACTILE_CORE_API void set_logger(Logger* logger) noexcept;
+
+/** Returns the logger instance used by logging macros. */
+[[nodiscard]] TACTILE_CORE_API auto get_logger() noexcept -> Logger*;
+
+}  // namespace tactile
---
+++ modules/core/src/tactile/core/debug/log/logger.cpp
@@ -0,0 +1,141 @@
+// Copyright (C) 2023 Albin Johansson (GNU General Public License v3.0)
+
+#include "tactile/core/debug/log/logger.hpp"
+
+#include <cstdio>     // fprintf, stderr
+#include <exception>  // exception
+#include <iterator>   // back_inserter
+
+#include <fmt/chrono.h>
+
+#include "tactile/core/debug/log/logger_sink.hpp"
+
+namespace tactile {
+namespace {
+
+constinit Logger* gLogger = nullptr;
+
+}  // namespace
+
+void Logger::log(const LogLevel level,
+                 const StringView fmt_string,
+                 const fmt::format_args args) noexcept
+{
+  try {
+    if (!mSinks.empty() && would_log(level)) {
+      const auto log_instant = SteadyClock::now();
+      const auto text = fmt::vformat(fmt_string, args);
+
+      const auto level_acronym = get_acronym(level);
+      const auto elapsed_time = _to_elapsed_time(log_instant);
+
+      fmt::memory_buffer prefix_buffer;
+      fmt::format_to(std::back_inserter(prefix_buffer),
+                     "[{} {:.>12%Q}]",
+                     level_acronym,
+                     elapsed_time);
+
+      const LogMessage msg {
+        .level = level,
+        .prefix = StringView {prefix_buffer.data(), prefix_buffer.size()},
+        .text = text,
+        .instant = log_instant,
+      };
+
+      const auto will_flush = would_flush(level);
+
+      for (auto* sink : mSinks) {
+        sink->log(msg);
+
+        if (will_flush) {
+          sink->flush();
+        }
+      }
+    }
+  }
+  catch (const std::exception& ex) {
+    std::fprintf(stderr, "### LOGGER ERROR: %s ###\n", ex.what());
+  }
+  catch (...) {
+    std::fprintf(stderr, "### UNKNOWN LOGGER ERROR ###\n");
+  }
+}
+
+void Logger::set_min_level(const LogLevel level) noexcept
+{
+  mMinLevel = level;
+}
+
+void Logger::flush_on(const LogLevel level) noexcept
+{
+  mFlushLevel = level;
+}
+
+void Logger::add_sink(ILoggerSink* sink)
+{
+  if (sink) {
+    mSinks.push_back(sink);
+  }
+}
+
+void Logger::set_reference_instant(const Maybe<SteadyClockInstant> instant)
+{
+  mReferenceInstant = instant;
+}
+
+auto Logger::would_log(const LogLevel level) const noexcept -> bool
+{
+  return level >= mMinLevel;
+}
+
+auto Logger::would_flush(const LogLevel level) const noexcept -> bool
+{
+  return level >= mFlushLevel;
+}
+
+void set_logger(Logger* logger) noexcept
+{
+  gLogger = logger;
+}
+
+auto get_logger() noexcept -> Logger*
+{
+  return gLogger;
+}
+
+auto Logger::get_acronym(const LogLevel level) noexcept -> StringView
+{
+  switch (level) {
+    case LogLevel::kTrace:
+      return "TRC";
+
+    case LogLevel::kDebug:
+      return "DBG";
+
+    case LogLevel::kInfo:
+      return "INF";
+
+    case LogLevel::kWarn:
+      return "WRN";
+
+    case LogLevel::kError:
+      return "ERR";
+
+    case LogLevel::kFatal:
+      return "FTL";
+  }
+
+  return "";
+}
+
+auto Logger::_to_elapsed_time(const SteadyClockInstant instant) const
+    -> chrono::microseconds
+{
+  if (mReferenceInstant.has_value()) {
+    return chrono::duration_cast<chrono::microseconds>(instant - *mReferenceInstant);
+  }
+
+  return chrono::duration_cast<chrono::microseconds>(instant.time_since_epoch());
+}
+
+}  // namespace tactile
