018948c11bee7367cc2ad624caee46c7f88aea24
Albin Johansson <albinoscar99@hotmail.com>
2023-09-16 17:46:48 +0200

    Update assert.hpp

--- modules/core/inc/tactile/core/debug/assert.hpp
+++ modules/core/inc/tactile/core/debug/assert.hpp
@@ -19,13 +19,23 @@
               : tactile::on_assertion_failed(#Expr, (Msg), __FILE__, __LINE__))
   #define TACTILE_ASSERT(Expr) TACTILE_ASSERT_MSG(Expr, "?")
 #else
-  #define TACTILE_ASSERT_MSG(Expr, Msg)
-  #define TACTILE_ASSERT(Expr)
+  #define TACTILE_ASSERT_MSG(Expr, Msg) (void) 0
+  #define TACTILE_ASSERT(Expr) (void) 0
 #endif
 
 namespace tactile {
 
-/** Prints an error message and terminates the program. */
+/**
+ * \brief Logs an error message and terminates the program.
+ *
+ * \details This function is called by the assert macros if an assertion fails, unless
+ *          TACTILE_ENABLE_ASSERTIONS is defined as 0.
+ *
+ * \param expr the assertion expression.
+ * \param  msg the assertion description message.
+ * \param file the file that contains the assertion.
+ * \param line the file line number of the assertion.
+ */
 [[noreturn]] TACTILE_CORE_API void on_assertion_failed(const char* expr,
                                                        const char* msg,
                                                        const char* file,
