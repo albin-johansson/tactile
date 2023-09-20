5ce6e17f53d54abb588a9cff3eed05eea56bdfa3
Albin Johansson <albinoscar99@hotmail.com>
2023-09-20 19:58:43 +0200

    Add Win32 virtual terminal processing function

--- modules/core/inc/tactile/core/platform/environment.hpp
+++ modules/core/inc/tactile/core/platform/environment.hpp
@@ -3,12 +3,27 @@
 #pragma once
 
 #include "tactile/core/api.hpp"
-#include "tactile/core/functional/maybe.hpp"
 #include "tactile/core/container/string.hpp"
+#include "tactile/core/functional/maybe.hpp"
+#include "tactile/core/prelude.hpp"
 
 namespace tactile {
 
 /** Returns the value of an environment variable. */
-[[nodiscard]] TACTILE_CORE_API auto get_env(const char* var) -> Maybe<String>;
+[[nodiscard]]
+TACTILE_CORE_API auto get_env(const char* var) -> Maybe<String>;
+
+/**
+ * \brief Enables virtual terminal sequences.
+ *
+ * \details This function can be used to enable the use of special ANSI color codes in
+ *          terminal output on Windows.
+ *
+ * \note This is a Windows specific API, and has no effect on other platforms.
+ *
+ * \see https://learn.microsoft.com/en-us/windows/console/console-virtual-terminal-sequences#example-of-enabling-virtual-terminal-processing
+ */
+TACTILE_CORE_API
+void win32_enable_virtual_terminal_processing();
 
 }  // namespace tactile
--- modules/core/src/tactile/core/platform/environment.cpp
+++ modules/core/src/tactile/core/platform/environment.cpp
@@ -4,7 +4,10 @@
 
 #include <cstdlib>  // getenv, _dupenv_s, free
 
-#include "tactile/core/prelude.hpp"
+#if TACTILE_OS_WINDOWS
+  #define WIN32_LEAN_AND_MEAN
+  #include <windows.h>
+#endif
 
 namespace tactile {
 
@@ -28,6 +31,28 @@ auto get_env(const char* var) -> Maybe<String>
   }
 
   return kNone;
+}
+
+void win32_enable_virtual_terminal_processing()
+{
+#if TACTILE_OS_WINDOWS
+  auto enable_virtual_terminal_processing_for = [](const DWORD std_handle_id) {
+    HANDLE handle = GetStdHandle(std_handle_id);
+    if (handle != INVALID_HANDLE_VALUE) {
+      DWORD modes = 0;
+      if (GetConsoleMode(handle, &modes)) {
+        if (!SetConsoleMode(handle,
+                            modes | ENABLE_VIRTUAL_TERMINAL_PROCESSING |
+                                DISABLE_NEWLINE_AUTO_RETURN)) {
+          SetConsoleMode(handle, modes | ENABLE_VIRTUAL_TERMINAL_PROCESSING);
+        }
+      }
+    }
+  };
+
+  enable_virtual_terminal_processing_for(STD_OUTPUT_HANDLE);
+  enable_virtual_terminal_processing_for(STD_ERROR_HANDLE);
+#endif
 }
 
 }  // namespace tactile
