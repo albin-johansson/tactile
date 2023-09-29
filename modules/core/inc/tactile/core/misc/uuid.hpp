c2a8a452db8d9392ff0a0b1cbbc9b20256884d4c
Albin Johansson <albinoscar99@hotmail.com>
2023-09-29 15:38:31 +0200

    Create uuid.hpp

---
+++ modules/core/inc/tactile/core/misc/uuid.hpp
@@ -0,0 +1,71 @@
+// Copyright (C) 2023 Albin Johansson (GNU General Public License v3.0)
+
+#pragma once
+
+#include "tactile/core/api.hpp"
+#include "tactile/core/container/array.hpp"
+#include "tactile/core/container/string.hpp"
+#include "tactile/core/prelude.hpp"
+
+namespace tactile {
+
+/**
+ * \brief Represents a universally unique identifier, or UUID.
+ *
+ * \details UUID instances are default initialized to zero. Create new UUID values
+ *          via the `generate` function.
+ *
+ *          \code{cpp}
+ *          const UUID uuid_a;
+ *          assert(uuid_a.is_null());
+ *
+ *          const auto uuid_b = UUID::generate();
+ *          assert(!uuid_b.is_null());
+ *          assert(uuid_a != uuid_b);
+ *          \endcode
+ */
+class UUID final {
+ public:
+  /**
+   * \brief Generates a new UUID.
+   *
+   * \return a new UUID.
+   */
+  [[nodiscard]]
+  TACTILE_CORE_API static auto generate() -> UUID;
+
+  /**
+   * \brief Produces a hash of the UUID.
+   *
+   * \return a hash value.
+   */
+  [[nodiscard]]
+  TACTILE_CORE_API auto hash_code() const noexcept -> usize;
+
+  /**
+   * \brief Indicates whether the UUID is null.
+   *
+   * \return true if the underlying array is all zeroes; false otherwise.
+   */
+  [[nodiscard]]
+  TACTILE_CORE_API auto is_null() const -> bool;
+
+  [[nodiscard]] auto operator==(const UUID&) const noexcept -> bool = default;
+
+ private:
+  Array<uint8, 16> mData {};
+
+  TACTILE_CORE_API friend auto to_string(const UUID& uuid) -> String;
+};
+
+/**
+ * \brief Returns a textual representation of a UUID.
+ *
+ * \param uuid the UUID value.
+ *
+ * \return a string with the format `HHHH-HH-HH-HH-HHHHHH`, using hexadecimal digits.
+ */
+[[nodiscard]]
+TACTILE_CORE_API auto to_string(const UUID& uuid) -> String;
+
+}  // namespace tactile
