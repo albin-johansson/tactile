b14c6372f069e720793eec443b7ddf6d38d89113
Albin Johansson <albinoscar99@hotmail.com>
2023-10-14 18:15:49 +0200

    Add TMJ object emitter

---
+++ modules/tmj-format/inc/tactile/tmj-format/emit/tmj_object_emitter.hpp
@@ -0,0 +1,33 @@
+// Copyright (C) 2023 Albin Johansson (GNU General Public License v3.0)
+
+#pragma once
+
+#include "tactile/core/container/vector.hpp"
+#include "tactile/core/io/ir.hpp"
+#include "tactile/core/prelude.hpp"
+#include "tactile/tmj-format/api.hpp"
+#include "tactile/tmj-format/common/json.hpp"
+
+namespace tactile::tmj {
+
+/**
+ * \brief Converts a map object to a TMJ JSON object.
+ *
+ * \param object the object to convert.
+ *
+ * \return a JSON object.
+ */
+[[nodiscard]]
+TACTILE_TMJ_API auto emit_object(const ir::Object& object) -> JSON;
+
+/**
+ * \brief Converts a group of map objects to an array of TMJ JSON objects.
+ *
+ * \param objects the objects to convert.
+ *
+ * \return a JSON array.
+ */
+[[nodiscard]]
+TACTILE_TMJ_API auto emit_object_array(const Vector<ir::Object>& objects) -> JSON;
+
+}  // namespace tactile::tmj
