9c2a3723f8869e323d59bb08f4491aa22990692f
Albin Johansson <albinoscar99@hotmail.com>
2023-10-29 01:24:10 +0200

    Add fallback if C++20 bit_cast is missing

--- modules/core/inc/tactile/core/platform/bits.hpp
+++ modules/core/inc/tactile/core/platform/bits.hpp
@@ -6,7 +6,9 @@
 #include <bit>          // bit_cast, endian, byteswap
 #include <concepts>     // integral, invocable
 #include <cstddef>      // byte
+#include <cstring>      // memcpy
 #include <type_traits>  // has_unique_object_representations_v
+#include <type_traits>  // is_trivially_copyable_v, is_trivially_constructible_v
 #include <version>      // __cpp_lib_byteswap
 
 #include "tactile/core/container/array.hpp"
@@ -29,6 +31,22 @@ void each_byte(const IntType value, const CallableType& callable)
   }
 }
 
+template <typename To, typename From>
+  requires(sizeof(To) == sizeof(From) &&          //
+           std::is_trivially_copyable_v<From> &&  //
+           std::is_trivially_copyable_v<To> &&    //
+           std::is_trivially_constructible_v<To>)
+[[nodiscard]] auto interpret_as(const From& src) noexcept -> To
+{
+#if __cpp_lib_bit_cast >= 201806L
+  return std::bit_cast<To>(src);
+#else
+  To dst;
+  std::memcpy(&dst, &src, sizeof dst);
+  return dst;
+#endif
+}
+
 /**
  * \brief Reverses the bytes in an integer value.
  *
@@ -46,10 +64,10 @@ template <std::integral IntType>
 #else
   using ByteArray = Array<std::byte, sizeof(IntType)>;
 
-  const auto bytes = std::bit_cast<ByteArray>(value);
+  const auto bytes = interpret_as<ByteArray>(value);
   std::reverse(bytes.begin(), bytes.end());
 
-  return std::bit_cast<IntType>(bytes);
+  return interpret_as<IntType>(bytes);
 #endif
 }
 
