c69ca84b6ae26407b1e04dd9ba17ddff73e03f7e
Albin Johansson <albinoscar99@hotmail.com>
2023-11-15 22:21:16 +0100

    Add alternative null optional constant

--- modules/core/inc/tactile/core/functional/maybe.hpp
+++ modules/core/inc/tactile/core/functional/maybe.hpp
@@ -10,5 +10,6 @@ template <typename T>
 using Maybe = std::optional<T>;
 
 inline constexpr std::nullopt_t kNone = std::nullopt;
+inline constexpr std::nullopt_t kNothing = std::nullopt;
 
 }  // namespace tactile
