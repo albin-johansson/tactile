05b80617199ddf50020a954427530e2c9c0a816e
Albin Johansson <albinoscar99@hotmail.com>
2023-11-18 15:06:37 +0100

    Add meta context visitor test utility

---
+++ modules/core/test/testutil/meta_context_visitor_tester.hpp
@@ -0,0 +1,67 @@
+// Copyright (C) 2023 Albin Johansson (GNU General Public License v3.0)
+
+#pragma once
+
+#include "tactile/core/context/meta_context_visitor.hpp"
+
+namespace tactile::testutil {
+
+class MetaContextVisitorTester final : public IMetaContextVisitor {
+ public:
+  int orthogonal_map_count {0};
+  int isometric_map_count {0};
+  int hexagonal_map_count {0};
+  int group_layer_count {0};
+  int tile_layer_count {0};
+  int object_layer_count {0};
+  int object_count {0};
+  int tileset_count {0};
+  int tile_count {0};
+
+  void visit([[maybe_unused]] OrthogonalMap& map) override
+  {
+    ++orthogonal_map_count;
+  }
+
+  void visit([[maybe_unused]] IsometricMap& map) override
+  {
+    ++isometric_map_count;
+  }
+
+  void visit([[maybe_unused]] HexagonalMap& map) override
+  {
+    ++hexagonal_map_count;
+  }
+
+  void visit([[maybe_unused]] GroupLayer& layer) override
+  {
+    ++group_layer_count;
+  }
+
+  void visit([[maybe_unused]] TileLayer& layer) override
+  {
+    ++tile_layer_count;
+  }
+
+  void visit([[maybe_unused]] ObjectLayer& layer) override
+  {
+    ++object_layer_count;
+  }
+
+  void visit([[maybe_unused]] Object& object) override
+  {
+    ++object_count;
+  }
+
+  void visit([[maybe_unused]] Tileset& tileset) override
+  {
+    ++tileset_count;
+  }
+
+  void visit([[maybe_unused]] Tile& tile) override
+  {
+    ++tile_count;
+  }
+};
+
+}  // namespace tactile::testutil
