d08c20106f16b5ad5e6b0920d895499667a8807d
Albin Johansson <albinoscar99@hotmail.com>
2023-11-18 16:31:45 +0100

    Add tool interface skeleton

---
+++ modules/core/inc/tactile/core/tool/mouse_event.hpp
@@ -0,0 +1,28 @@
+// Copyright (C) 2023 Albin Johansson (GNU General Public License v3.0)
+
+#pragma once
+
+#include "tactile/core/api.hpp"
+#include "tactile/core/math/vector.hpp"
+#include "tactile/core/prelude.hpp"
+
+namespace tactile {
+
+/**
+ * \brief Simply represents different mouse buttons.
+ */
+enum class MouseButton : int8 {
+  kLeft,
+  kMiddle,
+  kRight
+};
+
+/**
+ * \brief Provides information about a mouse event, used by tool implementations.
+ */
+struct TACTILE_CORE_API MouseEvent final {
+  MouseButton button;  ///< The active mouse button.
+  Float2 position;     ///< The current mouse position.
+};
+
+}  // namespace tactile
