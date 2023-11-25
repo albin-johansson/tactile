62c04fd226823e054a2e01c067f1a7b77ee133ab
Albin Johansson <albinoscar99@hotmail.com>
2023-11-25 13:51:06 +0100

    Create string_hash_test.cpp

---
+++ modules/core/test/container/string_hash_test.cpp
@@ -0,0 +1,21 @@
+// Copyright (C) 2023 Albin Johansson (GNU General Public License v3.0)
+
+#include <gtest/gtest.h>
+
+#include "tactile/core/container/string_map.hpp"
+
+using namespace tactile;
+
+using std::string_literals::operator""s;
+using std::string_view_literals::operator""sv;
+
+TEST(StringHash, HashDifferentStringRepresentations)
+{
+  const auto c_str_hash = StringHash {}("foobar");
+  const auto str_view_hash = StringHash {}("foobar"sv);
+  const auto str_hash = StringHash {}("foobar"s);
+
+  EXPECT_EQ(c_str_hash, str_hash);
+  EXPECT_EQ(c_str_hash, str_view_hash);
+  EXPECT_EQ(str_hash, str_view_hash);
+}
