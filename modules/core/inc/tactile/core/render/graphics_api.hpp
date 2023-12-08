dbac901da77d144f536ee69d88e03039fa672f64
Albin Johansson <albinoscar99@hotmail.com>
2023-12-08 23:10:09 +0100

    Create graphics_api.hpp

---
+++ modules/core/inc/tactile/core/render/graphics_api.hpp
@@ -0,0 +1,18 @@
+// Copyright (C) 2023 Albin Johansson (GNU General Public License v3.0)
+
+#pragma once
+
+#include "tactile/core/prelude.hpp"
+
+namespace tactile {
+
+/**
+ * \brief Represents the possible graphics APIs.
+ */
+enum class GraphicsAPI : int8 {
+  kUnknown,
+  kOpenGL,
+  kVulkan
+};
+
+}  // namespace tactile
