babc19cf5101fd3f144c7d86cdcccb5afa728ed8
Albin Johansson <albinoscar99@hotmail.com>
2023-12-18 22:56:13 +0100

    Add event dispatcher

---
+++ modules/core/inc/tactile/core/event/event_dispatcher.hpp
@@ -0,0 +1,11 @@
+// Copyright (C) 2023 Albin Johansson (GNU General Public License v3.0)
+
+#pragma once
+
+#include <entt/signal/dispatcher.hpp>
+
+namespace tactile {
+
+using EventDispatcher = entt::dispatcher;
+
+}  // namespace tactile
