5d4b47b420ed70bcf19c9820a8977674d72f6700
Albin Johansson <albinoscar99@hotmail.com>
2023-12-23 13:56:51 +0100

    Create language.hpp

---
+++ modules/core/inc/tactile/core/ui/i18n/language.hpp
@@ -0,0 +1,18 @@
+// Copyright (C) 2023 Albin Johansson (GNU General Public License v3.0)
+
+#pragma once
+
+#include "tactile/foundation/prelude.hpp"
+
+namespace tactile::ui {
+
+/**
+ * \brief Provides identifiers for the supported user interface languages.
+ */
+enum class LanguageID : int8 {
+  kAmericanEnglish,
+  kBritishEnglish,
+  kSwedish,
+};
+
+}  // namespace tactile::ui
