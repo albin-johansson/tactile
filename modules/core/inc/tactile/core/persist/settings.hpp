939b4eb9e0c0f71649cba0128854fcdc4e4afe03
Albin Johansson <albinoscar99@hotmail.com>
2023-12-23 14:58:55 +0100

    Tweak settings struct

--- modules/core/inc/tactile/core/persist/settings.hpp
+++ modules/core/inc/tactile/core/persist/settings.hpp
@@ -5,6 +5,7 @@
 #include "tactile/core/api.hpp"
 #include "tactile/core/ui/i18n/language.hpp"
 #include "tactile/foundation/container/string.hpp"
+#include "tactile/foundation/io/save/save_format_id.hpp"
 #include "tactile/foundation/math/vector.hpp"
 #include "tactile/foundation/meta/color.hpp"
 #include "tactile/foundation/prelude.hpp"
@@ -15,29 +16,30 @@ namespace tactile {
  * \brief Represents the persistent editor settings.
  */
 struct Settings final {
-  ui::LanguageID language;
-  String preferred_save_format_extension;
-  usize command_capacity;
-  int32 font_size;
-  int32 theme_saturation;
-  Int2 preferred_tile_size;
-  UColor viewport_background_color;
-  UColor grid_line_color;
+  ui::LanguageID language {ui::LanguageID::kAmericanEnglish};
+  SaveFormatId preferred_save_format {SaveFormatId::kTactileYaml};
+  usize command_capacity {100};
+  int32 font_size {13};
+  int32 theme_saturation {60};
+  Int2 preferred_tile_size {32, 32};
+  UColor viewport_background_color {0x3C, 0x3C, 0x3C, 0xFF};
+  UColor grid_line_color {0xFF, 0xFF, 0xFF, 0x05};
+  bool use_default_font : 1 {true};
+  bool embed_tilesets : 1 {true};
+  bool indent_output : 1 {true};
+  bool fold_tile_data : 1 {true};
+  bool show_active_layer_highlight : 1 {false};
+  bool show_layer_dock : 1 {true};
+  bool show_tileset_dock : 1 {true};
+  bool show_property_dock : 1 {true};
+  bool show_component_dock : 1 {true};
+  bool show_animation_dock : 1 {true};
+  bool show_log_dock : 1 {false};
+  bool show_viewport_grid : 1 {true};
+  bool show_viewport_overlay_fps : 1 {true};
+  bool show_window_borders : 1 {false};
+  bool restore_layout_at_startup : 1 {false};
+  bool restore_session_at_startup : 1 {true};
 };
-
-/**
- * \brief Creates a setting bundle with the default configuration.
- *
- * \return a settings bundle.
- */
-[[nodiscard]]
-TACTILE_CORE_API auto default_settings() -> Settings;
-
-/**
- * \brief Resets a setting bundle.
- *
- * \param settings the target settings.
- */
-TACTILE_CORE_API void reset_settings(Settings& settings);
 
 }  // namespace tactile
