d5050d94596a499f8449ab6a0076638775a0628a
Albin Johansson <albinoscar99@hotmail.com>
2023-12-23 14:21:37 +0100

    Add tile format struct

---
+++ modules/core/inc/tactile/core/map/tile_format.hpp
@@ -0,0 +1,21 @@
+// Copyright (C) 2023 Albin Johansson (GNU General Public License v3.0)
+
+#pragma once
+
+#include "tactile/foundation/functional/maybe.hpp"
+#include "tactile/foundation/io/compression/compression_kind.hpp"
+#include "tactile/foundation/misc/tile_encoding.hpp"
+#include "tactile/foundation/prelude.hpp"
+
+namespace tactile {
+
+/**
+ * \brief Describes the data format used by tile layers in a tilemap.
+ */
+struct TileFormat final {
+  TileEncoding encoding {TileEncoding::kPlainText};
+  CompressionMode compression {CompressionMode::kNone};
+  Maybe<int> compression_level {};
+};
+
+}  // namespace tactile
---
+++ modules/core/test/map/tile_format_test.cpp
@@ -0,0 +1,15 @@
+// Copyright (C) 2023 Albin Johansson (GNU General Public License v3.0)
+
+#include "tactile/core/map/tile_format.hpp"
+
+#include <gtest/gtest.h>
+
+using namespace tactile;
+
+TEST(TileFormat, Defaults)
+{
+  const TileFormat tile_format {};
+  EXPECT_EQ(tile_format.encoding, TileEncoding::kPlainText);
+  EXPECT_EQ(tile_format.compression, CompressionMode::kNone);
+  EXPECT_FALSE(tile_format.compression_level.has_value());
+}
