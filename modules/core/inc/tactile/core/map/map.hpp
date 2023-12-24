b1ed9dece0bda0b196e21dc44b598c58b2bb7740
Albin Johansson <albinoscar99@hotmail.com>
2023-12-24 15:27:40 +0100

    Tweak map interface

--- modules/core/inc/tactile/core/map/map.hpp
+++ modules/core/inc/tactile/core/map/map.hpp
@@ -8,8 +8,9 @@
 
 namespace tactile {
 
+struct MatrixExtent;
+struct TileFormat;
 class GroupLayer;
-class TileFormat;
 
 /**
  * \interface IMap
@@ -20,18 +21,46 @@ class IMap : public IMetaContext {
   TACTILE_INTERFACE_CLASS(IMap);
 
   /**
+   * \brief Updates the size of the map (and all associated tile layers).
+   *
+   * \param extent the new map extent.
+   */
+  virtual void set_extent(const MatrixExtent& extent) = 0;
+
+  /**
+   * \brief Returns the size of the map.
+   *
+   * \return a map extent.
+   */
+  [[nodiscard]]
+  virtual auto extent() const -> const MatrixExtent& = 0;
+
+  /**
+   * \brief Sets the logical size of tiles in the map.
+   *
+   * \param size the new logical tile size.
+   */
+  virtual void set_tile_size(Int2 size) = 0;
+
+  /**
+   * \brief Returns the logical size of all tiles.
+   *
+   * \return a tile size.
+   */
+  [[nodiscard]]
+  virtual auto tile_size() const -> Int2 = 0;
+
+  /**
    * \brief Returns the invisible root layer.
    *
    * \return the root group layer.
    */
   [[nodiscard]]
-  virtual auto get_root_layer() -> GroupLayer& = 0;
+  virtual auto root_layer() -> GroupLayer& = 0;
 
-  /**
-   * \copydoc get_root_layer()
-   */
+  /** \copydoc root_layer() */
   [[nodiscard]]
-  virtual auto get_root_layer() const -> const GroupLayer& = 0;
+  virtual auto root_layer() const -> const GroupLayer& = 0;
 
   /**
    * \brief Returns the associated tile format configuration.
@@ -39,13 +68,11 @@ class IMap : public IMetaContext {
    * \return a tile format configuration.
    */
   [[nodiscard]]
-  virtual auto get_tile_format() -> TileFormat& = 0;
+  virtual auto tile_format() -> TileFormat& = 0;
 
-  /**
-   * \copydoc get_tile_format()
-   */
+  /** \copydoc tile_format() */
   [[nodiscard]]
-  virtual auto get_tile_format() const -> const TileFormat& = 0;
+  virtual auto tile_format() const -> const TileFormat& = 0;
 };
 
 }  // namespace tactile
