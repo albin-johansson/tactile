0c25163873c8602a723c6452828cb892a67af658
Albin Johansson <albinoscar99@hotmail.com>
2024-01-13 18:08:56 +0100

    Add command for moving layers downwards

---
+++ modules/core/src/tactile/core/command/layer/move_layer_down_command.cpp
@@ -0,0 +1,39 @@
+// Copyright (C) 2023 Albin Johansson (GNU General Public License v3.0)
+
+#include "tactile/core/command/layer/move_layer_down_command.hpp"
+
+#include <fmt/ostream.h>
+
+#include "tactile/core/document/map_document.hpp"
+#include "tactile/core/layer/group_layer.hpp"
+#include "tactile/foundation/debug/validation.hpp"
+#include "tactile/foundation/log/logger.hpp"
+
+namespace tactile {
+
+MoveLayerDownCommand::MoveLayerDownCommand(MapDocument* document, const UUID& layer_uuid)
+  : mDocument {require_not_null(document, "null document")},
+    mLayerUuid {layer_uuid}
+{}
+
+void MoveLayerDownCommand::undo()
+{
+  TACTILE_LOG_TRACE("[MoveLayerDownCommand::undo] layer: {}", fmt::streamed(mLayerUuid));
+
+  auto& map = mDocument->map();
+  auto& root_layer = map.root_layer();
+
+  root_layer.move_layer_up(mLayerUuid);
+}
+
+void MoveLayerDownCommand::redo()
+{
+  TACTILE_LOG_TRACE("[MoveLayerDownCommand::redo] layer: {}", fmt::streamed(mLayerUuid));
+
+  auto& map = mDocument->map();
+  auto& root_layer = map.root_layer();
+
+  root_layer.move_layer_down(mLayerUuid);
+}
+
+}  // namespace tactile
---
+++ modules/core/test/command/layer/move_layer_down_command_test.cpp
@@ -0,0 +1,67 @@
+// Copyright (C) 2023 Albin Johansson (GNU General Public License v3.0)
+
+#include "tactile/core/command/layer/move_layer_down_command.hpp"
+
+#include <gtest/gtest.h>
+
+#include "tactile/core/document/map_document.hpp"
+#include "tactile/core/layer/group_layer.hpp"
+#include "tactile/core/layer/tile_layer.hpp"
+#include "tactile/core/map/orthogonal_map.hpp"
+
+using namespace tactile;
+
+class MoveLayerDownCommandTest : public testing::Test {
+ protected:
+  MoveLayerDownCommandTest()
+    : mDocument {make_unique<OrthogonalMap>(MatrixExtent {7, 7}, Int2 {32, 32})},
+      mGroupLayer {make_shared<GroupLayer>()},
+      mLayer1 {make_shared<TileLayer>(mDocument.map().extent())},
+      mLayer2 {make_shared<TileLayer>(mDocument.map().extent())},
+      mLayer3 {make_shared<TileLayer>(mDocument.map().extent())}
+  {
+    auto& root_layer = mDocument.map().root_layer();
+    root_layer.append_layer(mGroupLayer);
+
+    const auto group_layer_uuid = mGroupLayer->meta().uuid();
+    root_layer.append_layer_to(group_layer_uuid, mLayer1);
+    root_layer.append_layer_to(group_layer_uuid, mLayer2);
+    root_layer.append_layer_to(group_layer_uuid, mLayer3);
+  }
+
+  MapDocument mDocument;
+  Shared<GroupLayer> mGroupLayer;
+  Shared<TileLayer> mLayer1;
+  Shared<TileLayer> mLayer2;
+  Shared<TileLayer> mLayer3;
+};
+
+/// \trace tactile::MoveLayerDownCommand::redo
+/// \trace tactile::MoveLayerDownCommand::undo
+TEST_F(MoveLayerDownCommandTest, RedoUndo)
+{
+  auto& map = mDocument.map();
+  auto& root_layer = map.root_layer();
+
+  const auto layer1_uuid = mLayer1->meta().uuid();
+  const auto layer2_uuid = mLayer2->meta().uuid();
+  const auto layer3_uuid = mLayer3->meta().uuid();
+
+  EXPECT_EQ(root_layer.get_layer_local_index(layer1_uuid), 0);
+  EXPECT_EQ(root_layer.get_layer_local_index(layer2_uuid), 1);
+  EXPECT_EQ(root_layer.get_layer_local_index(layer3_uuid), 2);
+
+  MoveLayerDownCommand command {&mDocument, layer2_uuid};
+
+  command.redo();
+
+  EXPECT_EQ(root_layer.get_layer_local_index(layer1_uuid), 0);
+  EXPECT_EQ(root_layer.get_layer_local_index(layer3_uuid), 1);
+  EXPECT_EQ(root_layer.get_layer_local_index(layer2_uuid), 2);
+
+  command.undo();
+
+  EXPECT_EQ(root_layer.get_layer_local_index(layer1_uuid), 0);
+  EXPECT_EQ(root_layer.get_layer_local_index(layer2_uuid), 1);
+  EXPECT_EQ(root_layer.get_layer_local_index(layer3_uuid), 2);
+}
