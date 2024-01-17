980160bda231282abb528409b22d2bed73805041
Albin Johansson <albinoscar99@hotmail.com>
2024-01-17 17:54:54 +0100

    Add map orientation enum

---
+++ modules/core/inc/tactile/core/map/map_orientation.hpp
@@ -0,0 +1,18 @@
+// Copyright (C) 2023 Albin Johansson (GNU General Public License v3.0)
+
+#pragma once
+
+#include "tactile/foundation/prelude.hpp"
+
+namespace tactile {
+
+/**
+ * Represents different map presentation styles.
+ */
+enum class MapOrientation : int8 {
+  kOrthogonal,
+  kIsometric,
+  kHexagonal
+};
+
+}  // namespace tactile
