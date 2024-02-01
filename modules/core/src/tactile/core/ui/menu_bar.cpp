bc77b6f8fff5901d68a389d4fd1a65c5d8e44125
Albin Johansson <albinoscar99@hotmail.com>
2024-02-01 20:31:07 +0100

    Add menu bar class

---
+++ modules/core/src/tactile/core/ui/menu_bar.cpp
@@ -0,0 +1,16 @@
+// Copyright (C) 2023 Albin Johansson (GNU General Public License v3.0)
+
+#include "tactile/core/ui/menu_bar.hpp"
+
+#include <imgui.h>
+
+namespace tactile {
+
+void MenuBar::update(const Model& model, EventDispatcher& dispatcher)
+{
+  if (ImGui::BeginMainMenuBar()) {
+    ImGui::EndMainMenuBar();
+  }
+}
+
+}  // namespace tactile
