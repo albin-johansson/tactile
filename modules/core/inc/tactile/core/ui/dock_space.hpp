9fd478a754773072f45dd2061ece1702d1f79db6
Albin Johansson <albinoscar99@hotmail.com>
2024-02-01 20:27:25 +0100

    Add dock space class

---
+++ modules/core/inc/tactile/core/ui/dock_space.hpp
@@ -0,0 +1,38 @@
+// Copyright (C) 2023 Albin Johansson (GNU General Public License v3.0)
+
+#pragma once
+
+#include "tactile/core/api.hpp"
+#include "tactile/core/ui/widget.hpp"
+#include "tactile/foundation/functional/maybe.hpp"
+#include "tactile/foundation/prelude.hpp"
+
+namespace tactile {
+
+/**
+ * Manages the Dear ImGui dock space for the dock widgets.
+ */
+class TACTILE_CORE_API DockSpace final {
+ public:
+  /**
+   * Updates the dock space.
+   *
+   * \note
+   *    This function should be called once at the start of each frame, before
+   *    any other widgets have been submitted.
+   *
+   * \param model
+   * \param dispatcher
+   */
+  void update(const Model& model, EventDispatcher& dispatcher);
+
+ private:
+  Maybe<uint> mRootId;
+  bool mDidInit {false};
+
+  void _load_default_layout();
+};
+
+static_assert(Widget<DockSpace>);
+
+}  // namespace tactile
---
+++ modules/core/src/tactile/core/ui/dock_space.cpp
@@ -0,0 +1,33 @@
+// Copyright (C) 2023 Albin Johansson (GNU General Public License v3.0)
+
+#include "tactile/core/ui/dock_space.hpp"
+
+#include <imgui.h>
+#include <imgui_internal.h>
+
+#include "tactile/core/model/model.hpp"
+#include "tactile/foundation/debug/validation.hpp"
+#include "tactile/foundation/log/logger.hpp"
+
+namespace tactile {
+
+void DockSpace::update(const Model& model, EventDispatcher&)
+{
+  const auto* viewport = require_not_null(ImGui::GetMainViewport(), "null main viewport");
+  mRootId = ImGui::DockSpaceOverViewport(viewport);
+
+  if (!mDidInit && viewport->Size.x > 0 && viewport->Size.y > 0) {
+    TACTILE_LOG_DEBUG("[DockSpace] Initializing dock space");
+
+    // TODO
+
+    mDidInit = true;
+  }
+}
+
+void DockSpace::_load_default_layout()
+{
+  TACTILE_LOG_DEBUG("[DockSpace] Loading default layout");
+}
+
+}  // namespace tactile
