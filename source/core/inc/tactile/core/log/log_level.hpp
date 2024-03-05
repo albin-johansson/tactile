271b8282f232e365f154e02185298caeea4dd36e
Albin Johansson <albinoscar99@hotmail.com>
2024-03-05 20:09:49 +0100

    Create log_level.hpp

---
+++ source/core/inc/tactile/core/log/log_level.hpp
@@ -0,0 +1,24 @@
+// Copyright (C) 2023 Albin Johansson (GNU General Public License v3.0)
+
+#pragma once
+
+#include "tactile/base/int.hpp"
+
+namespace tactile {
+
+/**
+ * The supported log level categories.
+ *
+ * \details
+ *    The underlying value increases with severity.
+ */
+enum class LogLevel : uint8 {
+  kTrace,  ///< For verbose messages only useful for detailed debugging.
+  kDebug,  ///< For messages that provide useful developer information.
+  kInfo,   ///< For messages that provide general high-level information.
+  kWarn,   ///< For messages that warn about potential issues.
+  kError,  ///< For messages that report non-fatal errors.
+  kFatal,  ///< For reporting errors from which the application cannot recover.
+};
+
+}  // namespace tactile
