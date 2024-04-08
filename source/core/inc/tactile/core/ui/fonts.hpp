a396bd9610e048dc0c6ff48ad1e001114bdc237c
Albin Johansson <albinoscar99@hotmail.com>
2024-04-08 20:10:25 +0200

    Add font reloading utility

---
+++ source/core/inc/tactile/core/ui/fonts.hpp
@@ -0,0 +1,23 @@
+// Copyright (C) 2024 Albin Johansson (GNU General Public License v3.0)
+
+#pragma once
+
+#include "tactile/base/prelude.hpp"
+
+namespace tactile {
+
+class IRenderer;
+
+/**
+ * Attempts to reload the font texture atlas.
+ *
+ * \note
+ *    This function does nothing if the renderer doesn't support font reloading.
+ *
+ * \param renderer          The active renderer.
+ * \param font_size         The font size to use.
+ * \param framebuffer_scale The display framebuffer scale.
+ */
+void reload_fonts(IRenderer& renderer, float font_size, float framebuffer_scale);
+
+}  // namespace tactile
---
+++ source/core/src/tactile/core/ui/fonts.cpp
@@ -0,0 +1,61 @@
+// Copyright (C) 2024 Albin Johansson (GNU General Public License v3.0)
+
+#include "tactile/core/ui/fonts.hpp"
+
+#include <IconsFontAwesome6.h>
+#include <imgui.h>
+
+#include "tactile/base/container/array.hpp"
+#include "tactile/core/log/logger.hpp"
+#include "tactile/render/renderer.hpp"
+
+namespace tactile {
+namespace {
+
+inline constexpr Array<ImWchar, 3> kFontIconRange {ICON_MIN_FA, ICON_MAX_FA, 0};
+
+}  // namespace
+
+void reload_fonts(IRenderer& renderer,
+                  const float font_size,
+                  const float framebuffer_scale)
+{
+  if (renderer.can_reload_fonts()) {
+    TACTILE_LOG_DEBUG("Reloading fonts (size: {})", font_size);
+
+    auto& io = ImGui::GetIO();
+    io.Fonts->Clear();
+
+    const auto scaled_font_size = font_size * framebuffer_scale;
+
+    ImFontConfig roboto_config {};
+    roboto_config.SizePixels = scaled_font_size;
+    io.Fonts->AddFontFromFileTTF("assets/fonts/roboto/Roboto-Regular.ttf",
+                                 scaled_font_size,
+                                 &roboto_config);
+
+    ImFontConfig fa_config {};
+    fa_config.MergeMode = true;
+    fa_config.SizePixels = scaled_font_size;
+    fa_config.GlyphMinAdvanceX = scaled_font_size;
+    fa_config.GlyphMaxAdvanceX = scaled_font_size;
+    fa_config.GlyphOffset = {0, 2};
+    io.Fonts->AddFontFromFileTTF("assets/fonts/fa/fa-solid-900.ttf",
+                                 scaled_font_size,
+                                 &fa_config,
+                                 kFontIconRange.data());
+
+    // The global scale is 1 on most platforms, but 0.5 on macOS.
+    io.FontGlobalScale = 1.0f / framebuffer_scale;
+    io.Fonts->Build();
+
+    renderer.try_reload_fonts();
+
+    ImGui::GetStyle().ScaleAllSizes(1.0f);
+  }
+  else {
+    TACTILE_LOG_WARN("Tried to reload fonts, but the renderer didn't support it");
+  }
+}
+
+}  // namespace tactile
