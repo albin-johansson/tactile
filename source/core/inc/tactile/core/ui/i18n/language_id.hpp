6b432dd4ede70ab28e29e5165f7cf9a2e3ac1549
Albin Johansson <albinoscar99@hotmail.com>
2024-04-24 00:10:01 +0200

    Create language_id.hpp

---
+++ source/core/inc/tactile/core/ui/i18n/language_id.hpp
@@ -0,0 +1,25 @@
+// Copyright (C) 2024 Albin Johansson (GNU General Public License v3.0)
+
+#pragma once
+
+#include "tactile/base/int.hpp"
+#include "tactile/base/prelude.hpp"
+
+namespace tactile {
+
+/// \addtogroup I18N
+/// \{
+
+/**
+ * Provides identifiers for the supported languages.
+ */
+enum class LanguageID : uint8
+{
+  kAmericanEnglish,
+  kBritishEnglish,
+  kSwedish,
+};
+
+/// \}
+
+}  // namespace tactile
