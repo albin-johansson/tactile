04c7a975cc829f145eea99ed1d3e51f2e37407f2
Albin Johansson <albinoscar99@hotmail.com>
2024-05-10 13:45:41 +0200

    Add popup RAII utility

---
+++ source/core/inc/tactile/core/ui/common/popups.hpp
@@ -0,0 +1,75 @@
+// Copyright (C) 2024 Albin Johansson (GNU General Public License v3.0)
+
+#pragma once
+
+#include <imgui.h>
+
+#include "tactile/base/prelude.hpp"
+
+namespace tactile::ui {
+
+/// \addtogroup UI
+/// \{
+
+/**
+ * Tag type used to create modal popups.
+ */
+struct ModalPopupTag final
+{};
+
+/**
+ * Used to create modal popups.
+ */
+inline constexpr ModalPopupTag kModalPopup {};
+
+/**
+ * RAII helper for the BeginPopup{Modal}/EndPopup ImGui functions.
+ */
+class PopupScope final
+{
+ public:
+  TACTILE_DELETE_COPY(PopupScope);
+  TACTILE_DELETE_MOVE(PopupScope);
+
+  /**
+   * Pushes a popup to the widget stack.
+   *
+   * \param id    The popup identifier.
+   * \param flags The window flags to use.
+   */
+  [[nodiscard]]
+  explicit PopupScope(const char* id,
+                      ImGuiWindowFlags flags = ImGuiWindowFlags_None);
+
+  /**
+   * Pushes a modal popup to the widget stack.
+   *
+   * \param id    The popup identifier.
+   * \param flags The window flags to use.
+   */
+  [[nodiscard]]
+  PopupScope(ModalPopupTag,
+             const char* id,
+             ImGuiWindowFlags flags = ImGuiWindowFlags_None);
+
+  /**
+   * Pops the popup from the widget stack.
+   */
+  ~PopupScope() noexcept;
+
+  /**
+   * Indicates whether the popup is open.
+   *
+   * \return
+   * True if the popup is open; false otherwise.
+   */
+  [[nodiscard]]
+  auto is_open() const -> bool;
+
+ private:
+  bool mIsOpen {};
+};
+
+/// \}
+
+}  // namespace tactile::ui
---
+++ source/core/src/tactile/core/ui/common/popups.cpp
@@ -0,0 +1,29 @@
+// Copyright (C) 2024 Albin Johansson (GNU General Public License v3.0)
+
+#include "tactile/core/ui/common/popups.hpp"
+
+namespace tactile::ui {
+
+PopupScope::PopupScope(const char* id, const ImGuiWindowFlags flags)
+  : mIsOpen {ImGui::BeginPopup(id, flags)}
+{}
+
+PopupScope::PopupScope(ModalPopupTag,
+                       const char* id,
+                       const ImGuiWindowFlags flags)
+  : mIsOpen {ImGui::BeginPopupModal(id, nullptr, flags)}
+{}
+
+PopupScope::~PopupScope() noexcept
+{
+  if (mIsOpen) {
+    ImGui::EndPopup();
+  }
+}
+
+auto PopupScope::is_open() const -> bool
+{
+  return mIsOpen;
+}
+
+}  // namespace tactile::ui
