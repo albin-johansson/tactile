83cf0fdcdb660b2c1c57b1eaf71ef3f9e48a7571
Albin Johansson <albinoscar99@hotmail.com>
2024-05-11 16:09:29 +0200

    Add dynamic library utility

---
+++ source/core/inc/tactile/core/platform/dynamic_library.hpp
@@ -0,0 +1,93 @@
+// Copyright (C) 2024 Albin Johansson (GNU General Public License v3.0)
+
+#pragma once
+
+#include "tactile/base/container/path.hpp"
+#include "tactile/base/container/smart_ptr.hpp"
+#include "tactile/base/prelude.hpp"
+
+#if TACTILE_OS_WINDOWS
+  #define TACTILE_DLL_EXT ".dll"
+#elif TACTILE_OS_APPLE
+  #define TACTILE_DLL_EXT ".dylib"
+#else
+  #define TACTILE_DLL_EXT ".so"
+#endif
+
+namespace tactile {
+
+/// \addtogroup Platform
+/// \{
+
+/**
+ * Interface for dynamically loaded libraries.
+ *
+ * \details
+ * Dynamic library files typically use file extensions such as \c .so on
+ * Linux, \c .so or \c .dylib on macOS, and \c .dll on Windows. This interface
+ * can be used to obtain pointers to symbols with C linkage in such files at
+ * runtime.
+ *
+ * \see load_library(const Path&)
+ */
+class IDynamicLibrary
+{
+ public:
+  TACTILE_INTERFACE_CLASS(IDynamicLibrary);
+
+  /**
+   * Attempts to find a pointer to a symbol in the library.
+   *
+   * \details
+   * This function should only be used to obtain pointers to entities with C
+   * linkage to avoid running into C++ ABI issues. The actual functions may of
+   * course be implemented in C++.
+   *
+   * \param name The symbol name.
+   *
+   * \return
+   * A pointer to the symbol if successful; a null pointer otherwise.
+   */
+  [[nodiscard]]
+  virtual auto find_symbol(const char* name) const -> void* = 0;
+};
+
+/**
+ * Attempts to load a dynamic library.
+ *
+ * \details
+ * This function makes use of the `dlopen` and `LoadModuleA` APIs on Linux/macOS
+ * and Windows, respectively. For other platforms, this function simply returns
+ * a null pointer.
+ *
+ * \param path The file path to the dynamic library.
+ *
+ * \return
+ * The loaded library if successful; a null pointer otherwise.
+ */
+[[nodiscard]]
+auto load_library(const Path& path) -> Unique<IDynamicLibrary>;
+
+/**
+ * Attempts to find a symbol in a dynamic library.
+ *
+ * \pre The actual symbol type must match the provided type.
+ *
+ * \tparam T The type of the symbol.
+ *
+ * \param lib  The associated dynamic library.
+ * \param name The name of the symbol to load.
+ *
+ * \return
+ * A pointer to the symbol if successful; a null pointer otherwise.
+ */
+template <typename T>
+[[nodiscard]] auto find_symbol(const IDynamicLibrary& lib,
+                               const char* name) -> T*
+{
+  return reinterpret_cast<T*>(lib.find_symbol(name));
+}
+
+/// \}
+
+}  // namespace tactile
---
+++ source/core/src/tactile/core/platform/dynamic_library.cpp
@@ -0,0 +1,121 @@
+// Copyright (C) 2024 Albin Johansson (GNU General Public License v3.0)
+
+#include "tactile/core/platform/dynamic_library.hpp"
+
+#if TACTILE_OS_LINUX || TACTILE_OS_APPLE
+  #include <dlfcn.h>
+#endif
+
+#if TACTILE_OS_WINDOWS
+  #include <windows.h>
+#endif
+
+#include "tactile/core/log/logger.hpp"
+
+namespace tactile {
+inline namespace dynamic_library {
+
+#if TACTILE_OS_LINUX || TACTILE_OS_APPLE
+
+class UnixDynamicLibrary final : public IDynamicLibrary
+{
+ public:
+  TACTILE_DELETE_COPY(UnixDynamicLibrary);
+  TACTILE_DELETE_MOVE(UnixDynamicLibrary);
+
+  explicit UnixDynamicLibrary(void* handle) noexcept
+    : mHandle {handle}
+  {}
+
+  ~UnixDynamicLibrary() noexcept override
+  {
+    if (mHandle) {
+      dlclose(mHandle);
+      mHandle = nullptr;
+    }
+  }
+
+  [[nodiscard]]
+  static auto load(const Path& path) -> Unique<UnixDynamicLibrary>
+  {
+    void* handle = dlopen(path.c_str(), RTLD_LAZY);
+
+    if (!handle) {
+      TACTILE_LOG_ERROR("Could not load dynamic library: {}", dlerror());
+      return nullptr;
+    }
+
+    return std::make_unique<UnixDynamicLibrary>(handle);
+  }
+
+  [[nodiscard]]
+  auto find_symbol(const char* name) const -> void* override
+  {
+    return dlsym(mHandle, name);
+  }
+
+ private:
+  void* mHandle;
+};
+
+#endif  // TACTILE_OS_LINUX || TACTILE_OS_APPLE
+
+#if TACTILE_OS_WINDOWS
+
+class Win32DynamicLibrary final : public IDynamicLibrary
+{
+ public:
+  TACTILE_DELETE_COPY(Win32DynamicLibrary);
+  TACTILE_DELETE_MOVE(Win32DynamicLibrary);
+
+  explicit Win32DynamicLibrary(HMODULE handle)
+    : mHandle {handle}
+  {}
+
+  ~Win32DynamicLibrary() noexcept override
+  {
+    if (mHandle) {
+      FreeLibrary(mHandle);
+      mHandle = nullptr;
+    }
+  }
+
+  [[nodiscard]]
+  static auto load(const Path& path) -> Unique<UnixDynamicLibrary>
+  {
+    const auto handle = LoadLibraryA(path.string().c_str());
+
+    if (!handle) {
+      TACTILE_LOG_ERROR("Could not load dynamic library: {}", GetLastError());
+      return nullptr;
+    }
+
+    return std::make_unique<Win32DynamicLibrary>(handle);
+  }
+
+  [[nodiscard]]
+  auto find_symbol(const char* name) const -> void* override
+  {
+    return static_cast<void*>(GetProcAddress(mHandle, name));
+  }
+
+ private:
+  HMODULE mHandle;
+};
+
+#endif  // TACTILE_OS_WINDOWS
+
+}  // namespace dynamic_library
+
+auto load_library(const Path& path) -> Unique<IDynamicLibrary>
+{
+#if TACTILE_OS_LINUX || TACTILE_OS_APPLE
+  return UnixDynamicLibrary::load(path);
+#elif TACTILE_OS_WINDOWS
+  return Win32DynamicLibrary::load(path);
+#else
+  return nullptr;
+#endif  // TACTILE_OS_WINDOWS
+}
+
+}  // namespace tactile
