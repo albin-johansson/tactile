48a52960473d0017db1b400d15593c9f54ad8d05
Albin Johansson <albinoscar99@hotmail.com>
2024-05-18 14:05:14 +0200

    Use std::expected

--- source/base/inc/tactile/base/container/expected.hpp
+++ source/base/inc/tactile/base/container/expected.hpp
@@ -2,28 +2,14 @@
 
 #pragma once
 
+#include <expected>      // expected, unexpected
 #include <system_error>  // error_code, error_category
 #include <type_traits>   // decay_t
 #include <utility>       // forward
-#include <version>       // __cpp_lib_expected
-
-#if __cpp_lib_expected >= 202211L
-  #define TACTILE_HAS_STD_EXPECTED 1
-#else
-  #define TACTILE_HAS_STD_EXPECTED 0
-#endif
-
-#if TACTILE_HAS_STD_EXPECTED
-  #include <expected>  // expected, unexpected
-#else
-  #include <tl/expected.hpp>
-#endif
 
 #include "tactile/base/int.hpp"
 
 namespace tactile {
-
-#if TACTILE_HAS_STD_EXPECTED
 
 template <typename T, typename E>
 using Expected = std::expected<T, E>;
@@ -31,28 +17,15 @@ using Expected = std::expected<T, E>;
 template <typename E>
 using Unexpected = std::unexpected<E>;
 
-#else
-
-template <typename T, typename E>
-using Expected = tl::expected<T, E>;
-
-template <typename E>
-using Unexpected = tl::unexpected<E>;
-
-#endif
-
 template <typename E>
 [[nodiscard]] auto unexpected(E&& e) -> Unexpected<std::decay_t<E>>
 {
-#if TACTILE_HAS_STD_EXPECTED
   return std::unexpected {std::forward<E>(e)};
-#else
-  return tl::make_unexpected(std::forward<E>(e));
-#endif
 }
 
 template <typename T, typename E>
-[[nodiscard]] auto propagate_unexpected(const Expected<T, E>& e) -> Unexpected<E>
+[[nodiscard]] auto propagate_unexpected(const Expected<T, E>& e)
+    -> Unexpected<E>
 {
   return unexpected(e.error());
 }
