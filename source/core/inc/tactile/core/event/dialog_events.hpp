196259b59e50379867752e10e9ed677780739b65
Albin Johansson <albinoscar99@hotmail.com>
2024-06-11 06:34:15 +0200

    Add layer events

--- source/core/inc/tactile/core/event/dialog_events.hpp
+++ source/core/inc/tactile/core/event/dialog_events.hpp
@@ -3,23 +3,30 @@
 #pragma once
 
 #include "tactile/base/prelude.hpp"
+#include "tactile/core/entity/entity.hpp"
 
 namespace tactile {
 
 /**
  * Event for opening the map document creation dialog.
+ *
+ * \ingroup Event
  */
 struct ShowNewMapDialogEvent final
 {};
 
 /**
  * Event for opening the map document loading dialog.
+ *
+ * \ingroup Event
  */
 struct ShowOpenMapDialogEvent final
 {};
 
 /**
  * Event for opening the 'Save As' dialog.
+ *
+ * \ingroup Event
  */
 struct ShowSaveAsDialogEvent final
 {};
@@ -55,5 +62,16 @@ struct ShowAboutDialogEvent final
  */
 struct ShowCreditsDialogEvent final
 {};
+
+/**
+ * Event for opening the dialog for renaming a layer in the active map.
+ *
+ * \ingroup Event
+ */
+struct ShowRenameLayerDialogEvent final
+{
+  /** The target layer. */
+  EntityID layer_entity;
+};
 
 }  // namespace tactile
---
+++ source/core/inc/tactile/core/event/layer_events.hpp
@@ -0,0 +1,110 @@
+// Copyright (C) 2024 Albin Johansson (GNU General Public License v3.0)
+
+#pragma once
+
+#include "tactile/base/container/string.hpp"
+#include "tactile/base/prelude.hpp"
+#include "tactile/core/entity/entity.hpp"
+#include "tactile/core/layer/layer_type.hpp"
+#include "tactile/core/numeric/vec.hpp"
+
+namespace tactile {
+
+/**
+ * Event for creating a new layer in the active map.
+ *
+ * \ingroup Event
+ */
+struct CreateLayerEvent final
+{
+  /** The type of layer to create. */
+  LayerType type;
+};
+
+/**
+ * Event for removing a layer from the active map.
+ *
+ * \ingroup Event
+ */
+struct RemoveLayerEvent final
+{
+  /** The target layer. */
+  EntityID layer_entity;
+};
+
+/**
+ * Event for duplicating a layer in the active map.
+ *
+ * \ingroup Event
+ */
+struct DuplicateLayerEvent final
+{
+  /** The target layer. */
+  EntityID layer_entity;
+};
+
+/**
+ * Event for moving a layer up in the active map.
+ *
+ * \ingroup Event
+ */
+struct MoveLayerUpEvent final
+{
+  /** The target layer. */
+  EntityID layer_entity;
+};
+
+/**
+ * Event for moving a layer down in the active map.
+ *
+ * \ingroup Event
+ */
+struct MoveLayerDownEvent final
+{
+  /** The target layer. */
+  EntityID layer_entity;
+};
+
+/**
+ * Event for changing the name of a layer in the active map.
+ *
+ * \ingroup Event
+ */
+struct RenameLayerEvent final
+{
+  /** The target layer. */
+  EntityID layer_entity;
+
+  /** The new layer name. */
+  String name;
+};
+
+/**
+ * Event for changing the opacity of a layer in the active map.
+ *
+ * \ingroup Event
+ */
+struct SetLayerOpacityEvent final
+{
+  /** The target layer. */
+  EntityID layer_entity;
+
+  /** The new layer opacity. */
+  float opacity;
+};
+
+/**
+ * Event for changing the visibility of a layer in the active map.
+ *
+ * \ingroup Event
+ */
+struct SetLayerVisibleEvent final
+{
+  /** The target layer. */
+  EntityID layer_entity;
+
+  /** The new visibility. */
+  bool visible;
+};
+
+}  // namespace tactile
