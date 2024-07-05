576446298a02b71006a35d27a93c58c6cf2c0b70
Albin Johansson <albinoscar99@hotmail.com>
2024-07-06 01:02:33 +0200

    Add smart pointer for use across DLLs

--- source/base/inc/tactile/base/container/smart_ptr.hpp
+++ source/base/inc/tactile/base/container/smart_ptr.hpp
@@ -10,6 +10,9 @@ template <typename T, typename Deleter = std::default_delete<T>>
 using Unique = std::unique_ptr<T, Deleter>;
 
 template <typename T>
+using UniqueForeign = Unique<T, void (*)(T*) noexcept>;
+
+template <typename T>
 using Shared = std::shared_ptr<T>;
 
 template <typename T>
