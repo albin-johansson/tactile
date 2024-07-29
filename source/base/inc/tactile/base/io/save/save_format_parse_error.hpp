5ebff5b5735500ebc0c49d73e76835af54ba29e1
Albin Johansson <albinoscar99@hotmail.com>
2024-07-29 23:15:28 +0200

    Add save format error description utility

--- source/base/inc/tactile/base/io/save/save_format_parse_error.hpp
+++ source/base/inc/tactile/base/io/save/save_format_parse_error.hpp
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include "tactile/base/container/string.hpp"
 #include "tactile/base/int.hpp"
 #include "tactile/base/prelude.hpp"
 
@@ -68,5 +69,69 @@ enum class SaveFormatParseError : uint8
   kNoMapNextLayerId,
   kNoMapNextObjectId,
 };
+
+/**
+ * Returns a short human-readable description of a parse error.
+ *
+ * \param error The save format parse error.
+ *
+ * \return
+ * An error description.
+ */
+[[nodiscard]]
+constexpr auto to_string(const SaveFormatParseError error) noexcept -> StringView
+{
+  switch (error) {
+    case SaveFormatParseError::kNoPropertyName:          return "missing property name";
+    case SaveFormatParseError::kNoPropertyType:          return "missing property type";
+    case SaveFormatParseError::kNoPropertyValue:         return "missing property value";
+    case SaveFormatParseError::kBadPropertyType:         return "bad property type";
+    case SaveFormatParseError::kBadPropertyValue:        return "bad property value";
+    case SaveFormatParseError::kNoObjectId:              return "missing object identifier";
+    case SaveFormatParseError::kNoObjectName:            return "missing object name";
+    case SaveFormatParseError::kNoObjectX:               return "missing object x-coordinate";
+    case SaveFormatParseError::kNoObjectY:               return "missing object y-coordinate";
+    case SaveFormatParseError::kNoObjectWidth:           return "missing object width";
+    case SaveFormatParseError::kNoObjectHeight:          return "missing object height";
+    case SaveFormatParseError::kNoObjectVisibility:      return "missing object visibility";
+    case SaveFormatParseError::kNoLayerId:               return "missing layer identifier";
+    case SaveFormatParseError::kNoLayerName:             return "missing layer name";
+    case SaveFormatParseError::kNoLayerOpacity:          return "missing layer opacity";
+    case SaveFormatParseError::kNoLayerVisibility:       return "missing layer visibility";
+    case SaveFormatParseError::kNoLayerType:             return "missing layer type";
+    case SaveFormatParseError::kBadLayerType:            return "bad layer type";
+    case SaveFormatParseError::kNoTileLayerWidth:        return "missing tile layer width";
+    case SaveFormatParseError::kNoTileLayerHeight:       return "missing tile layer height";
+    case SaveFormatParseError::kNoTileLayerData:         return "missing tile layer data";
+    case SaveFormatParseError::kBadTileLayerEncoding:    return "bad tile layer encoding";
+    case SaveFormatParseError::kBadTileLayerData:        return "bad tile layer data";
+    case SaveFormatParseError::kNoGroupLayerData:        return "missing group layer data";
+    case SaveFormatParseError::kNoSuchCompressionFormat: return "no such compression format";
+    case SaveFormatParseError::kNoTilesetFirstTileId:
+      return "missing first tileset tile identifier";
+    case SaveFormatParseError::kNoTilesetName:             return "missing tileset name";
+    case SaveFormatParseError::kNoTilesetTileWidth:        return "missing tileset tile width";
+    case SaveFormatParseError::kNoTilesetTileHeight:       return "missing tileset tile height";
+    case SaveFormatParseError::kNoTilesetTileCount:        return "missing tileset tile count";
+    case SaveFormatParseError::kNoTilesetColumns:          return "missing tileset column count";
+    case SaveFormatParseError::kNoTilesetImageWidth:       return "missing tileset image width";
+    case SaveFormatParseError::kNoTilesetImageHeight:      return "missing tileset image height";
+    case SaveFormatParseError::kNoTilesetImage:            return "missing tileset image";
+    case SaveFormatParseError::kNoSuchTilesetImage:        return "no such tileset image";
+    case SaveFormatParseError::kNoTileIndex:               return "missing tile index";
+    case SaveFormatParseError::kNoAnimationFrameTileIndex: return "missing frame tile index";
+    case SaveFormatParseError::kNoAnimationFrameDuration:  return "missing frame duration";
+    case SaveFormatParseError::kNoMapOrientation:          return "missing orientation";
+    case SaveFormatParseError::kBadMapOrientation:         return "bad orientation";
+    case SaveFormatParseError::kNoMapWidth:                return "missing map width";
+    case SaveFormatParseError::kNoMapHeight:               return "missing map height";
+    case SaveFormatParseError::kNoMapTileWidth:            return "missing map tile width";
+    case SaveFormatParseError::kNoMapTileHeight:           return "missing map tile height";
+    case SaveFormatParseError::kNoMapNextLayerId:          return "missing map next layer identifier";
+    case SaveFormatParseError::kNoMapNextObjectId:         return "missing map next object identifier";
+  }
+
+  return "?";
+}
 
 }  // namespace tactile
