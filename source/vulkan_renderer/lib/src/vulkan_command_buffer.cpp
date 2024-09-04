28dbb2085a8afcb201daae64d67fce4254014d34
Albin Johansson <albinoscar99@hotmail.com>
2024-09-04 19:22:45 +0300

    Add Vulkan command buffer class

---
+++ source/vulkan_renderer/lib/src/vulkan_command_buffer.cpp
@@ -0,0 +1,71 @@
+// Copyright (C) 2024 Albin Johansson (GNU General Public License v3.0)
+
+#include "tactile/vulkan_renderer/vulkan_command_buffer.hpp"
+
+#include <utility>  // exchange
+
+#include "tactile/runtime/logging.hpp"
+#include "tactile/vulkan_renderer/vulkan_util.hpp"
+
+namespace tactile {
+
+VulkanCommandBuffer::VulkanCommandBuffer(VulkanCommandBuffer&& other) noexcept
+    : device {std::exchange(other.device, VK_NULL_HANDLE)},
+      pool {std::exchange(other.pool, VK_NULL_HANDLE)},
+      handle {std::exchange(other.handle, VK_NULL_HANDLE)}
+{}
+
+auto VulkanCommandBuffer::operator=(VulkanCommandBuffer&& other) noexcept
+    -> VulkanCommandBuffer&
+{
+  if (this != &other) {
+    _destroy();
+
+    device = std::exchange(other.device, VK_NULL_HANDLE);
+    pool = std::exchange(other.pool, VK_NULL_HANDLE);
+    handle = std::exchange(other.handle, VK_NULL_HANDLE);
+  }
+
+  return *this;
+}
+
+VulkanCommandBuffer::~VulkanCommandBuffer() noexcept
+{
+  _destroy();
+}
+
+void VulkanCommandBuffer::_destroy() noexcept
+{
+  if (handle != VK_NULL_HANDLE) {
+    vkFreeCommandBuffers(device, pool, 1, &handle);
+    handle = VK_NULL_HANDLE;
+  }
+}
+
+auto create_vulkan_command_buffer(VkDevice device, VkCommandPool pool)
+    -> std::expected<VulkanCommandBuffer, VkResult>
+{
+  const VkCommandBufferAllocateInfo command_buffer_allocate_info {
+    .sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO,
+    .pNext = nullptr,
+    .commandPool = pool,
+    .level = VK_COMMAND_BUFFER_LEVEL_PRIMARY,
+    .commandBufferCount = 1,
+  };
+
+  VulkanCommandBuffer command_buffer {};
+  command_buffer.device = device;
+  command_buffer.pool = pool;
+
+  const auto result =
+      vkAllocateCommandBuffers(device, &command_buffer_allocate_info, &command_buffer.handle);
+
+  if (result != VK_SUCCESS) {
+    log(LogLevel::kError, "Could not create Vulkan command buffer: {}", to_string(result));
+    return std::unexpected {result};
+  }
+
+  return command_buffer;
+}
+
+}  // namespace tactile
