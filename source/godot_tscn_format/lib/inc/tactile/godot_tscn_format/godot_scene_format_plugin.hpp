5921f808586ad8d024170acf21596ebdd40f6a59
Albin Johansson <albinoscar99@hotmail.com>
2024-10-05 18:36:50 +0200

    Rename Godot scene format plugin

--- source/godot_tscn_format/lib/inc/tactile/godot_tscn_format/godot_scene_format_plugin.hpp
+++ source/godot_tscn_format/lib/inc/tactile/godot_tscn_format/godot_scene_format_plugin.hpp
@@ -2,13 +2,16 @@
 
 #pragma once
 
+#include <memory>  // unique_ptr
+
 #include "tactile/base/prelude.hpp"
 #include "tactile/godot_tscn_format/api.hpp"
+#include "tactile/godot_tscn_format/godot_scene_format.hpp"
 #include "tactile/runtime/plugin.hpp"
 
-namespace tactile {
+namespace tactile::godot {
 
-class TACTILE_GODOT_API GodotTscnFormatPlugin final : public IPlugin
+class TACTILE_GODOT_API GodotSceneFormatPlugin final : public IPlugin
 {
  public:
   void load(IRuntime* runtime) override;
@@ -16,7 +19,8 @@ class TACTILE_GODOT_API GodotTscnFormatPlugin final : public IPlugin
   void unload() override;
 
  private:
-  IRuntime* mRuntime {};
+  IRuntime* m_runtime {};
+  std::unique_ptr<GodotSceneFormat> m_format {};
 };
 
 extern "C"
@@ -25,4 +29,4 @@ extern "C"
   TACTILE_GODOT_API void tactile_free_plugin(IPlugin* plugin);
 }
 
-}  // namespace tactile
+}  // namespace tactile::godot
