b1894ccff0245f162bc56207761cc5acb0c3c5bf
Albin Johansson <albinoscar99@hotmail.com>
2025-02-25 17:43:04 +0100

    Move basic command types to editor

--- cmake/tactile.cmake
+++ cmake/tactile.cmake
@@ -1,86 +1,89 @@
-set(TACTILE_CXX_STANDARD 23)
 
-if (CMAKE_CXX_COMPILER_ID MATCHES "MSVC")
-  list(APPEND TACTILE_COMPILE_OPTIONS
-       "/EHsc"
-       "/MP"
-       "/W4"
-       "/bigobj"
-       "/permissive-"
-       "/Zc:preprocessor"
-       "/Zc:__cplusplus"
-       "/wd4251"
-       "/wd4275"
+if (MSVC)
+  list(APPEND
+       TACTILE_COMPILE_OPTIONS
+       /EHsc
+       /MP
+       /W4
+       /bigobj
+       /permissive-
+       /Zc:preprocessor
+       /Zc:__cplusplus
+       /wd4251
+       /wd4275
        )
 else ()
-  list(APPEND TACTILE_COMPILE_OPTIONS
-       "-fvisibility=hidden"
-       "-Wall"
-       "-Wextra"
-       "-Wpedantic"
-       "-Wconversion"
-       "-Wsign-conversion"
-       "-Wswitch-enum"
-       "-Wold-style-cast"
+  list(APPEND
+       TACTILE_COMPILE_OPTIONS
+       -fvisibility=hidden
+       -Wall
+       -Wextra
+       -Wpedantic
+       -Wconversion
+       -Wsign-conversion
+       -Wswitch-enum
+       -Wold-style-cast
        )
 
   if (TACTILE_BUILD_TYPE STREQUAL "asan")
     if (NOT MSVC)
-      list(APPEND TACTILE_COMPILE_OPTIONS
-           "-fsanitize=address"
-           "-fno-sanitize-recover"
-           "-fno-omit-frame-pointer"
+      list(APPEND
+           TACTILE_COMPILE_OPTIONS
+           -fsanitize=address
+           -fno-sanitize-recover
+           -fno-omit-frame-pointer
            )
-      list(APPEND TACTILE_LINK_OPTIONS
-           "-fsanitize=address"
-           "-fno-sanitize-recover"
-           "-fno-omit-frame-pointer"
+      list(APPEND
+           TACTILE_LINK_OPTIONS
+           -fsanitize=address
+           -fno-sanitize-recover
+           -fno-omit-frame-pointer
            )
     endif ()
   endif ()
 endif ()
 
-list(APPEND TACTILE_PRECOMPILED_HEADERS
-     "<algorithm>"
-     "<array>"
-     "<charconv>"
-     "<chrono>"
-     "<codecvt>"
-     "<concepts>"
-     "<cstddef>"
-     "<cstdint>"
-     "<cstring>"
-     "<cstdlib>"
-     "<ctime>"
-     "<cmath>"
-     "<deque>"
-     "<exception>"
-     "<filesystem>"
-     "<fstream>"
-     "<functional>"
-     "<iterator>"
-     "<limits>"
-     "<locale>"
-     "<map>"
-     "<memory>"
-     "<numbers>"
-     "<optional>"
-     "<ostream>"
-     "<queue>"
-     "<random>"
-     "<span>"
-     "<sstream>"
-     "<string>"
-     "<string_view>"
-     "<system_error>"
-     "<type_traits>"
-     "<unordered_map>"
-     "<utility>"
-     "<variant>"
-     "<vector>"
+list(APPEND
+     TACTILE_PRECOMPILED_HEADERS
+     <algorithm>
+     <array>
+     <charconv>
+     <chrono>
+     <codecvt>
+     <concepts>
+     <cstddef>
+     <cstdint>
+     <cstring>
+     <cstdlib>
+     <ctime>
+     <cmath>
+     <deque>
+     <exception>
+     <filesystem>
+     <fstream>
+     <functional>
+     <iterator>
+     <limits>
+     <locale>
+     <map>
+     <memory>
+     <numbers>
+     <optional>
+     <ostream>
+     <queue>
+     <random>
+     <span>
+     <sstream>
+     <string>
+     <string_view>
+     <system_error>
+     <type_traits>
+     <unordered_map>
+     <utility>
+     <variant>
+     <vector>
      )
 
-message(DEBUG "TACTILE_CXX_STANDARD: ${TACTILE_CXX_STANDARD}")
 message(DEBUG "TACTILE_COMPILE_OPTIONS: ${TACTILE_COMPILE_OPTIONS}")
 message(DEBUG "TACTILE_LINK_OPTIONS: ${TACTILE_LINK_OPTIONS}")
 message(DEBUG "TACTILE_PRECOMPILED_HEADERS: ${TACTILE_PRECOMPILED_HEADERS}")
@@ -94,33 +97,38 @@ function(tactile_set_output_directory target directory)
                         )
 endfunction()
 
-function(tactile_set_properties target)
+function(tactile_prepare_target target)
   set_target_properties(${target}
                         PROPERTIES
-                        CXX_STANDARD "${TACTILE_CXX_STANDARD}"
-                        CXX_EXTENSIONS "OFF"
-                        CXX_STANDARD_REQUIRED "ON"
-                        POSITION_INDEPENDENT_CODE "ON"
-                        INTERPROCEDURAL_OPTIMIZATION "${TACTILE_USE_LTO}"
+                        NAMESPACE tactile
+                        CXX_STANDARD 23
+                        CXX_EXTENSIONS OFF
+                        CXX_STANDARD_REQUIRED ON
+                        POSITION_INDEPENDENT_CODE ON
+                        INTERPROCEDURAL_OPTIMIZATION ${TACTILE_USE_LTO}
                         PREFIX ""
                         )
+  tactile_set_output_directory(${target} ${TACTILE_BUILD_DIR})
 endfunction()
 
-function(tactile_set_compiler_options target)
-  target_compile_options(${target} PRIVATE "${TACTILE_COMPILE_OPTIONS}")
-  target_link_options(${target} PRIVATE "${TACTILE_LINK_OPTIONS}")
-endfunction()
+add_library(tactile_basic_target INTERFACE)
 
-function(tactile_prepare_target target)
-  tactile_set_compiler_options(${target})
-  tactile_set_properties(${target})
-  tactile_set_output_directory(${target} "${TACTILE_BUILD_DIR}")
+target_compile_options(tactile_basic_target INTERFACE ${TACTILE_COMPILE_OPTIONS})
 
-  if (TACTILE_USE_PRECOMPILED_HEADERS MATCHES "ON")
-    target_precompile_headers(${target} PRIVATE "${TACTILE_PRECOMPILED_HEADERS}")
-  endif ()
+target_link_options(tactile_basic_target INTERFACE ${TACTILE_LINK_OPTIONS})
 
-  if (TACTILE_BUILD_TYPE STREQUAL "asan" AND NOT MSVC)
-    target_link_libraries(${target} PRIVATE "-fsanitize=address")
-  endif ()
-endfunction()
+if (TACTILE_USE_PRECOMPILED_HEADERS)
+  target_precompile_headers(tactile_basic_target INTERFACE ${TACTILE_PRECOMPILED_HEADERS})
+endif ()
+
+if (TACTILE_BUILD_TYPE STREQUAL "asan" AND NOT MSVC)
+  target_link_libraries(tactile_basic_target INTERFACE "-fsanitize=address")
+endif ()
+
+if (WIN32)
+  target_compile_definitions(tactile_basic_target
+                             INTERFACE
+                             "WIN32_LEAN_AND_MEAN"
+                             "NOMINMAX"
+                             )
+endif ()
---
+++ source/editor/lib/src/tactile/editor/cmd/command_stack.cpp
@@ -0,0 +1,161 @@
+// Copyright (C) 2024 Albin Johansson (GNU General Public License v3.0)
+
+#include "tactile/core/cmd/command_stack.hpp"
+
+#include "tactile/core/debug/assert.hpp"
+
+namespace tactile::core {
+
+CommandStack::CommandStack(const std::size_t capacity)
+  : m_capacity {capacity}
+{}
+
+void CommandStack::mark_as_clean()
+{
+  m_clean_index = m_current_index;
+}
+
+void CommandStack::reset_clean_index()
+{
+  m_clean_index.reset();
+}
+
+void CommandStack::undo()
+{
+  TACTILE_ASSERT(can_undo());
+
+  m_commands.at(m_current_index.value())->undo();
+  _reset_or_decrease_current_index();
+}
+
+void CommandStack::redo()
+{
+  TACTILE_ASSERT(can_redo());
+
+  m_commands.at(_get_next_command_index())->redo();
+  _increase_current_index();
+}
+
+void CommandStack::_store(std::unique_ptr<ICommand> cmd)
+{
+  if (size() == capacity()) {
+    _remove_oldest_command();
+  }
+
+  _remove_commands_after_current_index();
+  _increase_current_index();
+
+  m_commands.push_back(std::move(cmd));
+}
+
+void CommandStack::set_capacity(const std::size_t capacity)
+{
+  m_capacity = capacity;
+
+  const auto command_count = size();
+  if (command_count > m_capacity) {
+    const auto excess_count = command_count - m_capacity;
+    for (std::size_t i = 0; i < excess_count; ++i) {
+      _remove_oldest_command();
+    }
+  }
+}
+
+auto CommandStack::is_clean() const -> bool
+{
+  return m_commands.empty() || (m_clean_index == m_current_index);
+}
+
+auto CommandStack::can_undo() const -> bool
+{
+  return !m_commands.empty() && m_current_index.has_value();
+}
+
+auto CommandStack::can_redo() const -> bool
+{
+  return (!m_commands.empty() && !m_current_index.has_value()) ||
+         (!m_commands.empty() && m_current_index < m_commands.size() - 1);
+}
+
+auto CommandStack::size() const -> std::size_t
+{
+  return m_commands.size();
+}
+
+auto CommandStack::capacity() const -> std::size_t
+{
+  return m_capacity;
+}
+
+auto CommandStack::index() const -> std::optional<std::size_t>
+{
+  return m_current_index;
+}
+
+auto CommandStack::clean_index() const -> std::optional<std::size_t>
+{
+  return m_clean_index;
+}
+
+void CommandStack::_remove_oldest_command()
+{
+  TACTILE_ASSERT(!m_commands.empty());
+
+  m_commands.front()->dispose();
+  m_commands.pop_front();
+  _reset_or_decrease_current_index();
+  _reset_or_decrease_clean_index();
+}
+
+void CommandStack::_remove_commands_after_current_index()
+{
+  const auto start_index = _get_next_command_index();
+
+  // If we have a clean index, and there are undone commands when another
+  // command is pushed, then the clean index becomes invalidated.
+  if (m_clean_index >= start_index) {
+    m_clean_index.reset();
+  }
+
+  const auto command_count = m_commands.size();
+  for (auto cmd_index = start_index; cmd_index < command_count; ++cmd_index) {
+    m_commands.back()->dispose();
+    m_commands.pop_back();
+  }
+}
+
+void CommandStack::_reset_or_decrease_clean_index()
+{
+  if (m_clean_index.has_value()) {
+    if (m_clean_index == 0) {
+      m_clean_index.reset();
+    }
+    else {
+      m_clean_index = *m_clean_index - 1;
+    }
+  }
+}
+
+void CommandStack::_reset_or_decrease_current_index()
+{
+  if (m_current_index.has_value()) {
+    if (m_current_index == 0) {
+      m_current_index.reset();
+    }
+    else {
+      m_current_index = *m_current_index - 1;
+    }
+  }
+}
+
+void CommandStack::_increase_current_index()
+{
+  m_current_index = _get_next_command_index();
+}
+
+auto CommandStack::_get_next_command_index() const -> std::size_t
+{
+  return m_current_index.has_value() ? *m_current_index + 1 : 0;
+}
+
+}  // namespace tactile::core
