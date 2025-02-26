ee414a7ed22f5507e3d97da4d3ae878092488dc9
Albin Johansson <albinoscar99@hotmail.com>
2025-02-26 22:29:09 +0100

    Add basic cast functions

--- source/core/test/src/main.cpp
+++ source/core/test/src/main.cpp
@@ -1,30 +1,9 @@
-// Copyright (C) 2024 Albin Johansson (GNU General Public License v3.0)
-
-#include <memory>   // make_unique
-#include <utility>  // move
+// Copyright (C) 2025 Albin Johansson (GNU General Public License v3.0)
 
 #include <gtest/gtest.h>
 
-#include "tactile/base/util/scope_exit.hpp"
-#include "tactile/core/logging.hpp"
-#include "tactile/core/platform/win32.hpp"
-#include "tactile/log/terminal_log_sink.hpp"
-
 auto main(int argc, char* argv[]) -> int
 {
-  tactile::core::win32_enable_virtual_terminal_processing();
-
-  auto terminal_sink = std::make_unique<tactile::log::TerminalLogSink>();
-  terminal_sink->set_use_ansi_colors(true);
-
-  tactile::log::Logger logger {};
-  logger.add_sink(std::move(terminal_sink));
-  logger.set_log_level(tactile::log::LogLevel::kTrace);
-  logger.set_reference_instant(tactile::log::Logger::clock_type::now());
-
-  const tactile::ScopeExit logger_guard {[] { tactile::core::set_logger(nullptr); }};
-  tactile::core::set_logger(&logger);
-
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
