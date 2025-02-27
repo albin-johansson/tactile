3fe204623d2c6d7f6e776274d4dbe97253532815
Albin Johansson <albinoscar99@hotmail.com>
2025-02-27 16:55:57 +0100

    Add executable target

---
+++ source/editor/app/src/tactile/editor/main.cpp
@@ -0,0 +1,8 @@
+// Copyright (C) 2024 Albin Johansson (GNU General Public License v3.0)
+
+#include "tactile/editor/launcher.hpp"
+
+auto main(const int argc, char* argv[]) -> int
+{
+  return tactile::editor::launch(argc, argv);
+}
---
+++ source/editor/lib/src/tactile/editor/launcher.cpp
@@ -0,0 +1,14 @@
+// Copyright (C) 2025 Albin Johansson (GNU General Public License v3.0)
+
+#include "tactile/editor/launcher.hpp"
+
+#include <cstdlib>  // EXIT_SUCCESS
+
+namespace tactile::editor {
+
+auto launch(const int argc, char* argv[]) -> int
+{
+  return EXIT_SUCCESS;
+}
+
+}  // namespace tactile::editor
