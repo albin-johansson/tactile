5f4b9de8a6cfbc979c3e557c79727fa7e5a883ad
Albin Johansson <albinoscar99@hotmail.com>
2025-03-03 21:28:27 +0100

    Add compressor interface

---
+++ source/core/lib/inc/tactile/core/plugin/compressor.hpp
@@ -0,0 +1,27 @@
+// Copyright (C) 2025 Albin Johansson (GNU General Public License v3.0)
+
+#pragma once
+
+#include "tactile/core/api.hpp"
+#include "tactile/core/basic/macros.hpp"
+#include "tactile/core/basic/primitives.hpp"
+#include "tactile/core/basic/result.hpp"
+#include "tactile/core/basic/span.hpp"
+#include "tactile/core/basic/vector.hpp"
+
+namespace tactile {
+
+/// Interface implemented by compression plugins.
+class TACTILE_CORE_API ICompressor
+{
+ public:
+  TACTILE_INTERFACE_CLASS(ICompressor);
+
+  /// Compresses a stream of bytes.
+  virtual auto compress(Span<const u8> data) -> Result<Vector<u8>> = 0;
+
+  /// Decompresses a stream of compressed bytes.
+  virtual auto decompress(Span<const u8> data) -> Result<Vector<u8>> = 0;
+};
+
+}  // namespace tactile
