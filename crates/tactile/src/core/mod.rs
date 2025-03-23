eed4a0f65480bfdc61212f52eaa8c7c0076a7edb
Albin Johansson <albinoscar99@hotmail.com>
2025-03-23 15:40:22 +0100

    Add Extent::pos_to_idx and Extent::area

--- crates/tactile/src/core/mod.rs
+++ crates/tactile/src/core/mod.rs
@@ -101,6 +101,33 @@ impl Extent {
     Self { width, height }
   }
 
+  /// Converts a [`TilePos`] to the corresponding 1-dimensional index within the extent.
+  ///
+  /// # Examples
+  ///
+  /// ```
+  /// use tactile::core::{Extent, TilePos};
+  ///
+  /// let extent = Extent::new(3, 3);
+  ///
+  /// assert_eq!(extent.pos_to_idx(TilePos::new(0, 0)).unwrap(), 0);
+  /// assert_eq!(extent.pos_to_idx(TilePos::new(1, 0)).unwrap(), 1);
+  /// assert_eq!(extent.pos_to_idx(TilePos::new(2, 0)).unwrap(), 2);
+  /// assert_eq!(extent.pos_to_idx(TilePos::new(0, 1)).unwrap(), 3);
+  /// ```
+  pub const fn pos_to_idx(&self, pos: TilePos) -> Option<isize> {
+    if !self.contains(pos.x, pos.y) {
+      None
+    } else {
+      Some(pos.y * self.width + pos.x)
+    }
+  }
+
+  /// Returns the area of the extent.
+  pub const fn area(&self) -> isize {
+    self.width * self.height
+  }
+
   /// Indicates whether the extent has a non-negative area.
   pub const fn has_area(&self) -> bool {
     self.width > 0 && self.height > 0
--- crates/tactile/tests/core/extent.rs
+++ crates/tactile/tests/core/extent.rs
@@ -2,15 +2,45 @@
 
 use googletest::{
   assert_that,
-  prelude::{eq, is_false, is_true},
+  prelude::{eq, is_false, is_true, none, some},
 };
-use tactile::core::Extent;
+use tactile::core::{Extent, TilePos};
 
 #[test]
 fn new() {
   let extent = Extent::new(15, 10);
   assert_that!(extent.width, eq(15));
   assert_that!(extent.height, eq(10));
+}
+
+#[test]
+fn pos_to_idx() {
+  let extent = Extent::new(3, 3);
+
+  assert_that!(extent.pos_to_idx(TilePos::new(0, 0)), some(eq(0)));
+  assert_that!(extent.pos_to_idx(TilePos::new(1, 0)), some(eq(1)));
+  assert_that!(extent.pos_to_idx(TilePos::new(2, 0)), some(eq(2)));
+  assert_that!(extent.pos_to_idx(TilePos::new(0, 1)), some(eq(3)));
+  assert_that!(extent.pos_to_idx(TilePos::new(1, 1)), some(eq(4)));
+  assert_that!(extent.pos_to_idx(TilePos::new(2, 1)), some(eq(5)));
+  assert_that!(extent.pos_to_idx(TilePos::new(0, 2)), some(eq(6)));
+  assert_that!(extent.pos_to_idx(TilePos::new(1, 2)), some(eq(7)));
+  assert_that!(extent.pos_to_idx(TilePos::new(2, 2)), some(eq(8)));
+
+  assert_that!(extent.pos_to_idx(TilePos::new(-1, 0)), none());
+  assert_that!(extent.pos_to_idx(TilePos::new(0, -1)), none());
+  assert_that!(extent.pos_to_idx(TilePos::new(3, 0)), none());
+  assert_that!(extent.pos_to_idx(TilePos::new(0, 3)), none());
+}
+
+#[test]
+fn area() {
+  assert_that!(Extent::new(0, 0).area(), eq(0));
+  assert_that!(Extent::new(1, 0).area(), eq(0));
+  assert_that!(Extent::new(0, 1).area(), eq(0));
+  assert_that!(Extent::new(1, 1).area(), eq(1));
+  assert_that!(Extent::new(2, 3).area(), eq(6));
+  assert_that!(Extent::new(3, 3).area(), eq(9));
 }
 
 #[test]
