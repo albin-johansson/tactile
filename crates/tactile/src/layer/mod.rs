becf95603a591620c5226fd48efa52166e33e851
Albin Johansson <albinoscar99@hotmail.com>
2025-03-30 23:47:34 +0200

    Add LayerId

--- crates/tactile/src/layer/mod.rs
+++ crates/tactile/src/layer/mod.rs
@@ -1,5 +1,37 @@
 // Copyright (C) 2025 Albin Johansson (GNU General Public License v3.0)
 
+use derive_more::{Add, AddAssign, Sub, SubAssign};
+
+/// Strong type for layer identifiers.
+#[derive(
+  Copy,
+  Clone,
+  Debug,
+  Default,
+  Eq,
+  PartialEq,
+  Ord,
+  PartialOrd,
+  Hash,
+  Add,
+  AddAssign,
+  Sub,
+  SubAssign,
+)]
+pub struct LayerId(u32);
+
+impl LayerId {
+  /// Creates a layer identifier.
+  pub fn new(id: u32) -> Self {
+    Self(id)
+  }
+
+  /// Returns the underlying value.
+  pub fn value(self) -> u32 {
+    self.0
+  }
+}
+
 mod annotation;
 pub use annotation::{Annotation, AnnotationKind};
 
