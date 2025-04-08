e2b7cc29498301c856eaba01541f315c6e7e52f9
Albin Johansson <albinoscar99@hotmail.com>
2025-04-08 06:58:07 +0200

    Tweak test layout for 'tactile' crate

--- crates/tactile/tests/tactile.rs
+++ crates/tactile/tests/tactile.rs
@@ -1,4 +1,3 @@
 // Copyright (C) 2024 Albin Johansson (GNU General Public License v3.0)
 
-mod tile;
 mod ut;
--- crates/tactile/tests/ut/mod.rs
+++ crates/tactile/tests/ut/mod.rs
@@ -1,3 +1,4 @@
 // Copyright (C) 2025 Albin Johansson (GNU General Public License v3.0)
 
 mod common;
+mod tile;
