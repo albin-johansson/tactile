17e28bce3abdca1a7775d3c0d5cdc87b56eda45e
Albin Johansson <albinoscar99@hotmail.com>
2025-04-17 06:48:51 +0200

    Add Texture struct

--- crates/tactile/src/io/texture.rs
+++ crates/tactile/src/io/texture.rs
@@ -4,6 +4,7 @@ use derive_more::Add;
 use derive_more::AddAssign;
 use derive_more::Sub;
 use derive_more::SubAssign;
+use glam::IVec2;
 
 /// Strong type for texture identifiers.
 #[derive(
@@ -25,13 +26,36 @@ pub struct TextureId(u32);
 
 impl TextureId {
   /// Creates a texture identifier.
-  pub fn new(id: u32) -> Self {
+  pub const fn new(id: u32) -> Self {
     Self(id)
   }
 
   /// Returns the underlying value.
-  pub fn value(self) -> u32 {
+  pub const fn value(self) -> u32 {
     self.0
   }
 }
 
+/// Represents a reference to a texture.
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub struct Texture {
+  id: TextureId,
+  size: IVec2,
+}
+
+impl Texture {
+  /// Creates a new `Texture`.
+  pub const fn new(id: TextureId, size: IVec2) -> Self {
+    Self { id, size }
+  }
+
+  /// Returns the associated [`TextureId`].
+  pub const fn id(&self) -> TextureId {
+    self.id
+  }
+
+  /// Returns the size of the texture.
+  pub const fn size(&self) -> IVec2 {
+    self.size
+  }
+}
