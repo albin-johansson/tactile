ea3af7fdcfffd805f151d9cca4641b907c1aadcc
Albin Johansson <albinoscar99@hotmail.com>
2025-04-28 19:57:15 +0200

    Tweak Vcpkg triplet files

--- cmake/x64-windows-tactile.cmake
+++ cmake/x64-windows-tactile.cmake
@@ -1,9 +1,9 @@
-set(VCPKG_TARGET_ARCHITECTURE x64)
-set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE static)
+set(VCPKG_TARGET_ARCHITECTURE "x64")
+set(VCPKG_CRT_LINKAGE "dynamic")
+set(VCPKG_LIBRARY_LINKAGE "static")
 
 if (PORT MATCHES "sdl3")
-  set(VCPKG_LIBRARY_LINKAGE dynamic)
+  set(VCPKG_LIBRARY_LINKAGE "dynamic")
 endif ()
 
 message("${PORT} is using ${VCPKG_LIBRARY_LINKAGE} linkage")
