4d96c0e37f350bd1644a663a228ddd62239ec82c
Albin Johansson <albinoscar99@hotmail.com>
2025-05-10 15:13:01 +0200

    Configure logger for 'tactile' module tests

--- tests/tactile/tactile_tests.main.cpp
+++ tests/tactile/tactile_tests.main.cpp
@@ -2,8 +2,31 @@
 
 #include <gtest/gtest.h>
 
-auto main(int argc, char* argv[]) -> int
+import std;
+import tactile;
+
+namespace tactile::tests {
+namespace {
+
+[[nodiscard]]
+auto run(int argc, char* argv[]) -> int
 {
+  auto& logger = get_logger();
+  logger.set_start_time(Logger::clock_type::now());
+  logger.set_min_level(LogLevel::kTrace);
+  logger.set_flush_level(LogLevel::kError);
+  logger.add_sink(make_unique<ConsoleLogSink>());
+
+  const Defer reset_logger {[] { get_logger().reset(); }};
+
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
+}
+
+}  // namespace
+}  // namespace tactile::tests
+
+auto main(const int argc, char* argv[]) -> int
+{
+  return tactile::tests::run(argc, argv);
 }
