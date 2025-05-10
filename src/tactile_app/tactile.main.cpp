e51bd97788d63078e14e256a7c012fc5ca7075f9
Albin Johansson <albinoscar99@hotmail.com>
2025-05-10 17:06:15 +0200

    Add tactile_app target

---
+++ src/tactile_app/tactile.main.cpp
@@ -0,0 +1,8 @@
+// Copyright (C) 2025 Albin Johansson (GNU General Public License v3.0)
+
+import tactile_editor;
+
+auto main(const int argc, char* argv[]) -> int
+{
+  return tactile::editor::launch(argc, argv);
+}
