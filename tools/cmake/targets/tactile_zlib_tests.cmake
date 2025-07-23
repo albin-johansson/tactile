9409d08788cdf1ae72903b6cee899ec297a0e01a
Albin Johansson <albinoscar99@hotmail.com>
2025-07-23 23:00:33 +0200

    Move Zlib test files

--- tools/cmake/targets/tactile_zlib_tests.cmake
+++ tools/cmake/targets/tactile_zlib_tests.cmake
@@ -1,14 +1,16 @@
-project(tactile_zlib_tests_project CXX)
+# Copyright (C) 2025 Albin Johansson
+
+find_package(GTest CONFIG REQUIRED)
 
 add_executable(tactile_zlib_tests)
 
+tactile_set_target_properties(tactile_zlib_tests)
+
 target_sources(tactile_zlib_tests
                PRIVATE
-               "tactile_zlib_tests.main.cpp"
-               "zlib_compression_format.test.cpp"
+               "${TACTILE_TEST_DIR}/tactile/zlib/tactile_zlib_tests.main.cpp"
+               "${TACTILE_TEST_DIR}/tactile/zlib/zlib_compression_format.test.cpp"
                )
-
-tactile_set_target_properties(tactile_zlib_tests)
 
 target_link_libraries(tactile_zlib_tests
                       PRIVATE
