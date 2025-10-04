425eb4e013db7340633d87dbfea0514982552a70
Albin Johansson <albinoscar99@hotmail.com>
2025-10-04 14:00:21 +0200

    Create events.hpp

---
+++ include/tactile/editor/event/events.hpp
@@ -0,0 +1,27 @@
+// Copyright (C) 2025 Albin Johansson
+// This software is provided under the terms of the MIT License.
+
+#pragma once
+
+#include <utility>
+
+#include "tactile/editor/event/event_id.hpp"
+
+namespace tactile::editor {
+
+/// Represents all kinds of editor events.
+///
+/// Every event type must provide a public static data member called "kID" with the value
+/// of the associated entry in this enum. See the EventQueue class for details.
+enum class EventKind : EventID  // NOLINT(*-enum-size)
+{
+  kQuit,
+};
+
+/// Event indicating that editor should be shut down.
+struct QuitEvent final
+{
+  constexpr static EventID kID = std::to_underlying(EventKind::kQuit);
+};
+
+}  // namespace tactile::editor
--- tools/cmake/targets/tactile_editor.cmake
+++ tools/cmake/targets/tactile_editor.cmake
@@ -13,7 +13,9 @@ target_link_libraries(tactile_editor
 
 target_sources(tactile_editor
                PUBLIC FILE_SET "HEADERS" BASE_DIRS "${PROJECT_SOURCE_DIR}/include" FILES
+               "${PROJECT_SOURCE_DIR}/include/tactile/editor/event/event_id.hpp"
                "${PROJECT_SOURCE_DIR}/include/tactile/editor/event/event_queue.hpp"
+               "${PROJECT_SOURCE_DIR}/include/tactile/editor/event/events.hpp"
                "${PROJECT_SOURCE_DIR}/include/tactile/editor/app.hpp"
 
                PRIVATE
