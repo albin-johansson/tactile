26b9b9d3fbe0c9219aee2abc86cf0e28239df6f4
Albin Johansson <albinoscar99@hotmail.com>
2025-10-04 13:36:55 +0200

    Create function.hpp

---
+++ include/tactile/core/container/function.hpp
@@ -0,0 +1,13 @@
+// Copyright (C) 2025 Albin Johansson
+// This software is provided under the terms of the MIT License.
+
+#pragma once
+
+#include <functional>
+
+namespace tactile {
+
+template <typename T>
+using Function = std::function<T>;
+
+}  // namespace tactile
