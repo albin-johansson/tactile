c0fcb9a5f2862c6aaca60c6561c233db83d5cc30
Albin Johansson <albinoscar99@hotmail.com>
2025-10-05 12:03:37 +0200

    Create array.hpp

---
+++ include/tactile/core/container/array.hpp
@@ -0,0 +1,15 @@
+// Copyright (C) 2025 Albin Johansson
+// This software is provided under the terms of the MIT License.
+
+#pragma once
+
+#include <array>
+
+#include "tactile/core/primitives.hpp"
+
+namespace tactile {
+
+template <typename T, usize N>
+using Array = std::array<T, N>;
+
+}  // namespace tactile
