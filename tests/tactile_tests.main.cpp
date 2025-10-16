0b62711f3ea1231e692404a1be7008a25df50fb3
Albin Johansson <albinoscar99@hotmail.com>
2025-10-16 16:49:53 +0200

    Initialize logger in tests

--- tests/tactile_tests.main.cpp
+++ tests/tactile_tests.main.cpp
@@ -3,8 +3,23 @@
 
 #include <gtest/gtest.h>
 
-auto main(int argc, char* argv[]) -> int
+#include "tactile/editor/util/logger_context.hpp"
+
+namespace tactile {
+namespace {
+
+// NOLINTNEXTLINE(*-avoid-c-arrays)
+auto run_tests(int argc, char* argv[]) -> int
 {
+  const editor::LoggerContext logger_context {};
   testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
+}
+
+}  // namespace
+}  // namespace tactile
+
+auto main(const int argc, char* argv[]) -> int
+{
+  return tactile::run_tests(argc, argv);
 }
