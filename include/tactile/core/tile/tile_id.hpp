b4b5d5b3bb9836e54a37036ee22e3c541a105484
Albin Johansson <albinoscar99@hotmail.com>
2025-10-18 15:39:24 +0200

    Add TileID alias

---
+++ include/tactile/core/tile/tile_id.hpp
@@ -0,0 +1,14 @@
+// Copyright (C) 2025 Albin Johansson
+// This software is provided under the terms of the MIT License.
+
+#pragma once
+
+#include "tactile/core/primitives.hpp"
+
+namespace tactile {
+
+using TileID = int32;
+
+inline constexpr TileID kEmptyTileId = 0;
+
+}  // namespace tactile
